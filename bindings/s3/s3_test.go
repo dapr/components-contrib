0a710b46dc4c173ab111345259b676dd5c5e8ba1
Yaron Schneider <yaronsc@microsoft.com>
2019-09-27 09:13:35 -0700

    add s3 binding (#6)
    * add s3 binding
    
    * add debug log on nonexistent key
    
    * include key in debug log

---
+++ bindings/s3/s3_test.go
@@ -0,0 +1,20 @@
+package s3
+
+import (
+	"testing"
+
+	"github.com/actionscore/components-contrib/bindings"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseMetadata(t *testing.T) {
+	m := bindings.Metadata{}
+	m.Properties = map[string]string{"AccessKey": "key", "Region": "region", "SecretKey": "secret", "Bucket": "test"}
+	s3 := AWSS3{}
+	meta, err := s3.parseMetadata(m)
+	assert.Nil(t, err)
+	assert.Equal(t, "key", meta.AccessKey)
+	assert.Equal(t, "region", meta.Region)
+	assert.Equal(t, "secret", meta.SecretKey)
+	assert.Equal(t, "test", meta.Bucket)
+}

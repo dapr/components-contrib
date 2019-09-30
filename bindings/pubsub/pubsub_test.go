a5c40a970d843478844c7d20b82cf1316ed15b39
Yaron Schneider <yaronsc@microsoft.com>
2019-09-30 08:46:20 -0700

    Add GCP Pub/Sub input and output binding (#18)
    * add gcp pub-sub input/output binding
    
    * update bindings list
    
    * Update pubsub_test.go

---
+++ bindings/pubsub/pubsub_test.go
@@ -0,0 +1,35 @@
+package pubsub
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/actionscore/components-contrib/bindings"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInit(t *testing.T) {
+	m := bindings.Metadata{}
+	m.Properties = map[string]string{"auth_provider_x509_cert_url": "https://auth", "auth_uri": "https://auth", "client_x509_cert_url": "https://cert", "client_email": "test@test.com", "client_id": "id", "private_key": "****",
+					 "private_key_id": "key_id", "project_id": "project1", "token_uri": "https://token", "type": "serviceaccount", "topic": "t1", "subscription": "s1"}
+	ps := GCPPubSub{}
+	b, err := ps.parseMetadata(m)
+	assert.Nil(t, err)
+
+	var pubsubMeta pubSubMetadata
+	err = json.Unmarshal(b, &pubsubMeta)
+	assert.Nil(t, err)
+
+	assert.Equal(t, "s1", pubsubMeta.Subscription)
+	assert.Equal(t, "t1", pubsubMeta.Topic)
+	assert.Equal(t, "https://auth", pubsubMeta.AuthProviderCertURL)
+	assert.Equal(t, "https://auth", pubsubMeta.AuthURI)
+	assert.Equal(t, "https://cert", pubsubMeta.ClientCertURL)
+	assert.Equal(t, "test@test.com", pubsubMeta.ClientEmail)
+	assert.Equal(t, "id", pubsubMeta.ClientID)
+	assert.Equal(t, "****", pubsubMeta.PrivateKey)
+	assert.Equal(t, "key_id", pubsubMeta.PrivateKeyID)
+	assert.Equal(t, "project1", pubsubMeta.ProjectID)
+	assert.Equal(t, "https://token", pubsubMeta.TokenURI)
+	assert.Equal(t, "serviceaccount", pubsubMeta.Type)
+}

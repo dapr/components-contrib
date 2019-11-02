120a7db2d3a54a1185f389847093b294768c7eb4
Roberto J Rojas <robertojrojas@gmail.com>
2019-11-02 12:16:30 -0400

    fixes issue #8: adds kafka as pubsub component

---
+++ pubsub/kafka/kafka_test.go
@@ -0,0 +1,25 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package kafka
+
+import (
+	"testing"
+
+	"github.com/dapr/components-contrib/pubsub"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseMetadata(t *testing.T) {
+	m := pubsub.Metadata{}
+	m.Properties = map[string]string{"consumerGroup": "a", "publishTopic": "a", "brokers": "a", "topics": "a"}
+	k := Kafka{}
+	meta, err := k.getKafkaMetadata(m)
+	assert.Nil(t, err)
+	assert.Equal(t, "a", meta.Brokers[0])
+	assert.Equal(t, "a", meta.ConsumerGroup)
+	assert.Equal(t, "a", meta.PublishTopic)
+	assert.Equal(t, "a", meta.Topics[0])
+}

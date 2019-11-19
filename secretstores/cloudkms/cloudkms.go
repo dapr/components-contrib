be0b2462145fdf79945c1fedb0e78c7fa04545d0
Prafulla Anurag <pranurag@microsoft.com>
2019-11-19 17:10:17 +0530

    Add GetSecret Module

--- secretstores/cloudkms/cloudkms.go
+++ secretstores/cloudkms/cloudkms.go
@@ -9,16 +9,12 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 
 	cloudkms "cloud.google.com/go/kms/apiv1"
 	"github.com/dapr/components-contrib/secretstores"
 	"google.golang.org/api/option"
 	kmspb "google.golang.org/genproto/googleapis/cloud/kms/v1"
-)
-
-const (
-	projectId  string = "projectId"
-	locationId string = "locationId"
 )
 
 // cloudkmsSecretStore is a secret store implementation of GCS KMS
@@ -38,6 +34,9 @@ type cloudkmsMetadata struct {
 	TokenURI            string `json:"token_uri"`
 	AuthProviderCertURL string `json:"auth_provider_x509_cert_url"`
 	ClientCertURL       string `json:"client_x509_cert_url"`
+	SecretFilePath      string `json:"secret_file_path"`
+	KeyRingID           string `json:"key_ring_id"`
+	CryptoKeyID         string `json:"crypto_key_id"`
 }
 
 // NewCloudKMSSecretStore returns a new cloudkmsSecretStore instance
@@ -72,7 +71,26 @@ func (c *cloudkmsSecretStore) Init(metadata secretstores.Metadata) error {
 
 // GetSecret retrieves a secret using a key and returns a map of decrypted string
 func (c *cloudkmsSecretStore) GetSecret(req secretstores.SecretStore) (secretstores.GetSecretResponse, error) {
-	//TBC
+	ciphertext, err := ioutil.ReadFile(c.metadata.SecretFilePath)
+	if err != nil {
+		return secretstores.GetSecretResponse{Data: nil}, fmt.Errorf("error reading secret file: %s", err)
+	}
+
+	name := fmt.Sprintf("projects/%s/locations/global/keyRings/%s/cryptoKeys/%s",
+		c.metadata.ProjectID, c.metadata.KeyRingID, c.metadata.CryptoKeyID)
+	secretResp, err := c.decryptSymmetric(name, ciphertext)
+
+	secretValue := string(secretResp)
+
+	if err != nil {
+		return secretstores.GetSecretResponse{Data: nil}, fmt.Errorf("error occurred while decrypting: %s", err)
+	}
+
+	return secretstores.GetSecretResponse{
+		Data: map[string]string{
+			secretstores.DefaultSecretRefKeyName: secretValue,
+		},
+	}, nil
 }
 
 func (c *cloudkmsSecretStore) decryptSymmetric(name string, ciphertext []byte) ([]byte, error) {

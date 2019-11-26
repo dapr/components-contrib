9ae9544a412259482ed78ca8b4cfd24405ed3f47
sayboras <sayboras@yahoo.com>
2019-11-26 22:40:00 +1100

    Incorporate review comment

---
+++ secretstores/aws/secretmanager/secretmanager_integ_test.go
@@ -0,0 +1,32 @@
+// +build integration
+
+package secretmanager
+
+import (
+	"github.com/dapr/components-contrib/secretstores"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+// TestGetSecret requires AWS specific environments for authentication
+// AWS_DEFAULT_REGION AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KkEY, AWS_SESSION_TOKEN
+func TestIntegrationGetSecret(t *testing.T) {
+	secretName := "/aws/secret/testing"
+	sm := NewSecretManager()
+	err := sm.Init(secretstores.Metadata{
+		Properties: map[string]string{
+			"Region":       os.Getenv("AWS_DEFAULT_REGION"),
+			"AccessKey":    os.Getenv("AWS_ACCESS_KEY_ID"),
+			"SecretKey":    os.Getenv("AWS_SECRET_ACCESS_KEY"),
+			"SessionToken": os.Getenv("AWS_SESSION_TOKEN"),
+		},
+	})
+	assert.Nil(t, err)
+	response, err := sm.GetSecret(secretstores.GetSecretRequest{
+		Name:     secretName,
+		Metadata: map[string]string{},
+	})
+	assert.Nil(t, err)
+	assert.NotNil(t, response)
+}

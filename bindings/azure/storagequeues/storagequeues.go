5d5583cacf33da9e168959ef6bd5e0ea06274d91
Russell Stather <russell.stather@outlook.com>
2019-11-26 21:35:12 +0200

    boilerplate added

--- bindings/azure/storagequeues/storagequeues.go
+++ bindings/azure/storagequeues/storagequeues.go
@@ -4,3 +4,35 @@
 // ------------------------------------------------------------
 
 package storagequeues
+
+import "github.com/dapr/components-contrib/bindings"
+
+// AzureStorageQueues is an input/output binding reading from and sending events to Azure Storage queues
+type AzureStorageQueues struct {
+}
+
+type storageQueuesMetadata struct {
+}
+
+// NewAzureStorageQueues returns a new AzureStorageQueues instance
+func NewAzureStorageQueues() *AzureStorageQueues {
+	return &AzureStorageQueues{}
+}
+
+// Init parses connection properties and creates a new Storage Queue client
+func (a *AzureStorageQueues) Init(metadata bindings.Metadata) error {
+	return nil
+}
+
+func (a *AzureStorageQueues) parseMetadata(metadata bindings.Metadata) (*storageQueuesMetadata, error) {
+
+	return nil, nil
+}
+
+func (a *AzureStorageQueues) Write(req *bindings.WriteRequest) error {
+	return nil
+}
+
+func (a *AzureStorageQueues) Read(handler func(*bindings.ReadResponse) error) error {
+	return nil
+}

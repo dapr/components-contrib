2c3996e87cf26a60e8ac51e3974a08b84c95a1f2
Russell Stather <Russell.Stather@mac.com>
2019-11-29 09:16:14 +0200

    write queue done parse metadata done

--- bindings/azure/storagequeues/storagequeues.go
+++ bindings/azure/storagequeues/storagequeues.go
@@ -5,13 +5,25 @@
 
 package storagequeues
 
-import "github.com/dapr/components-contrib/bindings"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/url"
+	"time"
+
+	"github.com/Azure/azure-storage-queue-go/azqueue"
+	"github.com/dapr/components-contrib/bindings"
+)
 
 // AzureStorageQueues is an input/output binding reading from and sending events to Azure Storage queues
 type AzureStorageQueues struct {
 }
 
 type storageQueuesMetadata struct {
+	AccountKey string `json:"accountKey"`
+	QueueName  string `json:"queueName"`
 }
 
 // NewAzureStorageQueues returns a new AzureStorageQueues instance
@@ -25,11 +37,44 @@ func (a *AzureStorageQueues) Init(metadata bindings.Metadata) error {
 }
 
 func (a *AzureStorageQueues) parseMetadata(metadata bindings.Metadata) (*storageQueuesMetadata, error) {
+	b, err := json.Marshal(metadata.Properties)
+	if err != nil {
+		return nil, err
+	}
 
-	return nil, nil
+	var m storageQueuesMetadata
+	err = json.Unmarshal(b, &m)
+	if err != nil {
+		return nil, err
+	}
+	return &m, nil
 }
 
 func (a *AzureStorageQueues) Write(req *bindings.WriteRequest) error {
+	var accountName = "daprcomp"
+	var accountKey = "LDCIro1iN8LPqFo822X4Oecap/8s8anCNslB2pcxE1/pax/svHY7StnpGnOcIE1JiSU8IAVrag8t6QrjcYH/JQ=="
+
+	u, _ := url.Parse(fmt.Sprintf("https://%s.queue.core.windows.net/queue6", accountName))
+
+	credential, err := azqueue.NewSharedKeyCredential(accountName, accountKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := context.TODO()
+	queueURL := azqueue.NewQueueURL(*u, azqueue.NewPipeline(credential, azqueue.PipelineOptions{}))
+	_, err = queueURL.Create(ctx, azqueue.Metadata{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	messagesURL := queueURL.NewMessagesURL()
+
+	_, err = messagesURL.Enqueue(ctx, "A message", time.Second*0, time.Minute*10)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 	return nil
 }
 
--- bindings/azure/storagequeues/storagequeues_test.go
+++ bindings/azure/storagequeues/storagequeues_test.go
@@ -4,3 +4,27 @@
 // ------------------------------------------------------------
 
 package storagequeues
+
+import (
+	"testing"
+
+	"github.com/dapr/components-contrib/bindings"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWriteQueue(t *testing.T) {
+	a := NewAzureStorageQueues()
+	a.Write(nil)
+}
+
+func TestParseMetadata(t *testing.T) {
+	m := bindings.Metadata{}
+	m.Properties = map[string]string{"accountKey": "myKey", "queueName": "queue1"}
+
+	a := NewAzureStorageQueues()
+	meta, err := a.parseMetadata(m)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "myKey", meta.AccountKey)
+	assert.Equal(t, "queue1", meta.QueueName)
+}

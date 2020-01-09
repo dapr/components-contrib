49553859a00f9f77f1085c903a94a8bf326cd8e9
Dr. David Kunz <david.kunz@sap.com>
2020-01-09 21:34:32 +0100

    Update redis_test.go (#165)
    Co-authored-by: Yaron Schneider <yaronsc@microsoft.com>

--- bindings/redis/redis_test.go
+++ bindings/redis/redis_test.go
@@ -14,10 +14,10 @@ import (
 
 func TestParseMetadata(t *testing.T) {
 	m := bindings.Metadata{}
-	m.Properties = map[string]string{"redisHost": "a", "redisPassword": "a"}
+	m.Properties = map[string]string{"redisHost": "host", "redisPassword": "password"}
 	r := Redis{}
 	redisM, err := r.parseMetadata(m)
 	assert.Nil(t, err)
-	assert.Equal(t, "a", redisM.Host)
-	assert.Equal(t, "a", redisM.Password)
+	assert.Equal(t, "host", redisM.Host)
+	assert.Equal(t, "password", redisM.Password)
 }

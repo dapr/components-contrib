f826ebcf02500b85e69e61cdfa5e7653906eb0f2
abhishek <abhirockzz@gmail.com>
2020-03-06 18:41:47 +0530

    update implementation to use EPH

--- bindings/azure/eventhubs/eventhubs_test.go
+++ bindings/azure/eventhubs/eventhubs_test.go
@@ -9,17 +9,64 @@ import (
 	"testing"
 
 	"github.com/dapr/components-contrib/bindings"
-	"github.com/dapr/dapr/pkg/logger"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestParseMetadata(t *testing.T) {
-	m := bindings.Metadata{}
-	m.Properties = map[string]string{"connectionString": "a", "consumerGroup": "a", "messageAge": "a"}
-	eh := AzureEventHubs{logger: logger.NewLogger("test")}
-	meta, err := eh.parseMetadata(m)
-	assert.Nil(t, err)
-	assert.Equal(t, "a", meta.ConnectionString)
-	assert.Equal(t, "a", meta.ConsumerGroup)
-	assert.Equal(t, "a", meta.MessageAge)
+	t.Run("test valid configuration", func(t *testing.T) {
+
+		props := map[string]string{connectionString: "fake", consumerGroup: "mygroup", storageAccountName: "account", storageAccountKey: "key", storageContainerName: "container"}
+
+		bindingsMetadata := bindings.Metadata{Properties: props}
+
+		m, err := parseMetadata(bindingsMetadata)
+
+		assert.NoError(t, err)
+		assert.Equal(t, m.connectionString, "fake")
+		assert.Equal(t, m.storageAccountName, "account")
+		assert.Equal(t, m.storageAccountKey, "key")
+		assert.Equal(t, m.storageContainerName, "container")
+		assert.Equal(t, m.consumerGroup, "mygroup")
+	})
+
+	type invalidConfigTestCase struct {
+		name   string
+		config map[string]string
+		errMsg string
+	}
+	invalidConfigTestCases := []invalidConfigTestCase{
+		{
+			"missing consumerGroup",
+			map[string]string{connectionString: "fake", storageAccountName: "account", storageAccountKey: "key", storageContainerName: "container"},
+			missingConsumerGroupErrorMsg,
+		},
+		{
+			"missing connectionString",
+			map[string]string{consumerGroup: "fake", storageAccountName: "account", storageAccountKey: "key", storageContainerName: "container"},
+			missingConnectionStringErrorMsg,
+		},
+		{
+			"missing storageAccountName",
+			map[string]string{consumerGroup: "fake", connectionString: "fake", storageAccountKey: "key", storageContainerName: "container"},
+			missingStorageAccountNameErrorMsg,
+		},
+		{
+			"missing storageAccountKey",
+			map[string]string{consumerGroup: "fake", connectionString: "fake", storageAccountName: "name", storageContainerName: "container"},
+			missingStorageAccountKeyErrorMsg,
+		},
+		{
+			"missing storageContainerName",
+			map[string]string{consumerGroup: "fake", connectionString: "fake", storageAccountName: "name", storageAccountKey: "key"},
+			missingStorageContainerNameErrorMsg,
+		}}
+
+	for _, c := range invalidConfigTestCases {
+		t.Run(c.name, func(t *testing.T) {
+			bindingsMetadata := bindings.Metadata{Properties: c.config}
+			_, err := parseMetadata(bindingsMetadata)
+			assert.Error(t, err)
+			assert.Equal(t, err.Error(), c.errMsg)
+		})
+	}
 }

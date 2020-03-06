c61b341702680359751c5040563f3af0ca713f4f
petjot <piotrekjasina@gmail.com>
2020-03-06 22:23:12 +0100

    Add Hazelcast pubsub support

---
+++ pubsub/hazelcast/metadata_test.go
@@ -0,0 +1,23 @@
+package hazelcast
+
+import (
+	"github.com/dapr/components-contrib/pubsub"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestValidateMetadata(t *testing.T) {
+	t.Run("return error when required servers is empty", func(t *testing.T) {
+		fakeMetaData := pubsub.Metadata{
+			Properties: map[string]string{
+				hazelcastServers: "",
+			},
+		}
+
+		m, err := parseHazelcastMetadata(fakeMetaData)
+
+		// assert
+		assert.Error(t, err)
+		assert.Empty(t, m)
+	})
+}

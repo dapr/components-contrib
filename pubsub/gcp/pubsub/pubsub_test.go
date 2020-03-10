4c93c7615ca1a23ddb8e0b75dca06a874a550965
Sivamuthu Kumar <sivamuthukumar07@gmail.com>
2020-03-09 21:47:29 -0400

    GCP Cloud Pubsub pub sub implementation

---
+++ pubsub/gcp/pubsub/pubsub_test.go
@@ -0,0 +1,36 @@
+package pubsub
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/dapr/components-contrib/pubsub"
+	"github.com/dapr/dapr/pkg/logger"
+)
+
+func TestInit(t *testing.T) {
+	m := pubsub.Metadata{}
+	m.Properties = map[string]string{"auth_provider_x509_cert_url": "https://auth", "auth_uri": "https://auth", "client_x509_cert_url": "https://cert", "client_email": "test@test.com", "client_id": "id", "private_key": "****",
+		"private_key_id": "key_id", "project_id": "project1", "token_uri": "https://token", "type": "serviceaccount", "subscription": "s1"}
+	ps := GCPPubSub{logger: logger.NewLogger("test")}
+	b, err := ps.parseMetadata(m)
+	assert.Nil(t, err)
+
+	var pubsubMeta metadata
+	err = json.Unmarshal(b, &pubsubMeta)
+	assert.Nil(t, err)
+
+	assert.Equal(t, "s1", pubsubMeta.Subscription)
+	assert.Equal(t, "https://auth", pubsubMeta.AuthProviderCertURL)
+	assert.Equal(t, "https://auth", pubsubMeta.AuthURI)
+	assert.Equal(t, "https://cert", pubsubMeta.ClientCertURL)
+	assert.Equal(t, "test@test.com", pubsubMeta.ClientEmail)
+	assert.Equal(t, "id", pubsubMeta.ClientID)
+	assert.Equal(t, "****", pubsubMeta.PrivateKey)
+	assert.Equal(t, "key_id", pubsubMeta.PrivateKeyID)
+	assert.Equal(t, "project1", pubsubMeta.ProjectID)
+	assert.Equal(t, "https://token", pubsubMeta.TokenURI)
+	assert.Equal(t, "serviceaccount", pubsubMeta.Type)
+}

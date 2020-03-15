6eea99555b8cf481a1b9ae794c8e7069ef9a41f6
Sivamuthu Kumar <sivamuthukumar07@gmail.com>
2020-03-15 01:24:57 -0400

    Add Kinesis data stream binding

---
+++ bindings/aws/kinesis/kinesis_test.go
@@ -0,0 +1,29 @@
+package kinesis
+
+import (
+	"testing"
+
+	"github.com/dapr/components-contrib/bindings"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseMetadata(t *testing.T) {
+	m := bindings.Metadata{}
+	m.Properties = map[string]string{
+		"AccessKey":    "key",
+		"Region":       "region",
+		"SecretKey":    "secret",
+		"ConsumerName": "test",
+		"StreamName":   "stream",
+		"Mode":         "extended",
+	}
+	kinesis := AWSKinesis{}
+	meta, err := kinesis.parseMetadata(m)
+	assert.Nil(t, err)
+	assert.Equal(t, "key", meta.AccessKey)
+	assert.Equal(t, "region", meta.Region)
+	assert.Equal(t, "secret", meta.SecretKey)
+	assert.Equal(t, "test", meta.ConsumerName)
+	assert.Equal(t, "stream", meta.StreamName)
+	assert.Equal(t, kinesisConsumerMode("extended"), meta.KinesisConsumerMode)
+}

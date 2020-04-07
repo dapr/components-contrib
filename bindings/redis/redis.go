ee5a3ad23ea12732a4097d705273378feb0eb5b0
pruthvidhodda <prdhodda@microsoft.com>
2020-04-06 19:42:48 -0700

    Add TLS support for bindings.redis

--- bindings/redis/redis.go
+++ bindings/redis/redis.go
@@ -7,25 +7,37 @@ package redis
 
 import (
 	"context"
-	"encoding/json"
+	"crypto/tls"
 	"errors"
+	"fmt"
+	"strconv"
+	"time"
 
 	"github.com/dapr/components-contrib/bindings"
 	"github.com/dapr/dapr/pkg/logger"
 
-	"github.com/joomcode/redispipe/redis"
-	"github.com/joomcode/redispipe/redisconn"
+	redis "github.com/go-redis/redis/v7"
+)
+
+const (
+	host                   = "redisHost"
+	password               = "redisPassword"
+	enableTLS              = "enableTLS"
+	maxRetries             = "maxRetries"
+	maxRetryBackoff        = "maxRetryBackoff"
+	defaultBase            = 10
+	defaultBitSize         = 0
+	defaultDB              = 0
+	defaultExpirationTime  = 0
+	defaultMaxRetries      = 3
+	defaultMaxRetryBackoff = time.Second * 2
+	defaultEnableTLS       = false
 )
 
 // Redis is a redis output binding
 type Redis struct {
-	client *redis.SyncCtx
+	client *redis.Client
 	logger logger.Logger
-}
-
-type redisMetadata struct {
-	Host     string `json:"redisHost"`
-	Password string `json:"redisPassword"`
 }
 
 // NewRedis returns a new redis bindings instance
@@ -34,46 +46,84 @@ func NewRedis(logger logger.Logger) *Redis {
 }
 
 // Init performs metadata parsing and connection creation
-func (r *Redis) Init(metadata bindings.Metadata) error {
-	m, err := r.parseMetadata(metadata)
+func (r *Redis) Init(meta bindings.Metadata) error {
+	m, err := r.parseMetadata(meta)
 	if err != nil {
 		return err
 	}
-	ctx := context.Background()
-	opts := redisconn.Opts{
-		DB:       0,
-		Password: m.Password,
+
+	opts := &redis.Options{
+		Addr:            m.host,
+		Password:        m.password,
+		DB:              defaultDB,
+		MaxRetries:      m.maxRetries,
+		MaxRetryBackoff: m.maxRetryBackoff,
 	}
-	conn, err := redisconn.Connect(ctx, m.Host, opts)
+
+	/* #nosec */
+	if m.enableTLS {
+		opts.TLSConfig = &tls.Config{
+			InsecureSkipVerify: m.enableTLS,
+		}
+	}
+
+	r.client = redis.NewClient(opts)
+	_, err = r.client.Ping().Result()
 	if err != nil {
-		return err
+		return fmt.Errorf("redis binding: error connecting to redis at %s: %s", m.host, err)
 	}
-	r.client = &redis.SyncCtx{
-		S: conn,
-	}
-	return nil
+
+	return err
 }
 
-func (r *Redis) parseMetadata(metadata bindings.Metadata) (*redisMetadata, error) {
-	connInfo := metadata.Properties
-	b, err := json.Marshal(connInfo)
-	if err != nil {
-		return nil, err
+func (r *Redis) parseMetadata(meta bindings.Metadata) (metadata, error) {
+	m := metadata{}
+
+	if val, ok := meta.Properties[host]; ok && val != "" {
+		m.host = val
+	} else {
+		return m, errors.New("redis binding error: missing host address")
 	}
 
-	var m redisMetadata
-	err = json.Unmarshal(b, &m)
-	if err != nil {
-		return nil, err
+	if val, ok := meta.Properties[password]; ok && val != "" {
+		m.password = val
 	}
-	return &m, nil
+
+	m.enableTLS = defaultEnableTLS
+	if val, ok := meta.Properties[enableTLS]; ok && val != "" {
+		tls, err := strconv.ParseBool(val)
+		if err != nil {
+			return m, fmt.Errorf("redis binding error: can't parse enableTLS field: %s", err)
+		}
+		m.enableTLS = tls
+	}
+
+	m.maxRetries = defaultMaxRetries
+	if val, ok := meta.Properties[maxRetries]; ok && val != "" {
+		parsedVal, err := strconv.ParseInt(val, defaultBase, defaultBitSize)
+		if err != nil {
+			return m, fmt.Errorf("redis binding error: can't parse maxRetries field: %s", err)
+		}
+		m.maxRetries = int(parsedVal)
+	}
+
+	m.maxRetryBackoff = defaultMaxRetryBackoff
+	if val, ok := meta.Properties[maxRetryBackoff]; ok && val != "" {
+		parsedVal, err := strconv.ParseInt(val, defaultBase, defaultBitSize)
+		if err != nil {
+			return m, fmt.Errorf("redis binding error: can't parse maxRetries field: %s", err)
+		}
+		m.maxRetryBackoff = time.Duration(parsedVal)
+	}
+
+	return m, nil
 }
 
 func (r *Redis) Write(req *bindings.WriteRequest) error {
 	if val, ok := req.Metadata["key"]; ok && val != "" {
 		key := val
-		res := r.client.Do(context.Background(), "SET", key, req.Data)
-		if err := redis.AsError(res); err != nil {
+		_, err := r.client.DoContext(context.Background(), "SET", key, req.Data).Result()
+		if err != nil {
 			return err
 		}
 		return nil

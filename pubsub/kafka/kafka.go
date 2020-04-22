a0ff5916423064c7a7f8dfd85935e9bba6f2caa1
khous <khousley@infoblox.com>
2020-04-22 11:37:36 -0700

    only close channel once

--- pubsub/kafka/kafka.go
+++ pubsub/kafka/kafka.go
@@ -7,15 +7,15 @@ package kafka
 
 import (
 	"context"
+	"fmt"
+	"github.com/Shopify/sarama"
+	"github.com/dapr/components-contrib/pubsub"
+	"github.com/dapr/dapr/pkg/logger"
 	"os"
 	"os/signal"
 	"strings"
 	"sync"
 	"syscall"
-
-	"github.com/Shopify/sarama"
-	"github.com/dapr/components-contrib/pubsub"
-	log "github.com/sirupsen/logrus"
 )
 
 // Kafka allows reading/writing to a Kafka consumer group
@@ -23,6 +23,7 @@ type Kafka struct {
 	producer      sarama.SyncProducer
 	consumerGroup string
 	brokers       []string
+	logger			logger.Logger
 }
 
 type kafkaMetadata struct {
@@ -34,15 +35,14 @@ type kafkaMetadata struct {
 type consumer struct {
 	ready    chan bool
 	callback func(msg *pubsub.NewMessage) error
-}
-
-func (consumer *consumer) Setup(sarama.ConsumerGroupSession) error {
-	close(consumer.ready)
-	return nil
+	once sync.Once
 }
 
 func (consumer *consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	fmt.Printf("message consuming claim...")
 	for message := range claim.Messages() {
+		// XXX: Remove this
+		fmt.Printf("message consuming claim: %v\n", message)
 		if consumer.callback != nil {
 			err := consumer.callback(&pubsub.NewMessage{
 				Topic: claim.Topic(),
@@ -53,6 +53,7 @@ func (consumer *consumer) ConsumeClaim(session sarama.ConsumerGroupSession, clai
 			}
 		}
 	}
+
 	return nil
 }
 
@@ -60,9 +61,19 @@ func (consumer *consumer) Cleanup(sarama.ConsumerGroupSession) error {
 	return nil
 }
 
+func (consumer *consumer) Setup(sarama.ConsumerGroupSession) error {
+	consumer.once.Do(func () {
+		close(consumer.ready)
+	})
+
+	return nil
+}
+
 // NewKafka returns a new kafka pubsub instance
-func NewKafka() pubsub.PubSub {
-	return &Kafka{}
+func NewKafka(l logger.Logger) pubsub.PubSub {
+	// XXX: Remove this override of the log level
+	l.SetOutputLevel(logger.DebugLevel)
+	return &Kafka{logger: l}
 }
 
 // Init does metadata parsing and connection establishment
@@ -80,15 +91,20 @@ func (k *Kafka) Init(metadata pubsub.Metadata) error {
 	k.brokers = meta.Brokers
 	k.producer = p
 	k.consumerGroup = meta.ConsumerGroup
+	k.logger.Debug("Kafka message bus initialization complete")
 	return nil
 }
 
 // Publish message to Kafka cluster
 func (k *Kafka) Publish(req *pubsub.PublishRequest) error {
-	_, _, err := k.producer.SendMessage(&sarama.ProducerMessage{
+	k.logger.Debugf("Publishing topic %v with data: %v", req.Topic, req.Data)
+	partition, offset, err := k.producer.SendMessage(&sarama.ProducerMessage{
 		Topic: req.Topic,
 		Value: sarama.ByteEncoder(req.Data),
 	})
+
+	k.logger.Debugf("Partition: %v, offset: %v", partition, offset)
+
 	if err != nil {
 		return err
 	}
@@ -96,52 +112,78 @@ func (k *Kafka) Publish(req *pubsub.PublishRequest) error {
 	return nil
 }
 
-// Subscribe to topic in the Kafka cluser
+// Subscribe to topic in the Kafka cluster
+// This call cannot block like its sibling in bindings/kafka because of where this is invoked in runtime.go
 func (k *Kafka) Subscribe(req pubsub.SubscribeRequest, handler func(msg *pubsub.NewMessage) error) error {
-
 	config := sarama.NewConfig()
-	config.Version = sarama.V1_0_0_0
+	config.Version = sarama.V2_0_0_0
 
-	cs := consumer{
-		callback: handler,
-		ready:    make(chan bool),
-	}
+	cg, err := sarama.NewConsumerGroup(k.brokers, k.consumerGroup, config)
 
-	client, err := sarama.NewConsumerGroup(k.brokers, k.consumerGroup, config)
 	if err != nil {
 		return err
 	}
 
 	ctx, cancel := context.WithCancel(context.Background())
+	ready := make(chan bool)
+	cs := consumer{
+		ready: ready,
+		callback: handler,
+	}
 
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
 	go func() {
-		defer wg.Done()
+		defer func() {
+			k.logger.Debugf("Closing ConsumerGroup for topic %s", req.Topic)
+			cg.Close()
+		}()
+
+		k.logger.Debugf("Subscribed and listening to topic: %s", req.Topic)
+
 		for {
-			if err = client.Consume(ctx, []string{req.Topic}, &cs); err != nil {
-				log.Errorf("error from consumer topic(%s) - %v", req.Topic, err)
+			// Consume the requested topic
+			err := cg.Consume(ctx, []string{req.Topic}, &cs)
+			if err != nil {
+				k.logger.Errorf("Error consuming %s: %s", req.Topic, err)
 			}
-			// check if context was cancelled, signaling that the consumer should stop
+
+			// If the context was cancelled, as is the case when handling SIGINT and SIGTERM below, then this pops
+			// us out of the consume loop
 			if ctx.Err() != nil {
 				return
 			}
-			cs.ready = make(chan bool)
 		}
 	}()
 
-	<-cs.ready
+	// Spin up a go routine to handle OS signals
+	go func() {
+		sigterm := make(chan os.Signal, 1)
+		signal.Notify(sigterm, syscall.SIGINT, syscall.SIGTERM)
+		recvdSignal := <-sigterm
+		k.logger.Debug("Interrupt signal received. Shutting down pubsub listener.")
+		cancel()
+		err = cg.Close()
+		if err != nil {
+			k.logger.Errorf("Error closing consumer group client: %v", err)
+		}
+		// Resending the received signal in order to resume normal processing of that signal
+		signal.Reset()
+		proc, err := os.FindProcess(os.Getpid())
 
-	sigterm := make(chan os.Signal, 1)
-	signal.Notify(sigterm, syscall.SIGINT, syscall.SIGTERM)
-	<-sigterm
-	cancel()
-	wg.Wait()
-	if err = client.Close(); err != nil {
-		return err
-	}
+		if err != nil {
+			k.logger.Errorf("Error when handling interrupt. Exiting\n%v", err)
+			os.Exit(1)
+			return
+		}
+
+		// Resend the signal to continue with normal handling
+		if err = proc.Signal(recvdSignal); err != nil {
+			k.logger.Errorf("Error resending signal. Exiting\n%v", err)
+			os.Exit(1)
+		}
+	}()
+
+	<-ready
 	return nil
-
 }
 
 // getKafkaMetadata returns new Kafka metadata
@@ -152,6 +194,9 @@ func (k *Kafka) getKafkaMetadata(metadata pubsub.Metadata) (*kafkaMetadata, erro
 	if val, ok := metadata.Properties["brokers"]; ok && val != "" {
 		meta.Brokers = strings.Split(val, ",")
 	}
+
+	k.logger.Infof("Found brokers: %v", meta.Brokers)
+
 	return &meta, nil
 }
 

087e2ee8748add7e025a5321a9c39992c0763909
Mark Chmarny <mark@chmarny.com>
2020-04-28 08:56:29 -0700

    titter search binding

---
+++ bindings/twitter/twitter.go
@@ -0,0 +1,137 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package twitter
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/dapr/components-contrib/bindings"
+	"github.com/dapr/dapr/pkg/logger"
+	"github.com/pkg/errors"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+)
+
+type twitterInput struct {
+	consumerKey    string
+	consumerSecret string
+	accessToken    string
+	accessSecret   string
+	query          string
+	logger         logger.Logger
+}
+
+var _ = bindings.InputBinding(&twitterInput{})
+
+// NewTwitter returns a new Twitter event input binding
+func NewTwitter(logger logger.Logger) bindings.InputBinding {
+	return &twitterInput{logger: logger}
+}
+
+// Init initializes the Twitter binding
+func (t *twitterInput) Init(metadata bindings.Metadata) error {
+	return t.parseMetadata(metadata)
+}
+
+func (t *twitterInput) parseMetadata(metadata bindings.Metadata) error {
+	ck, f := metadata.Properties["consumerKey"]
+	if !f || ck == "" {
+		return fmt.Errorf("consumerKey not set")
+	}
+	cs, f := metadata.Properties["consumerSecret"]
+	if !f || cs == "" {
+		return fmt.Errorf("consumerSecret not set")
+	}
+	at, f := metadata.Properties["accessToken"]
+	if !f || at == "" {
+		return fmt.Errorf("accessToken not set")
+	}
+	as, f := metadata.Properties["accessSecret"]
+	if !f || as == "" {
+		return fmt.Errorf("accessSecret not set")
+	}
+	q, f := metadata.Properties["query"]
+	if !f || q == "" {
+		return fmt.Errorf("query not set")
+	}
+
+	t.consumerKey = ck
+	t.consumerSecret = cs
+	t.accessToken = at
+	t.accessSecret = as
+	t.query = q
+
+	return nil
+}
+
+// Read triggers the Twitter search and events on each result tweet
+func (t *twitterInput) Read(handler func(*bindings.ReadResponse) error) error {
+
+	config := oauth1.NewConfig(t.consumerKey, t.consumerSecret)
+	token := oauth1.NewToken(t.accessToken, t.accessSecret)
+
+	httpClient := config.Client(oauth1.NoContext, token)
+	client := twitter.NewClient(httpClient)
+
+	limit, _, err := client.RateLimits.Status(nil)
+	if err != nil {
+		return errors.Wrapf(err, "error checking rate status: %v", err)
+	}
+	t.logger.Debugf("rate limit: %+v", limit)
+
+	demux := twitter.NewSwitchDemux()
+
+	demux.Tweet = func(tweet *twitter.Tweet) {
+		t.logger.Debugf("raw tweet: %+v", tweet)
+		data, err := json.Marshal(tweet)
+		if err != nil {
+			t.logger.Errorf("error marshaling tweet: %+v", tweet)
+			return
+		}
+		handler(&bindings.ReadResponse{
+			Data: data,
+			Metadata: map[string]string{
+				"query": t.query,
+			},
+		})
+	}
+
+	demux.StreamLimit = func(limit *twitter.StreamLimit) {
+		t.logger.Warnf("disconnect: %+v", limit)
+	}
+
+	demux.StreamDisconnect = func(disconnect *twitter.StreamDisconnect) {
+		t.logger.Errorf("stream disconnect: %+v", disconnect)
+	}
+
+	filterParams := &twitter.StreamFilterParams{
+		Track:         []string{t.query},
+		StallWarnings: twitter.Bool(true),
+	}
+
+	t.logger.Infof("starting stream for query: %s", t.query)
+	stream, err := client.Streams.Filter(filterParams)
+	if err != nil {
+		return errors.Wrapf(err, "error executing stream filter: %+v", filterParams)
+	}
+	defer stream.Stop()
+
+	t.logger.Info("starting handler...")
+	go demux.HandleChan(stream.Messages)
+
+	// handle SIGINT and SIGTERM (HIT CTRL-C) signals
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	t.logger.Info(<-ch)
+	t.logger.Infof("stopping stream for query: %s...", t.query)
+
+	return nil
+}

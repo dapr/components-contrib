8827c58e783c98857b14f243790c8b4cea361a16
Lynn Orrell <Lynn.Orrell@gmail.com>
2020-04-28 14:50:13 -0500

    Added Azure Event Grid Bindings (#290)
    * event grid udpates
    
    * Initial event grid input binding
    
    * Added Write() support to EventGrid component
    
    * Updated eventgrid binding to use fasthttp instead of net/http
    
    * Fixed linting issue in EventGrid binding
    
    * Tidy up go.mod
    
    * Added handshakePort to allow custom port for EG callback
    
    * Now properly throwing an http 500 on handler err
    
    Co-authored-by: Yaron Schneider <yaronsc@microsoft.com>
    Co-authored-by: Young Bu Park <youngp@microsoft.com>
    Co-authored-by: Artur Souza <artursouza.ms@outlook.com>

---
+++ bindings/azure/eventgrid/eventgrid.go
@@ -0,0 +1,193 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package eventgrid
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+
+	"time"
+
+	"github.com/Azure/azure-sdk-for-go/services/eventgrid/mgmt/2019-06-01/eventgrid"
+	"github.com/Azure/go-autorest/autorest/azure/auth"
+	"github.com/dapr/components-contrib/bindings"
+	"github.com/dapr/dapr/pkg/logger"
+	"github.com/valyala/fasthttp"
+)
+
+// AzureEventGrid allows sending/receiving Azure Event Grid events
+type AzureEventGrid struct {
+	metadata *azureEventGridMetadata
+	logger   logger.Logger
+}
+
+type azureEventGridMetadata struct {
+	TenantID                  string `json:"tenantId"`
+	SubscriptionID            string `json:"subscriptionId"`
+	ResourceGroupName         string `json:"resourceGroupName"`
+	TopicName                 string `json:"topicName"`
+	EventGridSubscriptionName string `json:"eventGridSubscriptionName"`
+	SubscriberEndpoint        string `json:"subscriberEndpoint"`
+	TopicEndpoint             string `json:"topicEndpoint"`
+	ClientID                  string `json:"clientId"`
+	ClientSecret              string `json:"clientSecret"`
+	HandshakePort             string `json:"handshakePort"`
+}
+
+// NewAzureEventGrid returns a new Azure Event Grid instance
+func NewAzureEventGrid(logger logger.Logger) *AzureEventGrid {
+	logger.Debug("NewAzureEventGrid() called...")
+	return &AzureEventGrid{logger: logger}
+}
+
+// Init performs metadata init
+func (a *AzureEventGrid) Init(metadata bindings.Metadata) error {
+	a.logger.Debugf("Parsing Event Grid metadata(%s)...", metadata.Name)
+
+	m, err := a.parseMetadata(metadata)
+	if err != nil {
+		return err
+	}
+	a.metadata = m
+
+	a.logger.Debug("Metadata parsed successfully.")
+
+	return nil
+}
+
+func (a *AzureEventGrid) Read(handler func(*bindings.ReadResponse) error) error {
+	a.logger.Debug("Read() called...")
+
+	err := a.createSubscription()
+	if err != nil {
+		return err
+	}
+
+	m := func(ctx *fasthttp.RequestCtx) {
+		if string(ctx.Path()) == "/api/events" {
+			switch string(ctx.Method()) {
+			case "OPTIONS":
+				ctx.Response.Header.Add("WebHook-Allowed-Origin", string(ctx.Request.Header.Peek("WebHook-Request-Origin")))
+				ctx.Response.Header.Add("WebHook-Allowed-Rate", "*")
+				ctx.Response.Header.SetStatusCode(fasthttp.StatusOK)
+				_, err = ctx.Response.BodyWriter().Write([]byte(""))
+				if err != nil {
+					a.logger.Error(err.Error())
+				}
+			case "POST":
+				bodyBytes := ctx.PostBody()
+
+				a.logger.Debug(string(bodyBytes))
+				err = handler(&bindings.ReadResponse{
+					Data: bodyBytes,
+				})
+				if err != nil {
+					a.logger.Error(err.Error())
+					ctx.Error(err.Error(), fasthttp.StatusInternalServerError)
+				}
+			}
+		}
+	}
+
+	fasthttp.ListenAndServe(fmt.Sprintf(":%s", a.metadata.HandshakePort), m)
+
+	a.logger.Debugf("listening for Event Grid events at http://localhost:%s/api/events", a.metadata.HandshakePort)
+
+	return nil
+}
+
+func (a *AzureEventGrid) Write(req *bindings.WriteRequest) error {
+	a.logger.Debug("Write() called. data: %s", string(req.Data))
+
+	request := fasthttp.AcquireRequest()
+	defer fasthttp.ReleaseRequest(request)
+	request.Header.SetMethod(fasthttp.MethodPost)
+	request.Header.Set("Content-Type", "application/cloudevents+json")
+	request.Header.Set("aeg-sas-key", a.metadata.ClientSecret)
+	request.SetRequestURI(a.metadata.TopicEndpoint)
+	request.SetBody(req.Data)
+
+	response := fasthttp.AcquireResponse()
+	defer fasthttp.ReleaseResponse(response)
+
+	client := &fasthttp.Client{WriteTimeout: time.Second * 10}
+	err := client.Do(request, response)
+	if err != nil {
+		a.logger.Error(err.Error())
+		return err
+	}
+
+	if response.StatusCode() != fasthttp.StatusOK {
+		body := response.Body()
+		a.logger.Error(string(body))
+		return errors.New(string(body))
+	}
+
+	return nil
+}
+
+func (a *AzureEventGrid) parseMetadata(metadata bindings.Metadata) (*azureEventGridMetadata, error) {
+	b, err := json.Marshal(metadata.Properties)
+	if err != nil {
+		return nil, err
+	}
+
+	var eventGridMetadata azureEventGridMetadata
+	err = json.Unmarshal(b, &eventGridMetadata)
+	if err != nil {
+		return nil, err
+	}
+
+	if eventGridMetadata.HandshakePort == "" {
+		eventGridMetadata.HandshakePort = "8080"
+	}
+	return &eventGridMetadata, nil
+}
+
+func (a *AzureEventGrid) createSubscription() error {
+	clientCredentialsConfig := auth.NewClientCredentialsConfig(a.metadata.ClientID, a.metadata.ClientSecret, a.metadata.TenantID)
+
+	subscriptionClient := eventgrid.NewEventSubscriptionsClient(a.metadata.SubscriptionID)
+	authorizer, err := clientCredentialsConfig.Authorizer()
+	if err != nil {
+		return err
+	}
+	subscriptionClient.Authorizer = authorizer
+
+	scope := fmt.Sprintf("/subscriptions/%s/resourceGroups/%s/providers/Microsoft.EventGrid/topics/%s", a.metadata.SubscriptionID, a.metadata.ResourceGroupName, a.metadata.TopicName)
+
+	eventInfo := eventgrid.EventSubscription{
+		EventSubscriptionProperties: &eventgrid.EventSubscriptionProperties{
+			Destination: eventgrid.WebHookEventSubscriptionDestination{
+				EndpointType: eventgrid.EndpointTypeWebHook,
+				WebHookEventSubscriptionDestinationProperties: &eventgrid.WebHookEventSubscriptionDestinationProperties{
+					EndpointURL: &a.metadata.SubscriberEndpoint,
+				},
+			},
+		},
+	}
+
+	a.logger.Debugf("Attempting to create or update Event Grid subscription. scope=%s endpointURL=%s", scope, a.metadata.SubscriberEndpoint)
+	result, err := subscriptionClient.CreateOrUpdate(context.Background(), scope, a.metadata.EventGridSubscriptionName, eventInfo)
+	if err != nil {
+		return err
+	}
+
+	res := result.Future.Response()
+
+	if res.StatusCode != fasthttp.StatusCreated {
+		bodyBytes, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			return err
+		}
+		return errors.New(string(bodyBytes))
+	}
+
+	return nil
+}
---
+++ bindings/azure/eventgrid/eventgrid_test.go
@@ -0,0 +1,42 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package eventgrid
+
+import (
+	"testing"
+
+	"github.com/dapr/components-contrib/bindings"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseMetadata(t *testing.T) {
+	m := bindings.Metadata{}
+	m.Properties = map[string]string{
+		"clientId":                  "a",
+		"clientSecret":              "a",
+		"eventGridSubscriptionName": "a",
+		"resourceGroupName":         "a",
+		"subscriberEndpoint":        "a",
+		"subscriptionId":            "a",
+		"tenantId":                  "a",
+		"topicEndpoint":             "a",
+		"topicName":                 "a",
+	}
+
+	eh := AzureEventGrid{}
+	meta, err := eh.parseMetadata(m)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "a", meta.ClientID)
+	assert.Equal(t, "a", meta.ClientSecret)
+	assert.Equal(t, "a", meta.EventGridSubscriptionName)
+	assert.Equal(t, "a", meta.ResourceGroupName)
+	assert.Equal(t, "a", meta.SubscriberEndpoint)
+	assert.Equal(t, "a", meta.SubscriptionID)
+	assert.Equal(t, "a", meta.TenantID)
+	assert.Equal(t, "a", meta.TopicEndpoint)
+	assert.Equal(t, "a", meta.TopicName)
+}

c601d0b4f55c17246c7bd2cab748aa0abfc6df8c
Mark Chmarny <mchmarny@users.noreply.github.com>
2020-04-29 08:02:57 -0700

    Added Twitter search input binding (#318)
    * titter search binding
    
    * refactored signal processing for lint comp
    
    * titter search binding
    
    * refactored signal processing for lint comp
    
    * fixed twitter ref
    
    Co-authored-by: Yaron Schneider <yaronsc@microsoft.com>

---
+++ bindings/twitter/twitter.go
@@ -0,0 +1,163 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package twitter
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/dapr/components-contrib/bindings"
+	"github.com/dapr/dapr/pkg/logger"
+	"github.com/pkg/errors"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+)
+
+type twitterInput struct {
+	consumerKey    string
+	consumerSecret string
+	accessToken    string
+	accessSecret   string
+	query          string
+	logger         logger.Logger
+}
+
+var _ = bindings.InputBinding(&twitterInput{})
+
+// NewTwitter returns a new Twitter event input binding
+func NewTwitter(logger logger.Logger) bindings.InputBinding {
+	return &twitterInput{logger: logger}
+}
+
+// Init initializes the Twitter binding
+func (t *twitterInput) Init(metadata bindings.Metadata) error {
+	return t.parseMetadata(metadata)
+}
+
+func (t *twitterInput) parseMetadata(metadata bindings.Metadata) error {
+	ck, f := metadata.Properties["consumerKey"]
+	if !f || ck == "" {
+		return fmt.Errorf("consumerKey not set")
+	}
+	cs, f := metadata.Properties["consumerSecret"]
+	if !f || cs == "" {
+		return fmt.Errorf("consumerSecret not set")
+	}
+	at, f := metadata.Properties["accessToken"]
+	if !f || at == "" {
+		return fmt.Errorf("accessToken not set")
+	}
+	as, f := metadata.Properties["accessSecret"]
+	if !f || as == "" {
+		return fmt.Errorf("accessSecret not set")
+	}
+	q, f := metadata.Properties["query"]
+	if !f || q == "" {
+		return fmt.Errorf("query not set")
+	}
+
+	t.consumerKey = ck
+	t.consumerSecret = cs
+	t.accessToken = at
+	t.accessSecret = as
+	t.query = q
+
+	return nil
+}
+
+// Read triggers the Twitter search and events on each result tweet
+func (t *twitterInput) Read(handler func(*bindings.ReadResponse) error) error {
+	config := oauth1.NewConfig(t.consumerKey, t.consumerSecret)
+	token := oauth1.NewToken(t.accessToken, t.accessSecret)
+
+	httpClient := config.Client(oauth1.NoContext, token)
+	client := twitter.NewClient(httpClient)
+
+	limit, _, err := client.RateLimits.Status(nil)
+	if err != nil {
+		return errors.Wrapf(err, "error checking rate status: %v", err)
+	}
+	t.logger.Debugf("rate limit: %+v", limit)
+
+	demux := twitter.NewSwitchDemux()
+
+	demux.Tweet = func(tweet *twitter.Tweet) {
+		t.logger.Debugf("raw tweet: %+v", tweet)
+		data, marshalErr := json.Marshal(tweet)
+		if marshalErr != nil {
+			t.logger.Errorf("error marshaling tweet: %+v", tweet)
+			return
+		}
+		handler(&bindings.ReadResponse{
+			Data: data,
+			Metadata: map[string]string{
+				"query": t.query,
+			},
+		})
+	}
+
+	demux.StreamLimit = func(limit *twitter.StreamLimit) {
+		t.logger.Warnf("disconnect: %+v", limit)
+	}
+
+	demux.StreamDisconnect = func(disconnect *twitter.StreamDisconnect) {
+		t.logger.Errorf("stream disconnect: %+v", disconnect)
+	}
+
+	filterParams := &twitter.StreamFilterParams{
+		Track:         []string{t.query},
+		StallWarnings: twitter.Bool(true),
+	}
+
+	t.logger.Debug("starting stream for query: %s", t.query)
+	stream, err := client.Streams.Filter(filterParams)
+	if err != nil {
+		return errors.Wrapf(err, "error executing stream filter: %+v", filterParams)
+	}
+	defer stream.Stop()
+
+	t.logger.Debug("starting handler...")
+	go demux.HandleChan(stream.Messages)
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+
+	go func() {
+		for {
+			s := <-signalChan
+			switch s {
+			// kill -SIGHUP XXXX
+			case syscall.SIGHUP:
+				t.logger.Info("stopping, component hung up")
+
+			// kill -SIGINT XXXX or Ctrl+c
+			case syscall.SIGINT:
+				t.logger.Info("stopping, process killed (SIGINT)")
+
+			// kill -SIGTERM XXXX
+			case syscall.SIGTERM:
+				t.logger.Info("stopping, process killed (SIGTERM)")
+
+			// kill -SIGQUIT XXXX
+			case syscall.SIGQUIT:
+				t.logger.Info("stopping, process killed (SIGQUIT)")
+
+			default:
+				t.logger.Info("stopping, process killed (unknown signal)")
+			}
+		}
+	}()
+
+	return nil
+}
---
+++ bindings/twitter/twitter_test.go
@@ -0,0 +1,74 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package twitter
+
+import (
+	"testing"
+
+	"github.com/dapr/components-contrib/bindings"
+	"github.com/dapr/dapr/pkg/logger"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	testTwitterConsumerKey    = "test-consumerKey"
+	testTwitterConsumerSecret = "test-consumerSecret"
+	testTwitterAccessToken    = "test-accessToken"
+	testTwitterAccessSecret   = "test-accessSecret"
+	testTwitterQuery          = "test-query"
+)
+
+func getTestMetadata() bindings.Metadata {
+	m := bindings.Metadata{}
+	m.Properties = map[string]string{
+		"consumerKey":    testTwitterConsumerKey,
+		"consumerSecret": testTwitterConsumerSecret,
+		"accessToken":    testTwitterAccessToken,
+		"accessSecret":   testTwitterAccessSecret,
+		"query":          testTwitterQuery,
+	}
+	return m
+}
+
+func TestParseMetadata(t *testing.T) {
+	m := getTestMetadata()
+	i := twitterInput{logger: logger.NewLogger("test")}
+	err := i.parseMetadata(m)
+	assert.Nilf(t, err, "error parsing valid metadata properties")
+	assert.Equal(t, testTwitterConsumerKey, i.consumerKey, "consumerKey should be the same")
+	assert.Equal(t, testTwitterConsumerSecret, i.consumerSecret, "consumerSecret should be the same")
+	assert.Equal(t, testTwitterAccessToken, i.accessToken, "accessToken should be the same")
+	assert.Equal(t, testTwitterAccessSecret, i.accessSecret, "accessSecret should be the same")
+
+	m.Properties["consumerKey"] = ""
+	err = i.parseMetadata(m)
+	assert.NotNilf(t, err, "no error parsing invalid metadata properties")
+
+	m.Properties["consumerKey"] = testTwitterConsumerKey
+	m.Properties["query"] = ""
+	err = i.parseMetadata(m)
+	assert.NotNilf(t, err, "no error parsing invalid metadata properties")
+}
+
+func TestInit(t *testing.T) {
+	m := getTestMetadata()
+	tw := NewTwitter(logger.NewLogger("test"))
+	err := tw.Init(m)
+	assert.Nilf(t, err, "error initializing valid metadata properties")
+}
+
+func TestReadError(t *testing.T) {
+	m := getTestMetadata()
+	tw := NewTwitter(logger.NewLogger("test"))
+	err := tw.Init(m)
+	assert.Nilf(t, err, "error initializing valid metadata properties")
+
+	tw.Read(func(res *bindings.ReadResponse) error {
+		t.Logf("result: %+v", res)
+		assert.NotNilf(t, err, "no error on read with invalid credentials")
+		return nil
+	})
+}

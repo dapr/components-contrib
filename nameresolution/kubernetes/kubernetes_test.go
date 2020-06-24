b9bdebdd9343786de6c7d5e3375fe76a05277edc
Young Bu Park <youngp@microsoft.com>
2020-06-24 11:23:15 -0700

    rename servicediscovery to nameresolution pkg and refactor it (#368)
    * rename servicediscovery to nameresolution pkg
    
    * fix linter
    
    * add comment
    
    * add more logs

--- nameresolution/kubernetes/kubernetes_test.go
+++ nameresolution/kubernetes/kubernetes_test.go
@@ -8,14 +8,14 @@ package kubernetes
 import (
 	"testing"
 
-	"github.com/dapr/components-contrib/servicediscovery"
+	"github.com/dapr/components-contrib/nameresolution"
 	"github.com/dapr/dapr/pkg/logger"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestResolve(t *testing.T) {
-	resolver := NewKubernetesResolver(logger.NewLogger("test"))
-	request := servicediscovery.ResolveRequest{ID: "myid", Namespace: "abc", Port: 1234}
+	resolver := NewResolver(logger.NewLogger("test"))
+	request := nameresolution.ResolveRequest{ID: "myid", Namespace: "abc", Port: 1234}
 
 	u := "myid-dapr.abc.svc.cluster.local:1234"
 	target, err := resolver.ResolveID(request)

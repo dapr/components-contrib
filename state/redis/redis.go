cfded06385983f7f899f315bd6ade9a699670448
Shalabh Mohan Shrivastava <shalabhs@microsoft.com>
2020-08-11 10:50:31 -0700

    State Store Components changes to adhere runtime changes for removing retry options (#426)
    * State Store Components changes to adhere runtime changes for removing state retry options
    
    * resolving code review comments - common method to CheckRequestOptions

--- state/redis/redis.go
+++ state/redis/redis.go
@@ -179,11 +179,11 @@ func (r *StateStore) deleteValue(req *state.DeleteRequest) error {
 
 // Delete performs a delete operation
 func (r *StateStore) Delete(req *state.DeleteRequest) error {
-	err := state.CheckDeleteRequestOptions(req)
+	err := state.CheckRequestOptions(req.Options)
 	if err != nil {
 		return err
 	}
-	return state.DeleteWithRetries(r.deleteValue, req)
+	return state.DeleteWithOptions(r.deleteValue, req)
 }
 
 // BulkDelete performs a bulk delete operation
@@ -239,7 +239,7 @@ func (r *StateStore) Get(req *state.GetRequest) (*state.GetResponse, error) {
 }
 
 func (r *StateStore) setValue(req *state.SetRequest) error {
-	err := state.CheckSetRequestOptions(req)
+	err := state.CheckRequestOptions(req.Options)
 	if err != nil {
 		return err
 	}
@@ -277,7 +277,7 @@ func (r *StateStore) setValue(req *state.SetRequest) error {
 
 // Set saves state into redis
 func (r *StateStore) Set(req *state.SetRequest) error {
-	return state.SetWithRetries(r.setValue, req)
+	return state.SetWithOptions(r.setValue, req)
 }
 
 // BulkSet performs a bulks save operation
---
+++ state/request_options_test.go
@@ -0,0 +1,59 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package state
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TestSetRequestWithOptions is used to test request options
+func TestSetRequestWithOptions(t *testing.T) {
+	t.Run("set with default options", func(t *testing.T) {
+		counter := 0
+		SetWithOptions(func(req *SetRequest) error {
+			counter++
+			return nil
+		}, &SetRequest{})
+		assert.Equal(t, 1, counter, "should execute only once")
+	})
+
+	t.Run("set with no explicit options", func(t *testing.T) {
+		counter := 0
+		SetWithOptions(func(req *SetRequest) error {
+			counter++
+			return nil
+		}, &SetRequest{
+			Options: SetStateOption{},
+		})
+		assert.Equal(t, 1, counter, "should execute only once")
+	})
+}
+
+// TestCheckRequestOptions is used to validate request options
+func TestCheckRequestOptions(t *testing.T) {
+	t.Run("set state options", func(t *testing.T) {
+		ro := SetStateOption{Concurrency: FirstWrite, Consistency: Eventual}
+		err := CheckRequestOptions(ro)
+		assert.NoError(t, err)
+	})
+	t.Run("delete state options", func(t *testing.T) {
+		ro := DeleteStateOption{Concurrency: FirstWrite, Consistency: Eventual}
+		err := CheckRequestOptions(ro)
+		assert.NoError(t, err)
+	})
+	t.Run("get state options", func(t *testing.T) {
+		ro := GetStateOption{Consistency: Eventual}
+		err := CheckRequestOptions(ro)
+		assert.NoError(t, err)
+	})
+	t.Run("invalid state options", func(t *testing.T) {
+		ro := SetStateOption{Concurrency: "invalid", Consistency: Eventual}
+		err := CheckRequestOptions(ro)
+		assert.Error(t, err)
+	})
+}

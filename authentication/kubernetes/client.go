3787e2e48867b2095156e9f2c3e06325bd2b9d13
Mukundan Sundararajan <musundar@microsoft.com>
2020-08-18 11:49:06 -0700

    Change Kubernetes client to official go-client in the events binding. (#436)
    * Change Kubernetes client to official go-client in the events binding.
    
    * Change adding flags to init
    
    * go mod tidy
    
    * Proper call for handler
    
    * Remove commented out code
    
    * Refactor indentation

--- authentication/kubernetes/client.go
+++ authentication/kubernetes/client.go
@@ -10,22 +10,27 @@ import (
 	"path/filepath"
 
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/util/homedir"
 
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
 )
 
-// GetKubeClient returns a kubernetes client
-func GetKubeClient() (*kubernetes.Clientset, error) {
-	var kubeconfig *string
+// nolint:gochecknoglobals
+var kubeconfig *string
+
+// nolint:gochecknoinits
+func init() {
 	if home := homedir.HomeDir(); home != "" {
 		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
 	} else {
 		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
 	}
-	flag.Parse()
+}
 
+// GetKubeClient returns a kubernetes client
+func GetKubeClient() (*kubernetes.Clientset, error) {
+	flag.Parse()
 	conf, err := rest.InClusterConfig()
 	if err != nil {
 		conf, err = clientcmd.BuildConfigFromFlags("", *kubeconfig)

4907650cb592a989357cb502a42b0d2a62ff1e42
Mark Chmarny <mark@chmarny.com>
2020-09-23 06:03:03 -0700

    test sql linting

--- bindings/postgres/postgres_test.go
+++ bindings/postgres/postgres_test.go
@@ -16,6 +16,17 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const (
+	testTableDDL = `CREATE TABLE IF NOT EXISTS foo (
+		id bigint NOT NULL,
+		v1 character varying(50) NOT NULL,
+		ts TIMESTAMP)`
+	testInsert = "INSERT INTO foo (id, v1, ts) VALUES (%d, 'test-%d', '%v')"
+	testDelete = "DELETE FROM foo"
+	testUpdate = "UPDATE foo SET ts = '%v' WHERE id = %d"
+	testSelect = "SELECT * FROM foo WHERE id < 3"
+)
+
 // SETUP TESTS
 // 1. createdb daprtest
 // 2. createuser daprtest
@@ -26,9 +37,10 @@ import (
 // go test -v -count=1 ./bindings/postgres -run ^TestCRUD
 
 func TestCRUD(t *testing.T) {
+	// skip this test unless the PG DB is available
 	if os.Getenv("POSTGRES_RUN_LIVE_TEST") != "true" {
 		t.Log("POSTGRES_RUN_LIVE_TEST not set, skipping test")
-		t.SkipNow() // skip this test unless the PG DB is available
+		t.SkipNow()
 	}
 	t.Log("POSTGRES_RUN_LIVE_TEST set, running live DB test")
 
@@ -46,54 +58,42 @@ func TestCRUD(t *testing.T) {
 	assert.NoError(t, err)
 
 	// create table
-	ddl := `CREATE TABLE IF NOT EXISTS foo (
-			id bigint NOT NULL,
-			v1 character varying(50) NOT NULL,
-			ts TIMESTAMP
-		)`
-
 	req := &bindings.InvokeRequest{
 		Operation: ExecOperation,
-		Metadata:  map[string]string{CommandSQLKey: ddl},
+		Metadata:  map[string]string{CommandSQLKey: testTableDDL},
 	}
 	res, err := b.Invoke(req)
 	assertResponse(t, res, err)
 
 	// delete all previous records if any
-	req.Metadata[CommandSQLKey] = "DELETE FROM foo"
+	req.Metadata[CommandSQLKey] = testDelete
 	res, err = b.Invoke(req)
 	assertResponse(t, res, err)
 
 	// insert recrods
 	for i := 0; i < 10; i++ {
-		req.Metadata[CommandSQLKey] = fmt.Sprintf(
-			"INSERT INTO foo (id, v1, ts) VALUES (%d, 'test-%d', '%v')",
-			i, i, time.Now().Format(time.RFC3339),
-		)
+		req.Metadata[CommandSQLKey] = fmt.Sprintf(testInsert, i, i, time.Now().Format(time.RFC3339))
 		res, err = b.Invoke(req)
 		assertResponse(t, res, err)
 	}
 
 	// update recrods
 	for i := 0; i < 10; i++ {
-		req.Metadata[CommandSQLKey] = fmt.Sprintf(
-			"UPDATE foo SET ts = '%v' WHERE id = %d",
-			time.Now().Format(time.RFC3339), i,
-		)
+		req.Metadata[CommandSQLKey] = fmt.Sprintf(testUpdate, time.Now().Format(time.RFC3339), i)
 		res, err = b.Invoke(req)
 		assertResponse(t, res, err)
 	}
 
 	// select records
 	req.Operation = QueryOperation
-	req.Metadata[CommandSQLKey] = "SELECT * FROM foo WHERE id < 3"
+	req.Metadata[CommandSQLKey] = testSelect
 	res, err = b.Invoke(req)
 	assertResponse(t, res, err)
 	t.Logf("result data: %v", string(res.Data))
 
 	// delete records
 	req.Operation = ExecOperation
-	req.Metadata[CommandSQLKey] = "DELETE FROM foo"
+	req.Metadata[CommandSQLKey] = testDelete
 	res, err = b.Invoke(req)
 	assertResponse(t, res, err)
 

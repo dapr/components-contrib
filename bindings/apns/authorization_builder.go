fc479085bfca718e9516a4a5df02ac635c175bc3
Michael Collins <michael.collins@neudesic.com>
2020-09-25 12:13:49 -0700

    Refactor Code
    I refactored and cleaned up the source code in apns.go. I made the code
    easier to read and understand. I split out the JWT generation into
    authorization_builder.go. I split out the create operation code into
    create_operation.go and refactored it to follow a pipeline pattern.
    
    I created doc.go and added documentation for how to use the APNS binding
    to send push notifications to Apple devices.

---
+++ bindings/apns/authorization_builder.go
@@ -0,0 +1,63 @@
+package apns
+
+import (
+	"sync"
+	"time"
+
+	"github.com/dapr/dapr/pkg/logger"
+	"github.com/dgrijalva/jwt-go"
+)
+
+type authorizationBuilder struct {
+	logger              logger.Logger
+	mutex               sync.RWMutex
+	authorizationHeader string
+	tokenExpiresAt      time.Time
+	keyID               string
+	teamID              string
+	privateKey          interface{}
+}
+
+func (a *authorizationBuilder) getAuthorizationHeader() (string, error) {
+	authorizationHeader, ok := a.readAuthorizationHeader()
+	if ok {
+		return authorizationHeader, nil
+	}
+
+	return a.generateAuthorizationHeader()
+}
+
+func (a *authorizationBuilder) readAuthorizationHeader() (string, bool) {
+	a.mutex.RLock()
+	defer a.mutex.RUnlock()
+
+	if time.Now().After(a.tokenExpiresAt) {
+		return "", false
+	}
+
+	return a.authorizationHeader, true
+}
+
+func (a *authorizationBuilder) generateAuthorizationHeader() (string, error) {
+	a.mutex.Lock()
+	defer a.mutex.Unlock()
+
+	a.logger.Debug("Authorization token expired; generating new token")
+
+	now := time.Now()
+	claims := jwt.StandardClaims{
+		IssuedAt: now.Unix(),
+		Issuer:   a.teamID,
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+	token.Header["kid"] = a.keyID
+	signedToken, err := token.SignedString(a.privateKey)
+	if err != nil {
+		return "", err
+	}
+
+	a.authorizationHeader = "bearer " + signedToken
+	a.tokenExpiresAt = now.Add(time.Minute * 55)
+
+	return a.authorizationHeader, nil
+}
---
+++ bindings/apns/create_operation.go
@@ -0,0 +1,147 @@
+package apns
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"github.com/dapr/components-contrib/bindings"
+	"github.com/dapr/dapr/pkg/logger"
+)
+
+type createOperation struct {
+	authorizationBuilder *authorizationBuilder
+	client               *http.Client
+	logger               logger.Logger
+	req                  *bindings.InvokeRequest
+	res                  *bindings.InvokeResponse
+	err                  error
+
+	urlPrefix           string
+	deviceToken         string
+	httpRequest         *http.Request
+	httpResponse        *http.Response
+	authorizationHeader string
+}
+
+func (op *createOperation) run() (*bindings.InvokeResponse, error) {
+	op.getDeviceToken()
+	op.makeHTTPRequest()
+	op.getAuthorizationHeader()
+	op.addHTTPHeaders()
+	op.sendPushNotification()
+	op.makeSuccessResponse()
+	op.makeErrorResponse()
+	op.closeResponseBody()
+	return op.res, op.err
+}
+
+func (op *createOperation) getDeviceToken() {
+	if op.err != nil {
+		return
+	}
+
+	deviceToken, ok := op.req.Metadata[deviceTokenKey]
+	if !ok || deviceToken == "" {
+		op.err = errors.New("the device-token parameter is required")
+		return
+	}
+
+	op.deviceToken = deviceToken
+}
+
+func (op *createOperation) makeHTTPRequest() {
+	if op.err != nil {
+		return
+	}
+
+	url := op.urlPrefix + op.deviceToken
+	op.httpRequest, op.err =
+		http.NewRequest(http.MethodPost, url, bytes.NewReader(op.req.Data))
+}
+
+func (op *createOperation) getAuthorizationHeader() {
+	if op.err != nil {
+		return
+	}
+
+	op.authorizationHeader, op.err =
+		op.authorizationBuilder.getAuthorizationHeader()
+}
+
+func (op *createOperation) addHTTPHeaders() {
+	if op.err != nil {
+		return
+	}
+
+	op.httpRequest.Header.Add("authorization", op.authorizationHeader)
+	op.addRequestHeader(pushTypeKey)
+	op.addRequestHeader(messageIDKey)
+	op.addRequestHeader(expirationKey)
+	op.addRequestHeader(priorityKey)
+	op.addRequestHeader(topicKey)
+	op.addRequestHeader(collapseIDKey)
+}
+
+func (op *createOperation) addRequestHeader(key string) {
+	if value, ok := op.req.Metadata[key]; ok && value != "" {
+		op.httpRequest.Header.Add(key, value)
+	}
+}
+
+func (op *createOperation) sendPushNotification() {
+	if op.err != nil {
+		return
+	}
+
+	op.logger.Debugf("Sending notification to device %v", op.deviceToken)
+	op.httpResponse, op.err = op.client.Do(op.httpRequest)
+}
+
+func (op *createOperation) makeSuccessResponse() {
+	if op.err != nil {
+		return
+	}
+
+	if op.httpResponse.StatusCode != http.StatusOK {
+		return
+	}
+
+	messageID := op.httpResponse.Header.Get(messageIDKey)
+	output := notificationResponse{MessageID: messageID}
+	var data bytes.Buffer
+	encoder := json.NewEncoder(&data)
+	op.err = encoder.Encode(&output)
+	if op.err == nil {
+		op.res = &bindings.InvokeResponse{Data: data.Bytes()}
+	}
+}
+
+func (op *createOperation) makeErrorResponse() {
+	if op.err != nil {
+		return
+	}
+
+	hasResponse := op.httpResponse != nil
+	if hasResponse && op.httpResponse.StatusCode == http.StatusOK {
+		return
+	}
+
+	if op.httpResponse.Body == nil {
+		return
+	}
+
+	var errorResponse errorResponse
+	decoder := json.NewDecoder(op.httpResponse.Body)
+	op.err = decoder.Decode(&errorResponse)
+	if op.err == nil {
+		op.err = errors.New(errorResponse.Reason)
+	}
+}
+
+func (op *createOperation) closeResponseBody() {
+	if op.httpResponse != nil && op.httpResponse.Body != nil {
+		_ = op.httpResponse.Body.Close()
+	}
+}

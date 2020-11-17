57899b894d9c0322002e6f3b933be72ca2dc69f5
Trond Hindenes <trond@hindenes.com>
2020-11-17 13:14:38 +0100

    Remove unneeded parameter

--- pubsub/aws/snssqs/snssqs.go
+++ pubsub/aws/snssqs/snssqs.go
@@ -23,7 +23,6 @@ type snsSqs struct {
 	topicHash map[string]string
 	// key is the topic name, value holds the ARN of the queue and its url
 	queues map[string]*sqsQueueInfo
-	// awsAcctID     string
 	snsClient     *sns.SNS
 	sqsClient     *sqs.SQS
 	metadata      *snsSqsMetadata

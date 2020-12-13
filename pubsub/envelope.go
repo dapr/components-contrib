30320f5f41ad809d6f95fe15399522ec26eb13dd
AbserAri <abserari@gmail.com>
2020-12-13 19:48:13 +0800

    recover to origin version

--- pubsub/envelope.go
+++ pubsub/envelope.go
@@ -7,6 +7,7 @@ package pubsub
 
 import (
 	"github.com/google/uuid"
+	jsoniter "github.com/json-iterator/go"
 )
 
 const (
@@ -14,33 +15,30 @@ const (
 	DefaultCloudEventType = "com.dapr.event.sent"
 	// CloudEventsSpecVersion is the specversion used by Dapr for the cloud events implementation
 	CloudEventsSpecVersion = "1.0"
-	// DefaultContentType is the default Cloud Events Envelope content type.
-	DefaultContentType = "application/json"
+	// ContentType is the Cloud Events HTTP content type
+	ContentType = "application/cloudevents+json"
 	// DefaultCloudEventSource is the default event source
 	DefaultCloudEventSource = "Dapr"
+	// DefaultCloudEventDataContentType is the default content-type for the data attribute
+	DefaultCloudEventDataContentType = "text/plain"
 )
 
 // CloudEventsEnvelope describes the Dapr implementation of the Cloud Events spec
 // Spec details: https://github.com/cloudevents/spec/blob/master/spec.md
 type CloudEventsEnvelope struct {
-	ID              string `json:"id"`
-	Source          string `json:"source"`
-	Type            string `json:"type"`
-	SpecVersion     string `json:"specversion"`
-	DataContentType string `json:"datacontenttype"`
-	Data            []byte `json:"data"`
-	Subject         string `json:"subject"`
-	Topic           string `json:"topic"`
-	PubsubName      string `json:"pubsubname"`
+	ID              string      `json:"id"`
+	Source          string      `json:"source"`
+	Type            string      `json:"type"`
+	SpecVersion     string      `json:"specversion"`
+	DataContentType string      `json:"datacontenttype"`
+	Data            interface{} `json:"data"`
+	Subject         string      `json:"subject"`
+	Topic           string      `json:"topic"`
+	PubsubName      string      `json:"pubsubname"`
 }
 
-// NewDaprEventWithRequest new a event which support the CloudEvent specversion 1.0 from PublishRequest.
-func NewDaprCloudEventWithRequest(req *PublishRequest) *CloudEventsEnvelope {
-	return NewCloudEventsEnvelope("", "", "", "", "", req.Topic, req.PubsubName, req.Data)
-}
-
-// NewCloudEventsEnvelope new a CloudEventsEnvelope wrap with raw data.
-func NewCloudEventsEnvelope(id, source, eventType, contentType, subject string, topic, pubsubName string, data []byte) *CloudEventsEnvelope {
+// NewCloudEventsEnvelope returns CloudEventsEnvelope from data or a new one when data content was not
+func NewCloudEventsEnvelope(id, source, eventType, subject string, topic string, pubsubName string, dataContentType string, data []byte) *CloudEventsEnvelope {
 	// defaults
 	if id == "" {
 		id = uuid.New().String()
@@ -54,19 +52,70 @@ func NewCloudEventsEnvelope(id, source, eventType, contentType, subject string, 
 	if subject == "" {
 		subject = DefaultCloudEventSource
 	}
-	if contentType == "" {
-		contentType = DefaultContentType
+	if dataContentType == "" {
+		dataContentType = DefaultCloudEventDataContentType
 	}
 
+	// check if JSON
+	var j interface{}
+	err := jsoniter.Unmarshal(data, &j)
+	if err != nil {
+		// not JSON, return new envelope
+		return &CloudEventsEnvelope{
+			ID:              id,
+			SpecVersion:     CloudEventsSpecVersion,
+			DataContentType: dataContentType,
+			Source:          source,
+			Type:            eventType,
+			Subject:         subject,
+			Topic:           topic,
+			PubsubName:      pubsubName,
+			Data:            string(data),
+		}
+	}
+
+	// handle CloudEvent
+	m, isMap := j.(map[string]interface{})
+	if isMap {
+		if _, isCE := m["specversion"]; isCE {
+			ce := &CloudEventsEnvelope{
+				ID:              getStrVal(m, "id"),
+				SpecVersion:     getStrVal(m, "specversion"),
+				DataContentType: getStrVal(m, "datacontenttype"),
+				Source:          getStrVal(m, "source"),
+				Type:            getStrVal(m, "type"),
+				Subject:         getStrVal(m, "subject"),
+				Topic:           topic,
+				PubsubName:      pubsubName,
+				Data:            m["data"],
+			}
+			// check if CE is valid
+			if ce.ID != "" && ce.SpecVersion != "" && ce.DataContentType != "" {
+				return ce
+			}
+		}
+	}
+
+	// content was JSON but not a valid CloudEvent, make one
 	return &CloudEventsEnvelope{
 		ID:              id,
 		SpecVersion:     CloudEventsSpecVersion,
-		DataContentType: contentType,
+		DataContentType: "application/json",
 		Source:          source,
 		Type:            eventType,
 		Subject:         subject,
 		Topic:           topic,
 		PubsubName:      pubsubName,
-		Data:            data,
+		Data:            j,
 	}
+}
+
+func getStrVal(m map[string]interface{}, key string) string {
+	if v, k := m[key]; k {
+		if s, ok := v.(string); ok {
+			return s
+		}
+	}
+
+	return ""
 }
--- pubsub/envelope_test.go
+++ pubsub/envelope_test.go
@@ -6,18 +6,41 @@
 package pubsub
 
 import (
+	"encoding/json"
+	"strings"
 	"testing"
 
-	jsoniter "github.com/json-iterator/go"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestCreateCloudEventsEnvelope(t *testing.T) {
-	envelope := NewCloudEventsEnvelope("a", "source", "eventType", "", "", "", "", []byte{})
+	envelope := NewCloudEventsEnvelope("a", "source", "eventType", "", "", "", "", nil)
 	assert.NotNil(t, envelope)
 }
 
-func TestEnvelopeWrapExistingCloudEvents(t *testing.T) {
+func TestEnvelopeXML(t *testing.T) {
+	t.Run("xml content", func(t *testing.T) {
+		str := `<root/>`
+		envelope := NewCloudEventsEnvelope("a", "", "", "", "routed.topic", "mypubsub", "application/xml", []byte(str))
+		assert.Equal(t, "application/xml", envelope.DataContentType)
+		assert.Equal(t, str, envelope.Data)
+		assert.Equal(t, "1.0", envelope.SpecVersion)
+		assert.Equal(t, "routed.topic", envelope.Topic)
+		assert.Equal(t, "mypubsub", envelope.PubsubName)
+	})
+
+	t.Run("xml without content-type", func(t *testing.T) {
+		str := `<root/>`
+		envelope := NewCloudEventsEnvelope("a", "", "", "", "routed.topic", "mypubsub", "", []byte(str))
+		assert.Equal(t, "text/plain", envelope.DataContentType)
+		assert.Equal(t, str, envelope.Data)
+		assert.Equal(t, "1.0", envelope.SpecVersion)
+		assert.Equal(t, "routed.topic", envelope.Topic)
+		assert.Equal(t, "mypubsub", envelope.PubsubName)
+	})
+}
+
+func TestEnvelopeUsingExistingCloudEvents(t *testing.T) {
 	t.Run("cloud event content", func(t *testing.T) {
 		str := `{
 			"specversion" : "1.0",
@@ -31,42 +54,67 @@ func TestEnvelopeWrapExistingCloudEvents(t *testing.T) {
 			"datacontenttype" : "text/xml",
 			"data" : "<much wow=\"xml\"/>"
 		}`
-		envelope := NewCloudEventsEnvelope("a", "", "", "", "", "routed.topic", "mypubsub", []byte(str))
-		var j interface{}
-		err := jsoniter.ConfigFastest.Unmarshal(envelope.Data, &j)
-		if err != nil {
-			t.Error(err)
-		}
-		m, isMap := j.(map[string]interface{})
-		if !isMap {
-			t.Error("Unexpect Json Type")
-		}
-		assert.Equal(t, "A234-1234-1234", m["id"])
-		assert.Equal(t, "text/xml", m["datacontenttype"])
-		assert.Equal(t, "1.0", m["specversion"])
+		envelope := NewCloudEventsEnvelope("a", "", "", "", "routed.topic", "mypubsub", "", []byte(str))
+		assert.Equal(t, "A234-1234-1234", envelope.ID)
+		assert.Equal(t, "text/xml", envelope.DataContentType)
+		assert.Equal(t, "1.0", envelope.SpecVersion)
 		assert.Equal(t, "routed.topic", envelope.Topic)
 		assert.Equal(t, "mypubsub", envelope.PubsubName)
 	})
 }
 
+func TestCreateFromJSON(t *testing.T) {
+	t.Run("has JSON object", func(t *testing.T) {
+		obj1 := struct {
+			Val1 string
+			Val2 int
+		}{
+			"test",
+			1,
+		}
+		data, _ := json.Marshal(obj1)
+		envelope := NewCloudEventsEnvelope("a", "source", "", "", "", "mypubsub", "", data)
+		t.Logf("data: %v", envelope.Data)
+		assert.Equal(t, "application/json", envelope.DataContentType)
+
+		obj2 := struct {
+			Val1 string
+			Val2 int
+		}{}
+		err := json.Unmarshal(data, &obj2)
+		assert.NoError(t, err)
+		assert.Equal(t, obj1.Val1, obj2.Val1)
+		assert.Equal(t, obj1.Val2, obj2.Val2)
+	})
+}
+
 func TestCreateCloudEventsEnvelopeDefaults(t *testing.T) {
 	t.Run("default event type", func(t *testing.T) {
-		envelope := NewCloudEventsEnvelope("a", "source", "", "", "", "", "mypubsub", nil)
+		envelope := NewCloudEventsEnvelope("a", "source", "", "", "", "mypubsub", "", nil)
 		assert.Equal(t, DefaultCloudEventType, envelope.Type)
 	})
 
 	t.Run("non-default event type", func(t *testing.T) {
-		envelope := NewCloudEventsEnvelope("a", "source", "e1", "", "", "", "mypubsub", nil)
+		envelope := NewCloudEventsEnvelope("a", "source", "e1", "", "", "mypubsub", "", nil)
 		assert.Equal(t, "e1", envelope.Type)
 	})
 
 	t.Run("spec version", func(t *testing.T) {
-		envelope := NewCloudEventsEnvelope("a", "source", "", "", "", "", "mypubsub", nil)
+		envelope := NewCloudEventsEnvelope("a", "source", "", "", "", "mypubsub", "", nil)
 		assert.Equal(t, CloudEventsSpecVersion, envelope.SpecVersion)
 	})
 
+	t.Run("quoted data", func(t *testing.T) {
+		list := []string{"v1", "v2", "v3"}
+		data := strings.Join(list, ",")
+		envelope := NewCloudEventsEnvelope("a", "source", "", "", "", "mypubsub", "", []byte(data))
+		t.Logf("data: %v", envelope.Data)
+		assert.Equal(t, "text/plain", envelope.DataContentType)
+		assert.Equal(t, data, envelope.Data.(string))
+	})
+
 	t.Run("string data content type", func(t *testing.T) {
-		envelope := NewCloudEventsEnvelope("a", "source", "", "", "", "", "mypubsub", []byte("data"))
-		assert.Equal(t, "application/json", envelope.DataContentType)
+		envelope := NewCloudEventsEnvelope("a", "source", "", "", "", "mypubsub", "", []byte("data"))
+		assert.Equal(t, "text/plain", envelope.DataContentType)
 	})
 }

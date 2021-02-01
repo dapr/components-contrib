af12eb87e631ede5e93251133b423abaef2cd864
EdwinVW <edwinvw@gmail.com>
2021-02-01 21:02:03 +0100

    Add test for MergeWithRequestMetadata

--- bindings/smtp/smtp_test.go
+++ bindings/smtp/smtp_test.go
@@ -44,3 +44,40 @@ func TestParseMetadata(t *testing.T) {
 		assert.Equal(t, "Test email", smtpMeta.Subject)
 	})
 }
+
+func TestMergeWithRequestMetadata(t *testing.T) {
+	t.Run("Has merged metadata", func(t *testing.T) {
+		var smtpMeta = Metadata {
+			Host:          "mailserver.dapr.io",
+			Port:          "25",
+			User:          "user@dapr.io",
+			SkipTLSVerify: true,
+			Password:      "P@$$w0rd!",
+			EmailFrom:     "from@dapr.io",
+			EmailTo:       "to@dapr.io",
+			EmailCC:       "cc@dapr.io",
+			EmailBCC:      "bcc@dapr.io",
+			Subject:       "Test email" }
+
+		var request = bindings.InvokeRequest{}
+		request.Metadata = map[string]string{
+			"emailFrom":     "req-from@dapr.io",
+			"emailTo":       "req-to@dapr.io",
+			"emailCC":       "req-cc@dapr.io",
+			"emailBCC":      "req-bcc@dapr.io",
+			"subject":       "req-Test email"}
+
+		mergedMeta := smtpMeta.mergeWithRequestMetadata(&request)
+
+		assert.Equal(t, "mailserver.dapr.io", mergedMeta.Host)
+		assert.Equal(t, "25", mergedMeta.Port)
+		assert.Equal(t, "user@dapr.io", mergedMeta.User)
+		assert.Equal(t, "P@$$w0rd!", mergedMeta.Password)
+		assert.Equal(t, true, mergedMeta.SkipTLSVerify)
+		assert.Equal(t, "req-from@dapr.io", mergedMeta.EmailFrom)
+		assert.Equal(t, "req-to@dapr.io", mergedMeta.EmailTo)
+		assert.Equal(t, "req-cc@dapr.io", mergedMeta.EmailCC)
+		assert.Equal(t, "req-bcc@dapr.io", mergedMeta.EmailBCC)
+		assert.Equal(t, "req-Test email", mergedMeta.Subject)
+	})
+}

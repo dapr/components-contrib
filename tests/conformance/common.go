236226dc8527c206e1b8be615c81ab7cf4c2f92c
halspang <70976921+halspang@users.noreply.github.com>
2021-02-04 15:21:38 -0800

    Add Azure Service Bus Queues binding to tests (#651)
    https://github.com/dapr/components-contrib/issues/619
    
    Co-authored-by: Artur Souza <artursouza.ms@outlook.com>

--- tests/conformance/common.go
+++ tests/conformance/common.go
@@ -17,6 +17,7 @@ import (
 	"fortio.org/fortio/log"
 	"github.com/dapr/components-contrib/bindings"
 	b_azure_blobstorage "github.com/dapr/components-contrib/bindings/azure/blobstorage"
+	b_azure_servicebusqueues "github.com/dapr/components-contrib/bindings/azure/servicebusqueues"
 	b_azure_storagequeues "github.com/dapr/components-contrib/bindings/azure/storagequeues"
 	b_http "github.com/dapr/components-contrib/bindings/http"
 	b_kafka "github.com/dapr/components-contrib/bindings/kafka"
@@ -303,6 +304,8 @@ func loadOutputBindings(tc TestComponent) bindings.OutputBinding {
 		binding = b_azure_blobstorage.NewAzureBlobStorage(testLogger)
 	case "azure.storagequeues":
 		binding = b_azure_storagequeues.NewAzureStorageQueues(testLogger)
+	case "azure.servicebusqueues":
+		binding = b_azure_servicebusqueues.NewAzureServiceBusQueues(testLogger)
 	case "kafka":
 		binding = b_kafka.NewKafka(testLogger)
 	case "http":
@@ -318,6 +321,8 @@ func loadInputBindings(tc TestComponent) bindings.InputBinding {
 	var binding bindings.InputBinding
 
 	switch tc.Component {
+	case "azure.servicebusqueues":
+		binding = b_azure_servicebusqueues.NewAzureServiceBusQueues(testLogger)
 	case "azure.storagequeues":
 		binding = b_azure_storagequeues.NewAzureStorageQueues(testLogger)
 	case "kafka":

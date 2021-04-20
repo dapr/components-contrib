48e1e0a755395dd5b092afe52b4d9524b9187adb
Artur Souza <artursouza.ms@outlook.com>
2021-04-20 08:28:41 -0700

    Update dns_test.go

--- nameresolution/dns/dns_test.go
+++ nameresolution/dns/dns_test.go
@@ -9,7 +9,7 @@ import (
 	"testing"
 
 	"github.com/dapr/components-contrib/nameresolution"
-	"github.com/dapr/dapr/pkg/logger"
+	"github.com/dapr/dapr/kit/logger"
 	"github.com/stretchr/testify/assert"
 )
 

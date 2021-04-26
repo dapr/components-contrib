13178fe26cccb266a1f002c4ebec780a38f46104
Daniel Beall <10102694+kdbeall@users.noreply.github.com>
2021-04-26 00:19:07 +0000

    Fix linter errors.

--- state/bpf/bpf.go
+++ state/bpf/bpf.go
@@ -15,7 +15,7 @@ import (
 
 	"github.com/cilium/ebpf"
 	"github.com/dapr/components-contrib/state"
-	"github.com/dapr/dapr/pkg/logger"
+	"github.com/dapr/kit/logger"
 	jsoniter "github.com/json-iterator/go"
 )
 
@@ -23,23 +23,26 @@ const (
 	fileName = "FileName"
 )
 
-type BPFMap struct {
+type Map struct {
+	state.DefaultBulkStore
 	client *ebpf.Map
 	json   jsoniter.API
 	logger logger.Logger
 }
 
 // NewBPFStateStore creates a new instance of BFP map state store
-func NewBPFStateStore(logger logger.Logger) *BPFMap {
-	s := &BPFMap{
+func NewBPFStateStore(logger logger.Logger) *Map {
+	s := &Map{
 		json:   jsoniter.ConfigFastest,
 		logger: logger,
 	}
+	s.DefaultBulkStore = state.NewDefaultBulkStore(s)
+
 	return s
 }
 
 // Init initializes the BPF map state store
-func (m *BPFMap) Init(metadata state.Metadata) error {
+func (m *Map) Init(metadata state.Metadata) error {
 	if metadata.Properties[fileName] == "" {
 		return errors.New("ebpf map error: FileName is missing")
 	}
@@ -48,42 +51,40 @@ func (m *BPFMap) Init(metadata state.Metadata) error {
 		return err
 	}
 	m.client = newClient
+
 	return nil
 }
 
 // Features returns the features available in this state store
-func (m *BPFMap) Features() []state.Feature {
+func (m *Map) Features() []state.Feature {
 	return nil
 }
 
 // Delete removes an entity from the store
-func (m *BPFMap) Delete(req *state.DeleteRequest) error {
+func (m *Map) Delete(req *state.DeleteRequest) error {
 	return fmt.Errorf("delete operation not supported")
 }
 
 // Get returns an entity from store
-func (m *BPFMap) Get(req *state.GetRequest) (*state.GetResponse, error) {
+func (m *Map) Get(req *state.GetRequest) (*state.GetResponse, error) {
 	key, _ := strconv.ParseUint(req.Key, 10, 0)
-	var bt []byte = make([]byte, m.client.ValueSize())
+	var bt = make([]byte, m.client.ValueSize())
 	err := m.client.Lookup(uint32(key), bt)
 	if err != nil {
 		return nil, err
 	}
+
 	return &state.GetResponse{
 		Data: bt,
 	}, nil
 }
 
 // Set adds/updates an entity on store
-func (m *BPFMap) Set(req *state.SetRequest) error {
+func (m *Map) Set(req *state.SetRequest) error {
 	return fmt.Errorf("set operation not supported")
 }
 
 // Close implements io.Closer
-func (m *BPFMap) Close() error {
-	err := m.client.Close()
-	if err != nil {
-		return err
-	}
-	return nil
+func (m *Map) Close() error {
+	return m.client.Close()
 }
--- state/bpf/bpf_test.go
+++ state/bpf/bpf_test.go
@@ -11,7 +11,7 @@ import (
 )
 
 func TestBpfReadWrite(t *testing.T) {
-	t.Run("store a string and pin to file", func(t *testing.T) {
+	t.Run("store a string and read it", func(t *testing.T) {
 		expected := "foo bar qaz"
 		m, err := ebpf.NewMap(&ebpf.MapSpec{
 			Type:       ebpf.Hash,
@@ -24,20 +24,7 @@ func TestBpfReadWrite(t *testing.T) {
 		if err := m.Put(uint32(0), expected); err != nil {
 			t.Fatal("Can't put:", err)
 		}
-		var bt []byte = make([]byte, len(expected))
-		if err := m.Lookup(uint32(0), &bt); err != nil {
-			t.Fatal("Can't lookup 0:", err)
-		}
-		actual := string(bt)
-		assert.Equal(t, expected, actual)
-		m.Pin("/sys/fs/bpf/foo")
-	})
-	t.Run("load and read from a pinned file", func(t *testing.T) {
-		expected := "foo bar qaz"
-		m, err := ebpf.LoadPinnedMap("/sys/fs/bpf/foo", &ebpf.LoadPinOptions{ReadOnly: true})
-		assert.Equal(t, nil, err)
-		defer m.Close()
-		var bt []byte = make([]byte, len(expected))
+		var bt = make([]byte, len(expected))
 		if err := m.Lookup(uint32(0), &bt); err != nil {
 			t.Fatal("Can't lookup 0:", err)
 		}

e8313db653957510ab5501ee065499b0379824bd
Виктор Борисов <viktor@repoint.io>
2021-05-13 19:01:52 +0700

    extend aws s3 binding: - get - delete - copy

--- bindings/aws/s3/s3.go
+++ bindings/aws/s3/s3.go
@@ -8,8 +8,11 @@ package s3
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"net/url"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	aws_auth "github.com/dapr/components-contrib/authentication/aws"
 	"github.com/dapr/components-contrib/bindings"
@@ -17,11 +20,18 @@ import (
 	"github.com/google/uuid"
 )
 
+const (
+	CopyOperation    bindings.OperationKind = "copy"
+)
+
+
 // AWSS3 is a binding for an AWS S3 storage bucket
 type AWSS3 struct {
-	metadata *s3Metadata
-	uploader *s3manager.Uploader
-	logger   logger.Logger
+	metadata   *s3Metadata
+	uploader   *s3manager.Uploader
+	downloader *s3manager.Downloader
+	svc        *s3.S3
+	logger     logger.Logger
 }
 
 type s3Metadata struct {
@@ -44,37 +54,83 @@ func (s *AWSS3) Init(metadata bindings.Metadata) error {
 	if err != nil {
 		return err
 	}
-	uploader, err := s.getClient(m)
+	uploader, downloader, svc, err := s.getClient(m)
 	if err != nil {
 		return err
 	}
 	s.metadata = m
 	s.uploader = uploader
+	s.downloader = downloader
+	s.svc = svc
 
 	return nil
 }
 
 func (s *AWSS3) Operations() []bindings.OperationKind {
-	return []bindings.OperationKind{bindings.CreateOperation}
+	return []bindings.OperationKind{bindings.CreateOperation, bindings.GetOperation, bindings.DeleteOperation, CopyOperation}
 }
 
 func (s *AWSS3) Invoke(req *bindings.InvokeRequest) (*bindings.InvokeResponse, error) {
-	key := ""
-	if val, ok := req.Metadata["key"]; ok && val != "" {
-		key = val
-	} else {
-		key = uuid.New().String()
-		s.logger.Debugf("key not found. generating key %s", key)
+	key := s.getKey(req)
+	
+	switch op := req.Operation; op {
+	case bindings.CreateOperation:
+		r := bytes.NewReader(req.Data)
+		_, err := s.uploader.Upload(&s3manager.UploadInput{
+			Bucket: aws.String(s.metadata.Bucket),
+			Key:    aws.String(key),
+			Body:   r,
+		})
+		if (err != nil) {
+			s.logger.Errorf("s3 CREATE operation exceptions: %s", err)
+		}
+		return nil, err
+
+	case bindings.GetOperation:
+		buff := &aws.WriteAtBuffer{}
+		_, err := s.downloader.Download(buff, &s3.GetObjectInput{
+			Bucket: aws.String(s.metadata.Bucket),
+			Key:    aws.String(key),
+		})
+		resp := (*bindings.InvokeResponse) (nil)
+		if (err == nil) {
+			resp = &bindings.InvokeResponse{
+				Data: buff.Bytes(),
+			}
+		}  else {
+			s.logger.Errorf("s3 GET operation exceptions: %s", err)
+		}
+		return resp, err
+		
+	case bindings.DeleteOperation:
+		_, err := s.svc.DeleteObject(&s3.DeleteObjectInput{
+			Bucket: aws.String(s.metadata.Bucket),
+			Key:    aws.String(key),
+		})
+		if (err != nil) {
+			s.logger.Errorf("s3 DELETE operation exceptions: %s", err)
+		}
+		return nil, err
+
+	case CopyOperation:
+		source, err := s.getReqParam(req, "source")
+		if (err != nil) {
+			return nil, err
+		}
+
+		_, err = s.svc.CopyObject(&s3.CopyObjectInput{
+
+			CopySource: aws.String(url.PathEscape(source)),
+			Bucket: aws.String(s.metadata.Bucket),
+			Key: aws.String(key),
+		})
+		if (err != nil) {
+			s.logger.Errorf("s3 COPY operation exceptions: %s", err)
+		}
+		return nil, err
 	}
 
-	r := bytes.NewReader(req.Data)
-	_, err := s.uploader.Upload(&s3manager.UploadInput{
-		Bucket: aws.String(s.metadata.Bucket),
-		Key:    aws.String(key),
-		Body:   r,
-	})
-
-	return nil, err
+	return nil, nil
 }
 
 func (s *AWSS3) parseMetadata(metadata bindings.Metadata) (*s3Metadata, error) {
@@ -92,13 +148,40 @@ func (s *AWSS3) parseMetadata(metadata bindings.Metadata) (*s3Metadata, error) {
 	return &m, nil
 }
 
-func (s *AWSS3) getClient(metadata *s3Metadata) (*s3manager.Uploader, error) {
+func (s *AWSS3) getReqParam(req *bindings.InvokeRequest, paramName string) (string, error) {
+	value := ""
+	err := (error)(nil)
+
+	if val, ok := req.Metadata[paramName]; ok && val != "" {
+		value = val
+	} else {
+		err = fmt.Errorf("param %s is required", paramName)
+	}
+
+	return value, err
+}
+
+func (s *AWSS3) getKey(req *bindings.InvokeRequest) (string) {
+	key := ""
+	if val, ok := req.Metadata["key"]; ok && val != "" {
+		key = val
+	} else {
+		key = uuid.New().String()
+		s.logger.Debugf("key not found. generating key %s", key)
+	}
+
+	return key
+}
+
+func (s *AWSS3) getClient(metadata *s3Metadata) (*s3manager.Uploader, *s3manager.Downloader,  *s3.S3, error) {
 	sess, err := aws_auth.GetClient(metadata.AccessKey, metadata.SecretKey, metadata.SessionToken, metadata.Region, metadata.Endpoint)
 	if err != nil {
-		return nil, err
+		return nil, nil, nil, err
 	}
 
 	uploader := s3manager.NewUploader(sess)
+	downloader := s3manager.NewDownloader(sess)
+	svc := s3.New(sess)
 
-	return uploader, nil
+	return uploader, downloader, svc, nil
 }
--- go.mod
+++ go.mod
@@ -26,7 +26,7 @@ require (
 	github.com/aliyun/aliyun-oss-go-sdk v2.0.7+incompatible
 	github.com/apache/pulsar-client-go v0.1.0
 	github.com/apache/thrift v0.14.0 // indirect
-	github.com/aws/aws-sdk-go v1.27.0
+	github.com/aws/aws-sdk-go v1.38.39
 	github.com/baiyubin/aliyun-sts-go-sdk v0.0.0-20180326062324-cfa1a18b161f // indirect
 	github.com/bradfitz/gomemcache v0.0.0-20190913173617-a41fca850d0b
 	github.com/cenkalti/backoff/v4 v4.1.0

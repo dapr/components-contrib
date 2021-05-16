33292d6a66049d2ef372a5cb794ea9f26af38deb
Chanyong Moon <dev.chanyongmoon@gmail.com>
2021-05-16 21:33:22 +0900

    add `Ping` implementation of blobstorage

--- state/azure/blobstorage/blobstorage.go
+++ state/azure/blobstorage/blobstorage.go
@@ -139,6 +139,12 @@ func (r *StateStore) Set(req *state.SetRequest) error {
 }
 
 func (r *StateStore) Ping() error {
+	accessConditions := azblob.BlobAccessConditions{}
+
+	if _, err := r.containerURL.GetProperties(context.Background(), accessConditions.LeaseAccessConditions); err != nil {
+		return fmt.Errorf("Blob sotrage: error connecting to Blob storage at %s: %s", r.containerURL.URL().Host, err)
+	}
+
 	return nil
 }
 

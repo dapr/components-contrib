76f36d42d75f6bffac3321428bdc00f9d22abbaa
Phil Kedy <phil.kedy@gmail.com>
2021-06-09 17:59:22 -0400

    Resolving gofumpt issues

--- state/sqlserver/sqlserver_integration_test.go
+++ state/sqlserver/sqlserver_integration_test.go
@@ -209,8 +209,7 @@ func (n *numbericKeyGenerator) NextKey() string {
 	return strconv.Itoa(int(val))
 }
 
-type uuidKeyGenerator struct {
-}
+type uuidKeyGenerator struct{}
 
 func (n uuidKeyGenerator) NextKey() string {
 	return uuid.New().String()

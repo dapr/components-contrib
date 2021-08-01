33acbb99220c783b88966ef1139581859765a7cf
周全 <946778530@qq.com>
2021-08-01 17:05:24 +0800

    [add] rocketmq pubsub

--- pubsub/rocketmq/metadata.go
+++ pubsub/rocketmq/metadata.go
@@ -1,8 +1,29 @@
 package rocketmq
 
+import "github.com/dapr/components-contrib/pubsub"
+
+const (
+	metadataRocketmqTag           = "rocketmq-tag"
+	metadataRocketmqKey           = "rocketmq-key"
+	metadataRocketmqConsumerGroup = "rocketmq-consumerGroup"
+	metadataRocketmqType          = "rocketmq-sub-type"
+	metadataRocketmqExpression    = "rocketmq-sub-expression"
+	metadataRocketmqBrokerName    = "rocketmq-broker-name"
+)
+
 type rocketMQMetaData struct {
-	Resolvers  []string
 	RetryTimes int
 	AccessKey  string
 	SecretKey  string
+	NameServer string
+}
+
+func parseRocketMQMetaData(metadata pubsub.Metadata) *rocketMQMetaData {
+	rmq := &rocketMQMetaData{
+		RetryTimes: 0,
+		AccessKey:  "",
+		SecretKey:  "",
+	}
+
+	return rmq
 }

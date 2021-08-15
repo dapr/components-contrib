6bb88153c26bb60722fa95b9d9c0114ad0d471bc
Javier Vela <fjvela@gmail.com>
2021-08-15 08:21:41 +0000

    S3 update create operation: support upload files in base64, return location and versionId on create operation

--- bindings/aws/s3/s3.go
+++ bindings/aws/s3/s3.go
@@ -7,7 +7,10 @@ package s3
 
 import (
 	"bytes"
+	b64 "encoding/base64"
 	"encoding/json"
+	"fmt"
+	"strconv"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
@@ -15,6 +18,11 @@ import (
 	"github.com/dapr/components-contrib/bindings"
 	"github.com/dapr/kit/logger"
 	"github.com/google/uuid"
+)
+
+const (
+	metadataDecodeBase64 = "decodeBase64"
+	metadataKey          = "key"
 )
 
 // AWSS3 is a binding for an AWS S3 storage bucket
@@ -31,6 +39,12 @@ type s3Metadata struct {
 	SecretKey    string `json:"secretKey"`
 	SessionToken string `json:"sessionToken"`
 	Bucket       string `json:"bucket"`
+	DecodeBase64 bool   `json:"decodeBase64,string"`
+}
+
+type createResponse struct {
+	Location  string  `json:"Location"`
+	VersionID *string `json:"VersionID"`
 }
 
 // NewAWSS3 returns a new AWSS3 instance
@@ -59,22 +73,56 @@ func (s *AWSS3) Operations() []bindings.OperationKind {
 }
 
 func (s *AWSS3) Invoke(req *bindings.InvokeRequest) (*bindings.InvokeResponse, error) {
+
+	metadata, err := s.metadata.mergeWithRequestMetadata(req)
+
+	if err != nil {
+		return nil, fmt.Errorf("s3 binding error. error merge metadata : %w", err)
+	}
 	key := ""
-	if val, ok := req.Metadata["key"]; ok && val != "" {
+	if val, ok := req.Metadata[metadataKey]; ok && val != "" {
 		key = val
 	} else {
 		key = uuid.New().String()
 		s.logger.Debugf("key not found. generating key %s", key)
 	}
 
+	d, err := strconv.Unquote(string(req.Data))
+	if err == nil {
+		req.Data = []byte(d)
+	}
+
+	if metadata.DecodeBase64 {
+		decoded, decodeError := b64.StdEncoding.DecodeString(string(req.Data))
+		if decodeError != nil {
+			return nil, fmt.Errorf("s3 binding error. decode : %w", decodeError)
+		}
+		req.Data = decoded
+	}
+
 	r := bytes.NewReader(req.Data)
-	_, err := s.uploader.Upload(&s3manager.UploadInput{
-		Bucket: aws.String(s.metadata.Bucket),
+
+	resultUpload, err := s.uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(metadata.Bucket),
 		Key:    aws.String(key),
 		Body:   r,
 	})
 
-	return nil, err
+	if err != nil {
+		return nil, fmt.Errorf("s3 binding error. Uploading: %w", err)
+	}
+
+	jsonResponse, err := json.Marshal(createResponse{
+		Location:  resultUpload.Location,
+		VersionID: resultUpload.VersionID,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("s3 binding error. Error marshalling create response: %w", err)
+	}
+
+	return &bindings.InvokeResponse{
+		Data: jsonResponse,
+	}, nil
 }
 
 func (s *AWSS3) parseMetadata(metadata bindings.Metadata) (*s3Metadata, error) {
@@ -101,4 +149,20 @@ func (s *AWSS3) getClient(metadata *s3Metadata) (*s3manager.Uploader, error) {
 	uploader := s3manager.NewUploader(sess)
 
 	return uploader, nil
+}
+
+// Helper to merge config and request metadata
+func (metadata s3Metadata) mergeWithRequestMetadata(req *bindings.InvokeRequest) (s3Metadata, error) {
+	merged := metadata
+
+	if val, ok := req.Metadata[metadataDecodeBase64]; ok && val != "" {
+		valBool, err := strconv.ParseBool(val)
+		if err != nil {
+			return merged, err
+		} else {
+			merged.DecodeBase64 = valBool
+		}
+	}
+
+	return merged, nil
 }
--- bindings/aws/s3/s3_test.go
+++ bindings/aws/s3/s3_test.go
@@ -13,17 +13,85 @@ import (
 )
 
 func TestParseMetadata(t *testing.T) {
-	m := bindings.Metadata{}
-	m.Properties = map[string]string{
-		"AccessKey": "key", "Region": "region", "SecretKey": "secret", "Bucket": "test", "Endpoint": "endpoint", "SessionToken": "token",
-	}
-	s3 := AWSS3{}
-	meta, err := s3.parseMetadata(m)
-	assert.Nil(t, err)
-	assert.Equal(t, "key", meta.AccessKey)
-	assert.Equal(t, "region", meta.Region)
-	assert.Equal(t, "secret", meta.SecretKey)
-	assert.Equal(t, "test", meta.Bucket)
-	assert.Equal(t, "endpoint", meta.Endpoint)
-	assert.Equal(t, "token", meta.SessionToken)
+	t.Run("Has correct metadata", func(t *testing.T) {
+
+		m := bindings.Metadata{}
+		m.Properties = map[string]string{
+			"AccessKey": "key", "Region": "region", "SecretKey": "secret", "Bucket": "test", "Endpoint": "endpoint", "SessionToken": "token",
+		}
+		s3 := AWSS3{}
+		meta, err := s3.parseMetadata(m)
+		assert.Nil(t, err)
+		assert.Equal(t, "key", meta.AccessKey)
+		assert.Equal(t, "region", meta.Region)
+		assert.Equal(t, "secret", meta.SecretKey)
+		assert.Equal(t, "test", meta.Bucket)
+		assert.Equal(t, "endpoint", meta.Endpoint)
+		assert.Equal(t, "token", meta.SessionToken)
+	})
+}
+func TestMergeWithRequestMetadata(t *testing.T) {
+	t.Run("Has merged metadata", func(t *testing.T) {
+
+		m := bindings.Metadata{}
+		m.Properties = map[string]string{
+			"AccessKey": "key", "Region": "region", "SecretKey": "secret", "Bucket": "test", "Endpoint": "endpoint", "SessionToken": "token",
+		}
+		s3 := AWSS3{}
+		meta, err := s3.parseMetadata(m)
+		assert.Nil(t, err)
+		assert.Equal(t, "key", meta.AccessKey)
+		assert.Equal(t, "region", meta.Region)
+		assert.Equal(t, "secret", meta.SecretKey)
+		assert.Equal(t, "test", meta.Bucket)
+		assert.Equal(t, "endpoint", meta.Endpoint)
+		assert.Equal(t, "token", meta.SessionToken)
+
+		request := bindings.InvokeRequest{}
+		request.Metadata = map[string]string{
+			"decodeBase64": "true",
+		}
+
+		mergedMeta, err := meta.mergeWithRequestMetadata(&request)
+
+		assert.Nil(t, err)
+
+		assert.Nil(t, err)
+		assert.Equal(t, "key", mergedMeta.AccessKey)
+		assert.Equal(t, "region", mergedMeta.Region)
+		assert.Equal(t, "secret", mergedMeta.SecretKey)
+		assert.Equal(t, "test", mergedMeta.Bucket)
+		assert.Equal(t, "endpoint", mergedMeta.Endpoint)
+		assert.Equal(t, "token", mergedMeta.SessionToken)
+		assert.Equal(t, true, mergedMeta.DecodeBase64)
+
+	})
+
+	t.Run("Has invalid merged metadata", func(t *testing.T) {
+
+		m := bindings.Metadata{}
+		m.Properties = map[string]string{
+			"AccessKey": "key", "Region": "region", "SecretKey": "secret", "Bucket": "test", "Endpoint": "endpoint", "SessionToken": "token",
+		}
+		s3 := AWSS3{}
+		meta, err := s3.parseMetadata(m)
+		assert.Nil(t, err)
+		assert.Equal(t, "key", meta.AccessKey)
+		assert.Equal(t, "region", meta.Region)
+		assert.Equal(t, "secret", meta.SecretKey)
+		assert.Equal(t, "test", meta.Bucket)
+		assert.Equal(t, "endpoint", meta.Endpoint)
+		assert.Equal(t, "token", meta.SessionToken)
+
+		request := bindings.InvokeRequest{}
+		request.Metadata = map[string]string{
+			"decodeBase64": "hello",
+		}
+
+		mergedMeta, err := meta.mergeWithRequestMetadata(&request)
+
+		assert.NotNil(t, err)
+		assert.NotNil(t, mergedMeta)
+
+	})
 }

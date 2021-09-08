f0d7abaa4f7980929d1fb59e37080bdd2c667b87
Javier Vela <fjvela@gmail.com>
2021-09-08 14:48:52 +0000

    gcp bucket update create operation: support upload files in base64

--- bindings/gcp/bucket/bucket.go
+++ bindings/gcp/bucket/bucket.go
@@ -7,7 +7,10 @@ package bucket
 
 import (
 	"context"
+	b64 "encoding/base64"
 	"encoding/json"
+	"fmt"
+	"strconv"
 
 	"cloud.google.com/go/storage"
 	"github.com/dapr/components-contrib/bindings"
@@ -16,9 +19,14 @@ import (
 	"google.golang.org/api/option"
 )
 
+const (
+	metadataDecodeBase64 = "decodeBase64"
+	metadataKey          = "key"
+)
+
 // GCPStorage allows saving data to GCP bucket storage
 type GCPStorage struct {
-	metadata gcpMetadata
+	metadata *gcpMetadata
 	client   *storage.Client
 	logger   logger.Logger
 }
@@ -35,6 +43,12 @@ type gcpMetadata struct {
 	TokenURI            string `json:"token_uri"`
 	AuthProviderCertURL string `json:"auth_provider_x509_cert_url"`
 	ClientCertURL       string `json:"client_x509_cert_url"`
+	DecodeBase64        bool   `json:"decodeBase64,string"`
+}
+
+type createResponse struct {
+	Location  string  `json:"Location"`
+	VersionID *string `json:"VersionID"`
 }
 
 // NewGCPStorage returns a new GCP storage instance
@@ -44,16 +58,12 @@ func NewGCPStorage(logger logger.Logger) *GCPStorage {
 
 // Init performs connection parsing
 func (g *GCPStorage) Init(metadata bindings.Metadata) error {
-	b, err := g.parseMetadata(metadata)
+
+	m, b, err := g.parseMetadata(metadata)
 	if err != nil {
 		return err
 	}
 
-	var gm gcpMetadata
-	err = json.Unmarshal(b, &gm)
-	if err != nil {
-		return err
-	}
 	clientOptions := option.WithCredentialsJSON(b)
 	ctx := context.Background()
 	client, err := storage.NewClient(ctx, clientOptions)
@@ -61,19 +71,25 @@ func (g *GCPStorage) Init(metadata bindings.Metadata) error {
 		return err
 	}
 
-	g.metadata = gm
+	g.metadata = m
 	g.client = client
 
 	return nil
 }
 
-func (g *GCPStorage) parseMetadata(metadata bindings.Metadata) ([]byte, error) {
+func (g *GCPStorage) parseMetadata(metadata bindings.Metadata) (*gcpMetadata, []byte, error) {
 	b, err := json.Marshal(metadata.Properties)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	return b, nil
+	var m gcpMetadata
+	err = json.Unmarshal(b, &m)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return &m, b, nil
 }
 
 func (g *GCPStorage) Operations() []bindings.OperationKind {
@@ -81,8 +97,14 @@ func (g *GCPStorage) Operations() []bindings.OperationKind {
 }
 
 func (g *GCPStorage) Invoke(req *bindings.InvokeRequest) (*bindings.InvokeResponse, error) {
+	metadata, err := g.metadata.mergeWithRequestMetadata(req)
+
+	if err != nil {
+		return nil, fmt.Errorf("gcp bucket binding error. error merge metadata : %w", err)
+	}
+
 	var name string
-	if val, ok := req.Metadata["name"]; ok && val != "" {
+	if val, ok := req.Metadata[metadataKey]; ok && val != "" {
 		name = val
 	} else {
 		id, err := uuid.NewRandom()
@@ -91,10 +113,23 @@ func (g *GCPStorage) Invoke(req *bindings.InvokeRequest) (*bindings.InvokeRespon
 		}
 		name = id.String()
 	}
+	d, err := strconv.Unquote(string(req.Data))
+	if err == nil {
+		req.Data = []byte(d)
+	}
+
+	if metadata.DecodeBase64 {
+		decoded, decodeError := b64.StdEncoding.DecodeString(string(req.Data))
+		if decodeError != nil {
+			return nil, fmt.Errorf("gcp bucket binding error. decode : %w", decodeError)
+		}
+		req.Data = decoded
+	}
+
 	h := g.client.Bucket(g.metadata.Bucket).Object(name).NewWriter(context.Background())
 	defer h.Close()
 	if _, err := h.Write(req.Data); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("gcp bucket binding error. Uploading: %w", err)
 	}
 
 	return nil, nil
@@ -102,4 +137,20 @@ func (g *GCPStorage) Invoke(req *bindings.InvokeRequest) (*bindings.InvokeRespon
 
 func (g *GCPStorage) Close() error {
 	return g.client.Close()
+}
+
+// Helper to merge config and request metadata
+func (metadata gcpMetadata) mergeWithRequestMetadata(req *bindings.InvokeRequest) (gcpMetadata, error) {
+	merged := metadata
+
+	if val, ok := req.Metadata[metadataDecodeBase64]; ok && val != "" {
+		valBool, err := strconv.ParseBool(val)
+		if err != nil {
+			return merged, err
+		} else {
+			merged.DecodeBase64 = valBool
+		}
+	}
+
+	return merged, nil
 }

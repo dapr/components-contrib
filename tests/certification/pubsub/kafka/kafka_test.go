02a41ab3f9a187857b645d6eb72ffa034cac70bd
Scott Hussey <sthussey@gmail.com>
2021-09-21 21:17:22 -0500

    parent dbb18e4d77fda64661277afc38963a9cf3994ed7 author Scott Hussey <sthussey@gmail.com> 1632277042 -0500 committer Scott Hussey <sthussey@gmail.com> 1639111222 -0600
    Support Oauth2 authentication for Kafka
    
    - Utilize the SASL OAUTHBEARER mechanism to support
      the Oauth2 client_credentials flow for Kafka
      authentication
    
    - Deprecate `authRequired` field and introduce `authType`
      to support varied authentication mechanisms
    
    - Add a metadata upgrade mechanism to support backwards
      compatability
    
    - Recommend broker specific scopes to guard against a
      compromised broker replaying a token
    
    Signed-off-by: Scott Hussey <sthussey@gmail.com>

--- tests/certification/pubsub/kafka/kafka_test.go
+++ tests/certification/pubsub/kafka/kafka_test.go
@@ -7,7 +7,9 @@ package kafka_test
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
+	"net/http"
 	"testing"
 	"time"
 
@@ -60,10 +62,14 @@ const (
 	topicName  = "neworder"
 )
 
-var brokers = []string{"localhost:19092", "localhost:29092", "localhost:39092"}
+var (
+	brokers          = []string{"localhost:19092", "localhost:29092", "localhost:39092"}
+	oauthClientQuery = "https://localhost:4444/clients/dapr"
+)
 
 func TestKafka(t *testing.T) {
 	log := logger.NewLogger("dapr.components")
+	// log.SetOutputLevel(logger.DebugLevel)
 	component := pubsub_loader.New("kafka", func() pubsub.PubSub {
 		return pubsub_kafka.NewKafka(log)
 	})
@@ -230,6 +236,24 @@ func TestKafka(t *testing.T) {
 
 			return err
 		})).
+		Step("wait for Dapr OAuth client", retry.Do(10*time.Second, 6, func(ctx flow.Context) error {
+			httpClient := &http.Client{
+				Transport: &http.Transport{
+					TLSClientConfig: &tls.Config{
+						InsecureSkipVerify: true, // test server certificate is not trusted.
+					},
+				},
+			}
+
+			resp, err := httpClient.Get(oauthClientQuery)
+			if err != nil {
+				return err
+			}
+			if resp.StatusCode != 200 {
+				return fmt.Errorf("oauth client query for 'dapr' not successful")
+			}
+			return nil
+		})).
 		//
 		// Run the application logic above.
 		Step(app.Run(appID1, fmt.Sprintf(":%d", appPort),
@@ -249,7 +273,7 @@ func TestKafka(t *testing.T) {
 		//
 		// Run the Dapr sidecar with the Kafka component.
 		Step(sidecar.Run(sidecarName2,
-			embedded.WithComponentsPath("./components/consumer2"),
+			embedded.WithComponentsPath("./components/mtls-consumer"),
 			embedded.WithAppProtocol(runtime.HTTPProtocol, appPort+portOffset),
 			embedded.WithDaprGRPCPort(runtime.DefaultDaprAPIGRPCPort+portOffset),
 			embedded.WithDaprHTTPPort(runtime.DefaultDaprHTTPPort+portOffset),
@@ -266,7 +290,7 @@ func TestKafka(t *testing.T) {
 		//
 		// Run the Dapr sidecar with the Kafka component.
 		Step(sidecar.Run(sidecarName3,
-			embedded.WithComponentsPath("./components/consumer2"),
+			embedded.WithComponentsPath("./components/oauth-consumer"),
 			embedded.WithAppProtocol(runtime.HTTPProtocol, appPort+portOffset*2),
 			embedded.WithDaprGRPCPort(runtime.DefaultDaprAPIGRPCPort+portOffset*2),
 			embedded.WithDaprHTTPPort(runtime.DefaultDaprHTTPPort+portOffset*2),

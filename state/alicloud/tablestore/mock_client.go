6475d3ced24891922e6ccbf3c9142aeb0eff461b
TheoneFx <chenxilzx1@gmail.com>
2021-09-28 08:27:20 +0800

    Add state alicloud.tablestore (#1116)
    * state alicloud.tablestore
    
    * some bug fix
    
    * gofumpt
    
    * add unit test
    
    * gofumpt
    
    * golangci-lint
    
    * code refactor
    
    * goimports
    
    * goimports
    
    Co-authored-by: Simon Leet <31784195+CodeMonkeyLeet@users.noreply.github.com>
    Co-authored-by: Long Dai <long0dai@foxmail.com>
    Co-authored-by: Long Dai <long.dai@intel.com>
    Co-authored-by: Artur Souza <artursouza.ms@outlook.com>
    Signed-off-by: 1046102779 <seachen@tencent.com>

---
+++ state/alicloud/tablestore/mock_client.go
@@ -0,0 +1,166 @@
+package tablestore
+
+import (
+	"bytes"
+	"encoding/binary"
+
+	"github.com/aliyun/aliyun-tablestore-go-sdk/tablestore"
+)
+
+type mockClient struct {
+	tablestore.TableStoreClient
+
+	data map[string][]byte
+}
+
+func (m *mockClient) DeleteRow(request *tablestore.DeleteRowRequest) (*tablestore.DeleteRowResponse, error) {
+	var key string
+	for _, col := range request.DeleteRowChange.PrimaryKey.PrimaryKeys {
+		if col.ColumnName == stateKey {
+			key = col.Value.(string)
+
+			break
+		}
+	}
+
+	delete(m.data, key)
+
+	return nil, nil
+}
+
+func (m *mockClient) GetRow(request *tablestore.GetRowRequest) (*tablestore.GetRowResponse, error) {
+	var key string
+	for _, col := range request.SingleRowQueryCriteria.PrimaryKey.PrimaryKeys {
+		if col.ColumnName == stateKey {
+			key = col.Value.(string)
+
+			break
+		}
+	}
+
+	val := m.data[key]
+
+	resp := &tablestore.GetRowResponse{
+		Columns: []*tablestore.AttributeColumn{{
+			ColumnName: stateValue,
+			Value:      val,
+		}},
+	}
+
+	return resp, nil
+}
+
+func (m *mockClient) UpdateRow(req *tablestore.UpdateRowRequest) (*tablestore.UpdateRowResponse, error) {
+	change := req.UpdateRowChange
+
+	var val []byte
+	var key string
+
+	for _, col := range change.PrimaryKey.PrimaryKeys {
+		if col.ColumnName == stateKey {
+			key = col.Value.(string)
+
+			break
+		}
+	}
+
+	for _, col := range change.Columns {
+		if col.ColumnName == stateValue {
+			buf := &bytes.Buffer{}
+			binary.Write(buf, binary.BigEndian, col.Value)
+			val = buf.Bytes()
+
+			break
+		}
+	}
+
+	m.data[key] = val
+
+	return nil, nil
+}
+
+func (m *mockClient) BatchGetRow(request *tablestore.BatchGetRowRequest) (*tablestore.BatchGetRowResponse, error) {
+	resp := &tablestore.BatchGetRowResponse{
+		TableToRowsResult: map[string][]tablestore.RowResult{},
+	}
+
+	for _, criteria := range request.MultiRowQueryCriteria {
+		tableRes := resp.TableToRowsResult[criteria.TableName]
+		if tableRes == nil {
+			tableRes = []tablestore.RowResult{}
+		}
+		for _, keys := range criteria.PrimaryKey {
+			for _, key := range keys.PrimaryKeys {
+				if key.ColumnName == stateKey {
+					pk := key.Value.(string)
+
+					if m.data[pk] == nil {
+						continue
+					}
+
+					value := m.data[key.Value.(string)]
+					tableRes = append(tableRes, tablestore.RowResult{
+						TableName: criteria.TableName,
+						Columns: []*tablestore.AttributeColumn{
+							{
+								ColumnName: stateValue,
+								Value:      value,
+							},
+						},
+						PrimaryKey: tablestore.PrimaryKey{
+							PrimaryKeys: []*tablestore.PrimaryKeyColumn{
+								{
+									ColumnName: stateKey,
+									Value:      key.Value,
+								},
+							},
+						},
+					})
+					resp.TableToRowsResult[criteria.TableName] = tableRes
+
+					break
+				}
+			}
+		}
+	}
+
+	return resp, nil
+}
+
+func (m *mockClient) BatchWriteRow(request *tablestore.BatchWriteRowRequest) (*tablestore.BatchWriteRowResponse, error) {
+	resp := &tablestore.BatchWriteRowResponse{}
+	for _, changes := range request.RowChangesGroupByTable {
+		for _, change := range changes {
+			switch inst := change.(type) {
+			case *tablestore.UpdateRowChange:
+				var pk string
+				for _, col := range inst.PrimaryKey.PrimaryKeys {
+					if col.ColumnName == stateKey {
+						pk = col.Value.(string)
+
+						break
+					}
+				}
+
+				for _, col := range inst.Columns {
+					if col.ColumnName == stateValue {
+						buf := &bytes.Buffer{}
+						binary.Write(buf, binary.BigEndian, col.Value)
+						m.data[pk] = buf.Bytes()
+					}
+				}
+
+			case *tablestore.DeleteRowChange:
+				for _, col := range inst.PrimaryKey.PrimaryKeys {
+					if col.ColumnName == stateKey {
+						delete(m.data, col.Value.(string))
+
+						break
+					}
+				}
+			}
+		}
+	}
+
+	return resp, nil
+}
---
+++ state/alicloud/tablestore/tablestore_test.go
@@ -0,0 +1,136 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation and Dapr Contributors.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package tablestore
+
+import (
+	"testing"
+
+	"github.com/agrea/ptr"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/dapr/components-contrib/state"
+	"github.com/dapr/kit/logger"
+)
+
+func TestTableStoreMetadata(t *testing.T) {
+	m := state.Metadata{}
+	m.Properties = map[string]string{
+		"accessKeyID":  "ACCESSKEYID",
+		"accessKey":    "ACCESSKEY",
+		"instanceName": "INSTANCENAME",
+		"tableName":    "TABLENAME",
+		"endpoint":     "ENDPOINT",
+	}
+	aliCloudTableStore := AliCloudTableStore{}
+
+	meta, err := aliCloudTableStore.parse(m)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "ACCESSKEYID", meta.AccessKeyID)
+	assert.Equal(t, "ACCESSKEY", meta.AccessKey)
+	assert.Equal(t, "INSTANCENAME", meta.InstanceName)
+	assert.Equal(t, "TABLENAME", meta.TableName)
+	assert.Equal(t, "ENDPOINT", meta.Endpoint)
+}
+
+func TestReadAndWrite(t *testing.T) {
+	ctl := gomock.NewController(t)
+
+	defer ctl.Finish()
+
+	store := NewAliCloudTableStore(logger.NewLogger("test"))
+	store.Init(state.Metadata{})
+
+	store.client = &mockClient{
+		data: make(map[string][]byte),
+	}
+
+	t.Run("test set 1", func(t *testing.T) {
+		setReq := &state.SetRequest{
+			Key:   "theFirstKey",
+			Value: "value of key",
+			ETag:  ptr.String("the etag"),
+		}
+		err := store.Set(setReq)
+		assert.Nil(t, err)
+	})
+
+	t.Run("test get 1", func(t *testing.T) {
+		getReq := &state.GetRequest{
+			Key: "theFirstKey",
+		}
+		resp, err := store.Get(getReq)
+		assert.Nil(t, err)
+		assert.NotNil(t, resp)
+		assert.Equal(t, "value of key", string(resp.Data))
+	})
+
+	t.Run("test set 2", func(t *testing.T) {
+		setReq := &state.SetRequest{
+			Key:   "theSecondKey",
+			Value: "1234",
+			ETag:  ptr.String("the etag"),
+		}
+		err := store.Set(setReq)
+		assert.Nil(t, err)
+	})
+
+	t.Run("test get 2", func(t *testing.T) {
+		getReq := &state.GetRequest{
+			Key: "theSecondKey",
+		}
+		resp, err := store.Get(getReq)
+		assert.Nil(t, err)
+		assert.NotNil(t, resp)
+		assert.Equal(t, "1234", string(resp.Data))
+	})
+
+	t.Run("test BulkSet", func(t *testing.T) {
+		err := store.BulkSet([]state.SetRequest{{
+			Key:   "theFirstKey",
+			Value: "666",
+		}, {
+			Key:   "theSecondKey",
+			Value: "777",
+		}})
+
+		assert.Nil(t, err)
+	})
+
+	t.Run("test BulkGet", func(t *testing.T) {
+		_, resp, err := store.BulkGet([]state.GetRequest{{
+			Key: "theFirstKey",
+		}, {
+			Key: "theSecondKey",
+		}})
+
+		assert.Nil(t, err)
+		assert.Equal(t, 2, len(resp))
+		assert.Equal(t, "666", string(resp[0].Data))
+		assert.Equal(t, "777", string(resp[1].Data))
+	})
+
+	t.Run("test delete", func(t *testing.T) {
+		req := &state.DeleteRequest{
+			Key: "theFirstKey",
+		}
+		err := store.Delete(req)
+		assert.Nil(t, err)
+	})
+
+	t.Run("test BulkGet2", func(t *testing.T) {
+		_, resp, err := store.BulkGet([]state.GetRequest{{
+			Key: "theFirstKey",
+		}, {
+			Key: "theSecondKey",
+		}})
+
+		assert.Nil(t, err)
+		assert.Equal(t, 1, len(resp))
+		assert.Equal(t, "777", string(resp[0].Data))
+	})
+}

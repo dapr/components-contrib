df71ece87d94e31656d58f6ae86a3c890ec5bfd9
Simon Leet <31784195+CodeMonkeyLeet@users.noreply.github.com>
2021-10-27 13:38:48 -0700

    Address flakiness in Azure SQL conformance test (#1219)
    * Add script to update Azure SQL firewall rules from GitHub meta API
    
    * Update state.azure.sql test to use unique DB names
    
    - Add use of `databaseName` metadata to state.azure.sql test.
    - Add dynamic generation of test `databaseName` to conformance.yml to
      avoid multiple test instances from racing.
    - Add wait before clean-up of Azure SQL DB instance in conformance.yml
      to prevent test flakiness and accumulation of undeleted DBs.
    - Remove dynamic Azure SQL firewall rule injection from conformance.yml.
    - The workflow relies on IPs used by GitHub Actions to be provisioned
      in the firewall rules already.
    - Update documentation for managing Azure SQL and testing instructions.
    
    Co-authored-by: Long Dai <long.dai@intel.com>
    Co-authored-by: Artur Souza <artursouza.ms@outlook.com>
    Signed-off-by: 1046102779 <seachen@tencent.com>

---
+++ .github/infrastructure/conformance/azure/allow-github-ips-in-azuresql.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+# ------------------------------------------------------------
+# Copyright (c) Microsoft Corporation and Dapr Contributors.
+# Licensed under the MIT License.
+# ------------------------------------------------------------
+
+import argparse
+import ipaddress
+import json
+import os
+import subprocess
+import sys
+import urllib.request
+
+def parseArgs():
+    abscwd = os.path.abspath(os.getcwd())
+    arg_parser = argparse.ArgumentParser(description='Generates the IP ranges based on CIDRs for GitHub Actions from meta API.')
+    arg_parser.add_argument('--outpath', type=str, default=abscwd, help='Optional. Full path to write the JSON output to.')
+    arg_parser.add_argument('--sqlserver', type=str, help='Name of the Azure SQL server to update firewall rules of. Required for deployment.')
+    arg_parser.add_argument('--resource-group', type=str, help='Resouce group containing the target Azure SQL server. Required for deployment.')
+    arg_parser.add_argument('--no-deployment', action='store_true', help='Specify this flag to generate the ARM template without deploying it.')
+    args = arg_parser.parse_args()
+
+    if not args.no_deployment:
+        is_missing_args = False
+        if not args.sqlserver:
+            print('ERROR: the following argument is required: --sqlserver')
+            is_missing_args = True
+        if not args.resource_group:
+            print('ERROR: the following argument is required: --resource-group')
+            is_missing_args = True
+        if is_missing_args:
+            arg_parser.print_help()
+            sys.exit(-1)
+
+    print('Arguments parsed: {}'.format(args))
+    return args
+
+def getResponse(url):
+    operUrl = urllib.request.urlopen(url)
+    if(operUrl.getcode()==200):
+        data = operUrl.read()
+        jsonData = json.loads(data)
+    else:
+        print('ERROR: failed to receive data', operUrl.getcode())
+    return jsonData
+
+def writeAllowedIPRangesJSON(outpath):
+    url = 'https://api.github.com/meta'
+    jsonData = getResponse(url)
+
+    ipRanges = []
+    prevStart = ''
+    prevEnd = ''
+
+    # Iterate the public IP CIDRs used to run GitHub Actions, and convert them
+    # into IP ranges for test SQL server firewall access.
+    for cidr in jsonData['actions']:
+        net = ipaddress.ip_network(cidr)
+        # SQL server firewall only supports up to 128 firewall rules.
+        # As a first cut, exclude all IPv6 addresses.
+        if net.version == 4:
+            start = net[0]
+            end = net[-1]
+            # print(f'{cidr} --> [{start}, {end}]')
+
+            if prevStart == '':
+                prevStart = start
+            if prevEnd == '':
+                prevEnd = end
+            elif prevEnd + 65536 > start:
+                # If the current IP range is within the granularity of a /16
+                # subnet mask to the previous range, coalesce them into one.
+                # This is necessary to get the number of rules down to ~100.
+                prevEnd = end
+            else:
+                ipRange = [str(prevStart), str(prevEnd)]
+                ipRanges.append(ipRange)
+                prevStart = start
+                prevEnd = end
+
+    if prevStart != '' and prevEnd != '':
+        ipRange = [str(prevStart), str(prevEnd)]
+        ipRanges.append(ipRange)
+
+    with open(outpath, 'w') as outfile:
+        json.dump(ipRanges, outfile)
+
+def main():
+    args = parseArgs()
+
+    # Get the GitHub IP Ranges to use as firewall allow-rules from the GitHub meta API
+    ipRangesFileName = os.path.join(args.outpath, 'github-ipranges.json')
+    writeAllowedIPRangesJSON(ipRangesFileName)
+    print(f'INFO: GitHub Actions public IP range rules written {ipRangesFileName}')
+
+    # Generate the ARM template from bicep to update Azure SQL server firewall rules
+    subprocess.call(['az', 'bicep', 'install'])
+    firewallTemplateName = os.path.join(args.outpath, 'update-sql-firewall-rules.json')
+    subprocess.call(['az', 'bicep', 'build', '--file', 'conf-test-azure-sqlserver-firewall.bicep', '--outfile', firewallTemplateName])
+    print(f'INFO: ARM template to update SQL Server firewall rules written to {firewallTemplateName}')
+
+    # Update the Azure SQL server firewall rules
+    if args.no_deployment:
+        print(f'INFO: --no-deployment specified, skipping update of SQL server {firewallTemplateName}')
+    else:
+        subprocess.call(['az', 'deployment', 'group', 'create', '--name', 'UpdateSQLFirewallRules', '--template-file', firewallTemplateName, '--resource-group', args.resource_group, '--parameters', f'sqlServerName={args.sqlserver}', '--parameters', f'ipRanges=@{ipRangesFileName}'])
+
+    sys.exit(0)
+
+if __name__ == '__main__':
+    main()

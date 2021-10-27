c4efb2357dacf19e78b7ac2b7c45f174be863033
Artur Souza <artursouza.ms@outlook.com>
2021-10-27 11:41:25 -0700

    Revert "Merge pull request #1143 from jigargandhi/content-type" (#1239)
    This reverts commit e373b954898a3d88f6bd41f4c119c0a181ac2b2a, reversing
    changes made to 8505c1a1db9d20ce488d48031073dcb0d2e0580e.
    
    Signed-off-by: 1046102779 <seachen@tencent.com>

--- state/requests.go
+++ state/requests.go
@@ -45,12 +45,11 @@ type DeleteStateOption struct {
 
 // SetRequest is the object describing an upsert request.
 type SetRequest struct {
-	Key         string            `json:"key"`
-	Value       interface{}       `json:"value"`
-	ETag        *string           `json:"etag,omitempty"`
-	Metadata    map[string]string `json:"metadata,omitempty"`
-	Options     SetStateOption    `json:"options,omitempty"`
-	ContentType string            `json:"contentType,omitempty"`
+	Key      string            `json:"key"`
+	Value    interface{}       `json:"value"`
+	ETag     *string           `json:"etag,omitempty"`
+	Metadata map[string]string `json:"metadata,omitempty"`
+	Options  SetStateOption    `json:"options,omitempty"`
 }
 
 // GetKey gets the Key on a SetRequest.

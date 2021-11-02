5ddfab4e687a93f9feecdda3ec738b4b0ac486dc
Bernd Verst <4535280+berndverst@users.noreply.github.com>
2021-11-02 02:16:15 +0000

    Certification tests certifiably certifying

--- tests/certification/state/sqlserver/sqlserver_test.go
+++ tests/certification/state/sqlserver/sqlserver_test.go
@@ -6,6 +6,7 @@
 package sqlserver_test
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -21,6 +22,8 @@ import (
 	secretstore_env "github.com/dapr/components-contrib/secretstores/local/env"
 	secretstores_loader "github.com/dapr/dapr/pkg/components/secretstores"
 
+	// mssql "github.com/denisenkom/go-mssqldb"
+
 	// Dapr runtime and Go-SDK
 	"github.com/dapr/dapr/pkg/runtime"
 	dapr_testing "github.com/dapr/dapr/pkg/testing"
@@ -29,15 +32,18 @@ import (
 	// Certification testing runnables
 	"github.com/dapr/components-contrib/tests/certification/embedded"
 	"github.com/dapr/components-contrib/tests/certification/flow"
+
 	"github.com/dapr/components-contrib/tests/certification/flow/dockercompose"
 	"github.com/dapr/components-contrib/tests/certification/flow/network"
 	"github.com/dapr/components-contrib/tests/certification/flow/sidecar"
+	"github.com/dapr/go-sdk/client"
 )
 
 const (
-	sidecarName       = "dapr-1"
-	dockerComposeYAML = "docker-compose.yml"
-	stateStoreName    = "dapr-state-store"
+	sidecarName             = "dapr-1"
+	dockerComposeYAML       = "docker-compose.yml"
+	stateStoreName          = "dapr-state-store"
+	certificationTestPrefix = "stable-certification-"
 )
 
 func TestSqlServer(t *testing.T) {
@@ -49,29 +55,68 @@ func TestSqlServer(t *testing.T) {
 	currentHttpPort := ports[1]
 
 	basicTest := func(ctx flow.Context) error {
-		client := sidecar.GetClient(ctx, sidecarName)
+		client, err := client.NewClientWithPort(fmt.Sprint(currentGrpcPort))
+		if err != nil {
+			panic(err)
+		}
+		defer client.Close()
 
 		// save state with the key certification1, default options: strong, last-write
-		err := client.SaveState(ctx, stateStoreName, "certificationkey1", []byte("certificationdata"))
+		err = client.SaveState(ctx, stateStoreName, certificationTestPrefix+"key1", []byte("certificationdata"))
 		assert.NoError(t, err)
 
 		// get state for key certificationkey1
-		item, err := client.GetState(ctx, stateStoreName, "certificationkey1")
+		item, err := client.GetState(ctx, stateStoreName, certificationTestPrefix+"key1")
 		assert.NoError(t, err)
 		assert.Equal(t, "certificationdata", string(item.Value))
 
 		// delete state for key certificationkey1
-		err = client.DeleteState(ctx, stateStoreName, "certificationkey1")
+		err = client.DeleteState(ctx, stateStoreName, certificationTestPrefix+"key1")
 		assert.NoError(t, err)
 
 		return nil
 	}
 
-	flow.New(t, "sqlserver certification").
+	flow.New(t, "SQLServer certification using SQL Server Docker").
 		// Run SQL Server using Docker Compose.
-		Step(dockercompose.Run("Spinning up SQL Server via Docker Compose", dockerComposeYAML)).
+		Step(dockercompose.Run("sqlserver", dockerComposeYAML)).
+		// This step only applies to spinning up the docker container for the first time
+		Step("Wait for Docker", flow.Sleep(time.Second*10)).
 
 		// Run the Dapr sidecar with the Kafka component.
+		Step(sidecar.Run(sidecarName,
+			embedded.WithoutApp(),
+			embedded.WithDaprGRPCPort(currentGrpcPort),
+			embedded.WithDaprHTTPPort(currentHttpPort),
+			embedded.WithComponentsPath("components/docker/default"),
+			runtime.WithSecretStores(
+				secretstores_loader.New("local.env", func() secretstores.SecretStore {
+					return secretstore_env.NewEnvSecretStore(log)
+				})),
+			runtime.WithStates(
+				state_loader.New("sqlserver", func() state.Store {
+					return state_sqlserver.NewSQLServerStateStore(log)
+				}),
+			))).
+		Step("Run basic test", basicTest).
+		// Introduce network interruption
+		Step("interrupt network",
+			network.InterruptNetwork(40*time.Second, nil, nil, "1433", "1434")).
+
+		// Component should recover at this point.
+		Step("wait", flow.Sleep(10*time.Second)).
+		Step("Run basic test again to verify reconnection occurred", basicTest, sidecar.Stop(sidecarName)).
+		Step("Stopping SQL Server Docker container", dockercompose.Stop("sqlserver", dockerComposeYAML)).
+		Run()
+
+	ports, err = dapr_testing.GetFreePorts(2)
+	assert.NoError(t, err)
+
+	currentGrpcPort = ports[0]
+	currentHttpPort = ports[1]
+
+	flow.New(t, "SQL Server certification using Azure SQL").
+		// Run the Dapr sidecar with the SQL Server component.
 		Step(sidecar.Run(sidecarName,
 			embedded.WithoutApp(),
 			embedded.WithDaprGRPCPort(currentGrpcPort),
@@ -90,10 +135,10 @@ func TestSqlServer(t *testing.T) {
 
 		// Errors will occurring here.
 		Step("interrupt network",
-			network.InterruptNetwork(30*time.Second, nil, nil, "1433")).
-		//
+			network.InterruptNetwork(40*time.Second, nil, nil, "1433", "1434")).
+
 		// Component should recover at this point.
-		Step("wait", flow.Sleep(30*time.Second)).
+		Step("wait", flow.Sleep(10*time.Second)).
 		Step("Run basic test again to verify reconnection occurred", basicTest, sidecar.Stop(sidecarName)).
 		Run()
 }

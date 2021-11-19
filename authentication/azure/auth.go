467738a998259034b2db3d0524d2dd8edc2a37de
Artur Souza <artursouza.ms@outlook.com>
2021-11-19 15:22:17 -0800

    Update auth.go

--- authentication/azure/auth.go
+++ authentication/azure/auth.go
@@ -44,7 +44,7 @@ func NewEnvironmentSettings(resourceName string, values map[string]string) (Envi
 	case "cosmosdb":
 		// Azure Cosmos DB (data plane)
 		es.Resource = "https://" + azureEnv.CosmosDBDNSSuffix
-	case "serviceBus":
+	case "servicebus":
 		es.Resource = azureEnv.ResourceIdentifiers.ServiceBus
 	default:
 		return es, errors.New("invalid resource name: " + resourceName)

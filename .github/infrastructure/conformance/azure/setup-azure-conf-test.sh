6bc92d7db6212c6d1f19c058a937d11dcd6389c7
Artur Souza <artursouza.ms@outlook.com>
2021-12-07 14:27:16 -0800

    Update license headers to Apache 2
    Signed-off-by: Artur Souza <artursouza.ms@outlook.com>

--- .github/infrastructure/conformance/azure/setup-azure-conf-test.sh
+++ .github/infrastructure/conformance/azure/setup-azure-conf-test.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
-# ------------------------------------------------------------
-# Copyright (c) Microsoft Corporation and Dapr Contributors.
-# Licensed under the MIT License.
-# ------------------------------------------------------------
+# Copyright 2021 The Dapr Authors
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 set -e
 
@@ -366,10 +372,16 @@ TEARDOWN_SCRIPT_NAME="${OUTPUT_PATH}/${PREFIX}-teardown-conf-test.sh"
 tee "${TEARDOWN_SCRIPT_NAME}" > /dev/null \
 << EOF
 #!/usr/bin/env bash
-# ------------------------------------------------------------
-# Copyright (c) Microsoft Corporation and Dapr Contributors.
-# Licensed under the MIT License.
-# ------------------------------------------------------------
+# Copyright 2021 The Dapr Authors
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 set +e
 echo "Deleting deployment ${DEPLOY_NAME} ..."
 az deployment sub delete --name "${DEPLOY_NAME}"
@@ -398,10 +410,16 @@ ENV_CONFIG_FILENAME="${OUTPUT_PATH}/${PREFIX}-conf-test-config.rc"
 tee "${ENV_CONFIG_FILENAME}" > /dev/null \
 << 'EOF'
 #!/usr/bin/env bash
-# ------------------------------------------------------------
-# Copyright (c) Microsoft Corporation and Dapr Contributors.
-# Licensed under the MIT License.
-# ------------------------------------------------------------
+# Copyright 2021 The Dapr Authors
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 if [[ -n ${NGROK_ENDPOINT} ]]; then
     export AzureEventGridSubscriberEndpoint="${NGROK_ENDPOINT}/api/events"

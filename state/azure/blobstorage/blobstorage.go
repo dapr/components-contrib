2c3dfc87085b6387d6554d0806c893f98675321a
Tim Burkert <burkert.tim@gmail.com>
2022-01-03 20:13:45 +0100

    state azure blobstorage: fail for invalid account name
    Signed-off-by: Tim Burkert <burkert.tim@gmail.com>

--- state/azure/blobstorage/blobstorage.go
+++ state/azure/blobstorage/blobstorage.go
@@ -32,6 +32,7 @@ import (
 	"context"
 	b64 "encoding/base64"
 	"fmt"
+	"net"
 	"net/url"
 	"strings"
 
@@ -91,16 +92,22 @@ func (r *StateStore) Init(metadata state.Metadata) error {
 	p := azblob.NewPipeline(credential, options)
 
 	var containerURL azblob.ContainerURL
+	var URL *url.URL
 	customEndpoint, ok := metadata.Properties[endpointKey]
 	if ok && customEndpoint != "" {
-		URL, parseErr := url.Parse(fmt.Sprintf("%s/%s/%s", customEndpoint, meta.accountName, meta.containerName))
-		if parseErr != nil {
-			return parseErr
+		URL, err = url.Parse(fmt.Sprintf("%s/%s/%s", customEndpoint, meta.accountName, meta.containerName))
+		if err != nil {
+			return err
 		}
 		containerURL = azblob.NewContainerURL(*URL, p)
 	} else {
-		URL, _ := url.Parse(fmt.Sprintf("https://%s.blob.%s/%s", meta.accountName, env.StorageEndpointSuffix, meta.containerName))
+		URL, _ = url.Parse(fmt.Sprintf("https://%s.blob.%s/%s", meta.accountName, env.StorageEndpointSuffix, meta.containerName))
 		containerURL = azblob.NewContainerURL(*URL, p)
+	}
+
+	_, err = net.LookupHost(URL.Hostname())
+	if err != nil {
+		return err
 	}
 
 	ctx := context.Background()

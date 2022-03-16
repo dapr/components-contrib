5da0ab0a1544c669d68c416b2477a4d1f8fe95ee
jasonlica <jasonli@huawei.com>
2022-03-16 17:23:39 -0400

    Implement a new state store component for the huawei dcs
    Signed-off-by: jasonlica <jasonli@huawei.com>

---
+++ state/huawei/dcs/dcs.go
@@ -0,0 +1,139 @@
+/*
+Copyright 2021 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package dcs
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/dapr/components-contrib/state"
+	redis "github.com/dapr/components-contrib/state/redis"
+	"github.com/dapr/kit/logger"
+)
+
+type StateStore struct {
+	r        *redis.StateStore
+	metadata dcsMetadata
+	logger   logger.Logger
+}
+
+type dcsMetadata struct {
+	InstanceName string `json:"instanceName"`
+	InstanceID   string `json:"instanceId"`
+	ProjectID    string `json:"projectId"`
+	Region       string `json:"region"`
+	VPCId        string `json:"vpcId"`
+	DCSHost      string `json:"dcsHost"`
+	DCSPassword  string `json:"dcsPassword"`
+}
+
+// NewStateStore returns a new DCS (redis) state store.
+func NewDCSStateStore(logger logger.Logger) *StateStore {
+	logger.Debugf("starting new DCS state store")
+	d := &StateStore{
+		logger: logger,
+	}
+	d.r = redis.NewRedisStateStore(logger)
+
+	return d
+}
+
+func parseDCSMetadata(meta state.Metadata) (*dcsMetadata, error) {
+	b, err := json.Marshal(meta.Properties)
+	if err != nil {
+		return nil, err
+	}
+
+	var m dcsMetadata
+	err = json.Unmarshal(b, &m)
+	if err != nil {
+		return nil, err
+	}
+	if m.InstanceID == "" {
+		return nil, fmt.Errorf("missing dcs instance id")
+	}
+	if m.ProjectID == "" {
+		return nil, fmt.Errorf("missing dcs project id")
+	}
+	if m.DCSHost == "" {
+		return nil, fmt.Errorf("missing dcs host")
+	}
+
+	return &m, nil
+}
+
+// Init does metadata and connection parsing.
+func (d *StateStore) Init(metadata state.Metadata) error {
+	d.logger.Debugf("initializing DCS and parsing metadata")
+	m, err := parseDCSMetadata(metadata)
+	if err != nil {
+		return err
+	}
+	d.metadata = *m
+
+	// re-map DCS host/password to respective redis keys
+	metadata.Properties["redisHost"] = metadata.Properties["dcsHost"]
+	metadata.Properties["redisPassword"] = metadata.Properties["dcsPassword"]
+
+	err = d.r.Init(metadata)
+	if err != nil {
+		return err
+	}
+
+	d.logger.Debugf("DCS metadata [%v]", metadata)
+
+	return nil
+}
+
+func (d *StateStore) Ping() error {
+	return d.r.Ping()
+}
+
+func (d *StateStore) BulkDelete(req []state.DeleteRequest) error {
+	return d.r.BulkDelete(req)
+}
+
+func (d *StateStore) BulkGet(req []state.GetRequest) (bool, []state.BulkGetResponse, error) {
+	return d.r.BulkGet(req)
+}
+
+// BulkSet performs a bulk set operation.
+func (d *StateStore) BulkSet(req []state.SetRequest) error {
+	return d.r.BulkSet(req)
+}
+
+// Delete performs a delete operation.
+func (d *StateStore) Delete(req *state.DeleteRequest) error {
+	return d.r.Delete(req)
+}
+
+// Get retrieves state from dcs with a key.
+func (d *StateStore) Get(req *state.GetRequest) (*state.GetResponse, error) {
+	return d.r.Get(req)
+}
+
+// Set saves into dcs.
+func (d *StateStore) Set(req *state.SetRequest) error {
+	return d.r.Set(req)
+}
+
+// Multi performs a transactional operation. succeeds only if all operations succeed, and fails if one or more operations fail.
+func (d *StateStore) Multi(request *state.TransactionalStateRequest) error {
+	return d.r.Multi(request)
+}
+
+// Features returns the features available in this state store.
+func (d *StateStore) Features() []state.Feature {
+	return d.r.Features()
+}
---
+++ state/huawei/dcs/dcs_test.go
@@ -0,0 +1,317 @@
+/*
+Copyright 2021 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package dcs
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+	"unsafe"
+
+	"github.com/alicebob/miniredis/v2"
+	jsoniter "github.com/json-iterator/go"
+
+	rediscomponent "github.com/dapr/components-contrib/internal/component/redis"
+	"github.com/dapr/components-contrib/state"
+	redis "github.com/dapr/components-contrib/state/redis"
+	"github.com/dapr/kit/logger"
+
+	redisext "github.com/go-redis/redis/v8"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	defaultDB              = 0
+	defaultMaxRetries      = 3
+	defaultMaxRetryBackoff = time.Second * 2
+)
+
+type metadata struct {
+	maxRetries      int
+	maxRetryBackoff time.Duration
+	ttlInSeconds    *int
+	queryIndexes    string
+}
+
+type querySchemaElem struct {
+	schema []interface{}
+	keys   map[string]string
+}
+
+type querySchemas map[string]*querySchemaElem
+
+// This is a copy (mock) for the actual redis.StateStore
+// make sure to update this when necessary, to keep the test cases passed
+type MockRedisStateStore struct {
+	state.DefaultBulkStore
+	client         redisext.UniversalClient
+	clientSettings *rediscomponent.Settings
+	json           jsoniter.API
+	metadata       metadata
+	replicas       int
+	querySchemas   querySchemas
+
+	features []state.Feature
+	logger   logger.Logger
+
+	ctx    context.Context
+	cancel context.CancelFunc
+}
+
+func TestInit(t *testing.T) {
+	//TODO: Modify to use the Mock redis store. currently, it uses the local redis instance
+
+	l := logger.NewLogger("test")
+	m := state.Metadata{}
+	d := NewDCSStateStore(l)
+
+	t.Run("Init with valid metadata", func(t *testing.T) {
+		m.Properties = map[string]string{
+			"InstanceName": "my-dcs",
+			"InstanceId":   "1234",
+			"ProjectId":    "abcd",
+			"Region":       "cn-north-4",
+			"VPCId":        "vpc-test",
+			"DCSHost":      "127.0.0.1:6379",
+			"DCSPassword":  "pass",
+		}
+		err := d.Init(m)
+		assert.Nil(t, err)
+	})
+
+	t.Run("Init with missing instance id", func(t *testing.T) {
+		m.Properties = map[string]string{
+			"ProjectId": "abcd",
+		}
+		err := d.Init(m)
+		assert.NotNil(t, err)
+		assert.Equal(t, err, fmt.Errorf("missing dcs instance id"))
+	})
+
+	t.Run("Init with missing project id", func(t *testing.T) {
+		m.Properties = map[string]string{
+			"InstanceId": "1234",
+		}
+		err := d.Init(m)
+		assert.NotNil(t, err)
+		assert.Equal(t, err, fmt.Errorf("missing dcs project id"))
+	})
+
+	t.Run("Init with missing DCS host", func(t *testing.T) {
+		m.Properties = map[string]string{
+			"InstanceId": "1234",
+			"ProjectId":  "abcd",
+		}
+		err := d.Init(m)
+		assert.NotNil(t, err)
+		assert.Equal(t, err, fmt.Errorf("missing dcs host"))
+	})
+
+	t.Run("Init only with mandatory fields", func(t *testing.T) {
+		m.Properties = map[string]string{
+			"InstanceId": "1234",
+			"ProjectId":  "abcd",
+			"DCSHost":    "127.0.0.1:6379",
+		}
+		err := d.Init(m)
+		assert.Nil(t, err)
+	})
+}
+
+func TestPing(t *testing.T) {
+	l := logger.NewLogger("test")
+	rs := setupMockRedisStateStore(l)
+
+	d := &StateStore{
+		r:      rs,
+		logger: l,
+	}
+
+	err := d.Ping()
+	assert.NoError(t, err)
+
+	rs.Close()
+
+	err = d.Ping()
+	assert.Error(t, err)
+}
+
+func TestSet(t *testing.T) {
+	l := logger.NewLogger("test")
+	rs := setupMockRedisStateStore(l)
+	defer rs.Close()
+
+	type value struct {
+		Value string
+	}
+
+	t.Run("Successfully set item", func(t *testing.T) {
+		d := StateStore{
+			r:      rs,
+			logger: l,
+		}
+		req := &state.SetRequest{
+			Key: "key",
+			Value: value{
+				Value: "value",
+			},
+		}
+		err := d.Set(req)
+		assert.Nil(t, err)
+	})
+}
+
+func TestGet(t *testing.T) {
+	l := logger.NewLogger("test")
+	rs := setupMockRedisStateStore(l)
+	defer rs.Close()
+
+	t.Run("Successfully with no required key", func(t *testing.T) {
+		d := StateStore{
+			r:      rs,
+			logger: l,
+		}
+		req := &state.GetRequest{
+			Key:      "key",
+			Metadata: nil,
+			Options: state.GetStateOption{
+				Consistency: "strong",
+			},
+		}
+		out, err := d.Get(req)
+		assert.Nil(t, err)
+		assert.Nil(t, out.Data)
+	})
+}
+
+func TestDelete(t *testing.T) {
+	l := logger.NewLogger("test")
+	rs := setupMockRedisStateStore(l)
+	defer rs.Close()
+
+	t.Run("Successfully with non existing item", func(t *testing.T) {
+		req := &state.DeleteRequest{
+			Key: "key",
+		}
+
+		d := StateStore{
+			r:      rs,
+			logger: l,
+		}
+		err := d.Delete(req)
+		assert.Nil(t, err)
+	})
+}
+
+func TestBulkSet(t *testing.T) {
+	l := logger.NewLogger("test")
+	rs := setupMockRedisStateStore(l)
+	defer rs.Close()
+
+	type value struct {
+		Value string
+	}
+
+	t.Run("Successfully set items", func(t *testing.T) {
+		d := StateStore{
+			r:      rs,
+			logger: l,
+		}
+		req := []state.SetRequest{
+			{
+				Key: "key1",
+				Value: value{
+					Value: "value1",
+				},
+			},
+			{
+				Key: "key2",
+				Value: value{
+					Value: "value2",
+				},
+			},
+		}
+		err := d.BulkSet(req)
+		assert.Nil(t, err)
+	})
+}
+
+func TestBulkDelete(t *testing.T) {
+	l := logger.NewLogger("test")
+	rs := setupMockRedisStateStore(l)
+	defer rs.Close()
+
+	t.Run("Successfully with non existing items", func(t *testing.T) {
+		d := StateStore{
+			r:      rs,
+			logger: l,
+		}
+		req := []state.DeleteRequest{
+			{
+				Key: "key1",
+			},
+			{
+				Key: "key2",
+			},
+		}
+		err := d.BulkDelete(req)
+		assert.Nil(t, err)
+	})
+}
+
+func setupMockRedisStateStore(logger logger.Logger) *redis.StateStore {
+	_, c := setupMiniredis()
+
+	globalTTLInSeconds := 100
+
+	qs := make(map[string]*querySchemaElem)
+	qs["test"] = &querySchemaElem{
+		keys:   make(map[string]string),
+		schema: []interface{}{},
+	}
+	ss := MockRedisStateStore{
+		DefaultBulkStore: state.DefaultBulkStore{},
+		client:           c,
+		clientSettings:   &rediscomponent.Settings{},
+		json:             jsoniter.ConfigFastest,
+		metadata:         metadata{maxRetries: defaultMaxRetries, maxRetryBackoff: defaultMaxRetryBackoff, ttlInSeconds: &globalTTLInSeconds, queryIndexes: ""},
+		replicas:         1,
+		querySchemas:     qs,
+		features:         []state.Feature{},
+		logger:           logger,
+		ctx:              nil,
+		cancel: func() {
+		},
+	}
+	ss.ctx, ss.cancel = context.WithCancel(context.Background())
+	rs := (*redis.StateStore)(unsafe.Pointer(&ss))
+	ss.DefaultBulkStore = state.NewDefaultBulkStore(rs)
+
+	return rs
+}
+
+func setupMiniredis() (*miniredis.Miniredis, *redisext.Client) {
+	s, err := miniredis.Run()
+	if err != nil {
+		panic(err)
+	}
+	opts := &redisext.Options{
+		Addr: s.Addr(),
+		DB:   defaultDB,
+	}
+
+	return s, redisext.NewClient(opts)
+}

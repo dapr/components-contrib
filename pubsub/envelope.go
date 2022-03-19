a1485fc94af2f725fcfe6360ca0536bf31ee09d8
1046102779 <seachen@tencent.com>
2022-03-19 15:06:23 +0800

    fix/typo: Change traceid to traceparent
    Signed-off-by: 1046102779 <seachen@tencent.com>

--- pubsub/envelope.go
+++ pubsub/envelope.go
@@ -34,24 +34,27 @@ const (
 	DefaultCloudEventSource = "Dapr"
 	// DefaultCloudEventDataContentType is the default content-type for the data attribute.
 	DefaultCloudEventDataContentType = "text/plain"
-	TraceIDField                     = "traceid"
-	TraceStateField                  = "tracestate"
-	TopicField                       = "topic"
-	PubsubField                      = "pubsubname"
-	ExpirationField                  = "expiration"
-	DataContentTypeField             = "datacontenttype"
-	DataField                        = "data"
-	DataBase64Field                  = "data_base64"
-	SpecVersionField                 = "specversion"
-	TypeField                        = "type"
-	SourceField                      = "source"
-	IDField                          = "id"
-	SubjectField                     = "subject"
+	// traceid, backwards compatibles.
+	// ::TODO delete traceid, and keep traceparent.
+	TraceIDField         = "traceid"
+	TraceParentField     = "traceparent"
+	TraceStateField      = "tracestate"
+	TopicField           = "topic"
+	PubsubField          = "pubsubname"
+	ExpirationField      = "expiration"
+	DataContentTypeField = "datacontenttype"
+	DataField            = "data"
+	DataBase64Field      = "data_base64"
+	SpecVersionField     = "specversion"
+	TypeField            = "type"
+	SourceField          = "source"
+	IDField              = "id"
+	SubjectField         = "subject"
 )
 
 // NewCloudEventsEnvelope returns a map representation of a cloudevents JSON.
 func NewCloudEventsEnvelope(id, source, eventType, subject string, topic string, pubsubName string,
-	dataContentType string, data []byte, traceID string, traceState string) map[string]interface{} {
+	dataContentType string, data []byte, traceParent string, traceState string) map[string]interface{} {
 	// defaults
 	if id == "" {
 		id = uuid.New().String()
@@ -90,7 +93,8 @@ func NewCloudEventsEnvelope(id, source, eventType, subject string, topic string,
 		TypeField:            eventType,
 		TopicField:           topic,
 		PubsubField:          pubsubName,
-		TraceIDField:         traceID,
+		TraceIDField:         traceParent,
+		TraceParentField:     traceParent,
 		TraceStateField:      traceState,
 	}
 
@@ -104,14 +108,15 @@ func NewCloudEventsEnvelope(id, source, eventType, subject string, topic string,
 }
 
 // FromCloudEvent returns a map representation of an existing cloudevents JSON.
-func FromCloudEvent(cloudEvent []byte, topic, pubsub, traceID string, traceState string) (map[string]interface{}, error) {
+func FromCloudEvent(cloudEvent []byte, topic, pubsub, traceParent string, traceState string) (map[string]interface{}, error) {
 	var m map[string]interface{}
 	err := jsoniter.Unmarshal(cloudEvent, &m)
 	if err != nil {
 		return m, err
 	}
 
-	m[TraceIDField] = traceID
+	m[TraceIDField] = traceParent
+	m[TraceParentField] = traceParent
 	m[TraceStateField] = traceState
 	m[TopicField] = topic
 	m[PubsubField] = pubsub
--- pubsub/envelope_test.go
+++ pubsub/envelope_test.go
@@ -136,7 +136,7 @@ func TestCreateCloudEventsEnvelopeDefaults(t *testing.T) {
 	t.Run("trace id", func(t *testing.T) {
 		envelope := NewCloudEventsEnvelope("a", "source", "", "", "",
 			"mypubsub", "", []byte("data"), "1", "")
-		assert.Equal(t, "1", envelope[TraceIDField])
+		assert.Equal(t, "1", envelope[TraceParentField])
 	})
 
 	t.Run("trace state", func(t *testing.T) {
@@ -239,7 +239,7 @@ func TestNewFromExisting(t *testing.T) {
 		assert.Equal(t, "a", n["customfield"])
 		assert.Equal(t, "b", n[TopicField])
 		assert.Equal(t, "pubsub", n[PubsubField])
-		assert.Equal(t, "1", n[TraceIDField])
+		assert.Equal(t, "1", n[TraceParentField])
 		assert.Equal(t, "key=value", n[TraceStateField])
 		assert.Nil(t, n[DataField])
 		assert.Nil(t, n[DataBase64Field])
@@ -264,7 +264,7 @@ func TestNewFromExisting(t *testing.T) {
 		assert.Equal(t, "a", n["customfield"])
 		assert.Equal(t, "b", n[TopicField])
 		assert.Equal(t, "pubsub", n[PubsubField])
-		assert.Equal(t, "1", n[TraceIDField])
+		assert.Equal(t, "1", n[TraceParentField])
 		assert.Equal(t, "key=value", n[TraceStateField])
 		assert.Nil(t, n[DataBase64Field])
 		assert.Equal(t, "hello world", n[DataField])
@@ -284,7 +284,7 @@ func TestNewFromExisting(t *testing.T) {
 		assert.Equal(t, "a", n["customfield"])
 		assert.Equal(t, "b", n[TopicField])
 		assert.Equal(t, "pubsub", n[PubsubField])
-		assert.Equal(t, "1", n[TraceIDField])
+		assert.Equal(t, "1", n[TraceParentField])
 		assert.Equal(t, "key=value", n[TraceStateField])
 		assert.Nil(t, n[DataField])
 		assert.Equal(t, base64.StdEncoding.EncodeToString([]byte{0x1}), n[DataBase64Field])
@@ -306,8 +306,7 @@ func TestNewFromRawPayload(t *testing.T) {
 		assert.Equal(t, "1.0", n[SpecVersionField])
 		assert.Equal(t, "mytopic", n[TopicField])
 		assert.Equal(t, "mypubsub", n[PubsubField])
-		assert.Nil(t, n["traceid"])
-		assert.Nil(t, n[TraceIDField])
+		assert.Nil(t, n[TraceParentField])
 		assert.Nil(t, n[DataField])
 		assert.Equal(t, "aGVsbG8gd29ybGQ=", n[DataBase64Field])
 	})

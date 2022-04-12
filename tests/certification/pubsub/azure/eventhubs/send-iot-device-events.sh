0d72d92f25e30e8dd8ad9759a7a0065d418e9995
Surender Singh Malik <surenderssm@users.noreply.github.com>
2022-04-12 19:46:05 +0530

    fixing iot device issue
    Signed-off-by: Surender Singh Malik <surenderssm@users.noreply.github.com>

--- tests/certification/pubsub/azure/eventhubs/send-iot-device-events.sh
+++ tests/certification/pubsub/azure/eventhubs/send-iot-device-events.sh
@@ -14,29 +14,26 @@
 
 set -e
 
-if [[ -z "${IOT_HUB_NAME}" ]]; then
-    echo "ERROR: IOT_HUB_NAME environment variable not defined."
-    exit 1
-fi
-if [[ -z "${AZURE_CREDENTIALS}" ]]; then
-    echo "ERROR: AZURE_CREDENTIALS environment variable not defined."
+if [[ -z "${AzureIotHubName}" ]]; then
+    echo "ERROR: AzureIotHubName environment variable not defined."
     exit 1
 fi
 
-# Log in to Azure using provided Service Principal (SP) credentials
-# The provided SP must have Contributor role access to the IoT Hub specified by IOT_HUB_NAME
-SDK_AUTH_SP_APPID="$(echo "${AZURE_CREDENTIALS}" | grep 'clientId' | sed -E 's/(.*clientId\"\: \")|\",//g')"
-SDK_AUTH_SP_CLIENT_SECRET="$(echo "${AZURE_CREDENTIALS}" | grep 'clientSecret' | sed -E 's/(.*clientSecret\"\: \")|\",//g')"
-SDK_AUTH_SP_TENANT="$(echo "${AZURE_CREDENTIALS}" | grep 'tenantId' | sed -E 's/(.*tenantId\"\: \")|\",//g')"
+# Log in to Azure using provided Service Principal (SPN) credentials
+# Required roles for SPN on IoT hub
+# `IoT Hub Data Contributor` scoped at this resource
+# `Owner` scoped at this resource
 
-az login --service-principal -u ${SDK_AUTH_SP_APPID} -p ${SDK_AUTH_SP_CLIENT_SECRET} --tenant ${SDK_AUTH_SP_TENANT}
+# login to azure
+az login --service-principal -u $AzureCertificationServicePrincipalClientId -p $AzureCertificationServicePrincipalClientSecret --tenant $AzureCertificationTenantId
 
 # Create test device ID if not already present
-IOT_HUB_TEST_DEVICE_NAME="test-device"
-if [[ -z "$(az iot hub device-identity show -n ${IOT_HUB_NAME} -d ${IOT_HUB_TEST_DEVICE_NAME})" ]]; then
-    az iot hub device-identity create -n ${IOT_HUB_NAME} -d ${IOT_HUB_TEST_DEVICE_NAME}
+IOT_HUB_TEST_DEVICE_NAME="certification-test-device"
+if [[ -z "$(az iot hub device-identity show -n ${AzureIotHubName} -d ${IOT_HUB_TEST_DEVICE_NAME})" ]]; then
+    az iot hub device-identity create -n ${AzureIotHubName} -d ${IOT_HUB_TEST_DEVICE_NAME}
     sleep 5
 fi
 
 # Send the test IoT device messages to the IoT Hub.`testmessageForEventHubCertificationTest` is being asserted in the certification test
-az iot device simulate -n ${IOT_HUB_NAME} -d ${IOT_HUB_TEST_DEVICE_NAME} --data 'testmessageForEventHubCertificationTest' --msg-count 2 --msg-interval 1 --protocol http --properties "iothub-userid=dapr-user-id;iothub-messageid=dapr-message-id"
+# TODO : read messageCount and data as an argument
+az iot device simulate -n ${AzureIotHubName} -d ${IOT_HUB_TEST_DEVICE_NAME} --data 'testmessageForEventHubCertificationTest' --msg-count 10 --msg-interval 1 --protocol http --properties "iothub-userid=dapr-user-id;iothub-messageid=dapr-message-id"

14e6ea83bd5e49d4d1f7c47982622be420ba3d3d
Sky Ao <aoxiaojian@gmail.com>
2022-04-28 01:19:57 +0800

    refactory kafka pubsub code to extract common kafka code for reuse
    Signed-off-by: Sky Ao <aoxiaojian@gmail.com>

---
+++ internal/component/kafka/consumer.go
@@ -0,0 +1,161 @@
+/*
+Copyright 2021 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package kafka
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/Shopify/sarama"
+	"github.com/cenkalti/backoff/v4"
+	"github.com/dapr/kit/retry"
+)
+
+type consumer struct {
+	k        *Kafka
+	ready    chan bool
+	callback EventHandler
+	once     sync.Once
+}
+
+func (consumer *consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	if consumer.callback == nil {
+		return fmt.Errorf("nil consumer callback")
+	}
+
+	b := consumer.k.backOffConfig.NewBackOffWithContext(session.Context())
+	for message := range claim.Messages() {
+		event := NewEvent{
+			Topic: message.Topic,
+			Data:  message.Value,
+		}
+		if err := retry.NotifyRecover(func() error {
+			consumer.k.logger.Debugf("Processing Kafka message: %s/%d/%d [key=%s]", message.Topic, message.Partition, message.Offset, asBase64String(message.Key))
+			err := consumer.callback(session.Context(), &event)
+			if err == nil {
+				session.MarkMessage(message, "")
+			}
+
+			return err
+		}, b, func(err error, d time.Duration) {
+			consumer.k.logger.Errorf("Error processing Kafka message: %s/%d/%d [key=%s]. Retrying...", message.Topic, message.Partition, message.Offset, asBase64String(message.Key))
+		}, func() {
+			consumer.k.logger.Infof("Successfully processed Kafka message after it previously failed: %s/%d/%d [key=%s]", message.Topic, message.Partition, message.Offset, asBase64String(message.Key))
+		}); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (consumer *consumer) Cleanup(sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+func (consumer *consumer) Setup(sarama.ConsumerGroupSession) error {
+	consumer.once.Do(func() {
+		close(consumer.ready)
+	})
+
+	return nil
+}
+
+// Subscribe to topic in the Kafka cluster
+// This call cannot block like its sibling in bindings/kafka because of where this is invoked in runtime.go.
+func (k *Kafka) Subscribe(topic string, _ map[string]string, handler EventHandler) error {
+	if k.consumerGroup == "" {
+		return errors.New("kafka: consumerGroup must be set to subscribe")
+	}
+
+	topics := k.addTopic(topic)
+
+	// Close resources and reset synchronization primitives
+	k.closeSubscriptionResources()
+
+	cg, err := sarama.NewConsumerGroup(k.brokers, k.consumerGroup, k.config)
+	if err != nil {
+		return err
+	}
+
+	k.cg = cg
+
+	ctx, cancel := context.WithCancel(context.Background())
+	k.cancel = cancel
+
+	ready := make(chan bool)
+	k.consumer = consumer{
+		k:        k,
+		ready:    ready,
+		callback: handler,
+	}
+
+	go func() {
+		defer func() {
+			k.logger.Debugf("Closing ConsumerGroup for topics: %v", topics)
+			err := k.cg.Close()
+			if err != nil {
+				k.logger.Errorf("Error closing consumer group: %v", err)
+			}
+		}()
+
+		k.logger.Debugf("Subscribed and listening to topics: %s", topics)
+
+		for {
+			k.logger.Debugf("Starting loop to consume.")
+
+			// Consume the requested topics
+			bo := backoff.WithContext(backoff.NewConstantBackOff(k.consumeRetryInterval), ctx)
+			innerErr := retry.NotifyRecover(func() error {
+				return k.cg.Consume(ctx, topics, &(k.consumer))
+			}, bo, func(err error, t time.Duration) {
+				k.logger.Errorf("Error consuming %v. Retrying...: %v", topics, err)
+			}, func() {
+				k.logger.Infof("Recovered consuming %v", topics)
+			})
+			if innerErr != nil && !errors.Is(innerErr, context.Canceled) {
+				k.logger.Errorf("Permanent error consuming %v: %v", topics, innerErr)
+			}
+
+			// If the context was cancelled, as is the case when handling SIGINT and SIGTERM below, then this pops
+			// us out of the consume loop
+			if ctx.Err() != nil {
+				return
+			}
+		}
+	}()
+
+	<-ready
+
+	return nil
+}
+
+// Close down consumer group resources, refresh once.
+func (k *Kafka) closeSubscriptionResources() {
+	if k.cg != nil {
+		k.cancel()
+		err := k.cg.Close()
+		if err != nil {
+			k.logger.Errorf("Error closing consumer group: %v", err)
+		}
+
+		k.consumer.once.Do(func() {
+			close(k.consumer.ready)
+			k.consumer.once = sync.Once{}
+		})
+	}
+}
---
+++ internal/component/kafka/kafka.go
@@ -0,0 +1,161 @@
+/*
+Copyright 2021 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package kafka
+
+import (
+	"context"
+	"time"
+
+	"github.com/Shopify/sarama"
+	"github.com/dapr/kit/logger"
+	"github.com/dapr/kit/retry"
+)
+
+// Kafka allows reading/writing to a Kafka consumer group.
+type Kafka struct {
+	producer      sarama.SyncProducer
+	consumerGroup string
+	brokers       []string
+	logger        logger.Logger
+	authType      string
+	saslUsername  string
+	saslPassword  string
+	initialOffset int64
+	cg            sarama.ConsumerGroup
+	topics        map[string]bool
+	cancel        context.CancelFunc
+	consumer      consumer
+	config        *sarama.Config
+
+	backOffConfig        retry.Config
+	consumeRetryInterval time.Duration
+}
+
+func NewKafka(l logger.Logger) *Kafka {
+	return &Kafka{
+		logger: l,
+	}
+}
+
+// Init does metadata parsing and connection establishment.
+func (k *Kafka) Init(metadata map[string]string) error {
+	upgradedMetadata, err := k.upgradeMetadata(metadata)
+	if err != nil {
+		return err
+	}
+
+	meta, err := k.getKafkaMetadata(upgradedMetadata)
+	if err != nil {
+		return err
+	}
+
+	k.brokers = meta.Brokers
+	k.consumerGroup = meta.ConsumerGroup
+	k.initialOffset = meta.InitialOffset
+	k.authType = meta.AuthType
+
+	config := sarama.NewConfig()
+	config.Version = meta.Version
+	config.Consumer.Offsets.Initial = k.initialOffset
+
+	if meta.ClientID != "" {
+		config.ClientID = meta.ClientID
+	}
+
+	err = updateTLSConfig(config, meta)
+	if err != nil {
+		return err
+	}
+
+	switch k.authType {
+	case oidcAuthType:
+		k.logger.Info("Configuring SASL OAuth2/OIDC authentication")
+		err = updateOidcAuthInfo(config, meta)
+		if err != nil {
+			return err
+		}
+	case passwordAuthType:
+		k.logger.Info("Configuring SASL Password authentication")
+		k.saslUsername = meta.SaslUsername
+		k.saslPassword = meta.SaslPassword
+		updatePasswordAuthInfo(config, k.saslUsername, k.saslPassword)
+	case mtlsAuthType:
+		k.logger.Info("Configuring mTLS authentcation")
+		err = updateMTLSAuthInfo(config, meta)
+		if err != nil {
+			return err
+		}
+	}
+
+	k.config = config
+	sarama.Logger = SaramaLogBridge{daprLogger: k.logger}
+
+	k.producer, err = getSyncProducer(*k.config, k.brokers, meta.MaxMessageBytes)
+	if err != nil {
+		return err
+	}
+
+	k.topics = make(map[string]bool)
+
+	// Default retry configuration is used if no
+	// backOff properties are set.
+	if err := retry.DecodeConfigWithPrefix(
+		&k.backOffConfig,
+		metadata,
+		"backOff"); err != nil {
+		return err
+	}
+	k.consumeRetryInterval = meta.ConsumeRetryInterval
+
+	k.logger.Debug("Kafka message bus initialization complete")
+
+	return nil
+}
+
+func (k *Kafka) Close() (err error) {
+	k.closeSubscriptionResources()
+
+	if k.producer != nil {
+		err = k.producer.Close()
+		k.producer = nil
+	}
+
+	return err
+}
+
+func (k *Kafka) addTopic(newTopic string) []string {
+	// Add topic to our map of topics
+	k.topics[newTopic] = true
+
+	topics := make([]string, len(k.topics))
+
+	i := 0
+	for topic := range k.topics {
+		topics[i] = topic
+		i++
+	}
+
+	return topics
+}
+
+// EventHandler is the handler used to handle the subscribed event.
+type EventHandler func(ctx context.Context, msg *NewEvent) error
+
+// NewEvent is an event arriving from a message bus instance.
+type NewEvent struct {
+	Data        []byte            `json:"data"`
+	Topic       string            `json:"topic"`
+	Metadata    map[string]string `json:"metadata"`
+	ContentType *string           `json:"contentType,omitempty"`
+}

c8bdf335ea15ca0d4fde60ec0bc846aff83eead4
lotuc <lotu.c@outlook.com>
2022-05-12 03:11:51 +0800

    Add topic metadata for mqtt input binding and support user defined topic for mqtt output binding (#1674)
    * feat(bindings/mqtt): add data incoming topic to metadata
    
    Signed-off-by: lotuc <lotu.c@outlook.com>
    
    * feat(bindings/mqtt): support user defined topic on create action
    
    Signed-off-by: lotuc <lotu.c@outlook.com>
    
    * chore(bindings/mqtt): add integration test and topic response check test
    
    Signed-off-by: lotuc <lotu.c@outlook.com>
    
    * fix(bindings/mqtt): ignore misspell linting error for word mosquitto
    
    Signed-off-by: lotuc <lotu.c@outlook.com>
    
    Co-authored-by: Yaron Schneider <schneider.yaron@live.com>
    Signed-off-by: Eddie <eddie@wassef.com>

---
+++ bindings/mqtt/mqtt_integration_test.go
@@ -0,0 +1,114 @@
+package mqtt
+
+import (
+	"context"
+	"os"
+	"testing"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/dapr/components-contrib/bindings"
+	"github.com/dapr/kit/logger"
+)
+
+const (
+	// Environment variable containing the host name for MQTT integration tests
+	// To run using docker:
+	//   Create mosquitto.conf with content:
+	//     listener 1883
+	//     allow_anonymous true
+	//   And run:
+	// nolint:misspell
+	//     docker run -d -v mosquitto.conf:/mosquitto/config/mosquitto.conf --name test-mqtt -p 1883:1883 eclipse-mosquitto:2
+	// In that case the connection string will be: tcp://127.0.0.1:1883
+	testMQTTConnectionStringEnvKey = "DAPR_TEST_MQTT_URL"
+)
+
+func getConnectionString() string {
+	return os.Getenv(testMQTTConnectionStringEnvKey)
+}
+
+func TestInvokeWithTopic(t *testing.T) {
+	t.Parallel()
+
+	url := getConnectionString()
+	if url == "" {
+		t.Skipf("MQTT connection string configuration must be set in environment variable '%s' (example 'tcp://localhost:1883')", testMQTTConnectionStringEnvKey)
+	}
+
+	topicDefault := "/app/default"
+	const msgDefault = "hello from default"
+	dataDefault := []byte(msgDefault)
+
+	topicCustomized := "/app/customized"
+	const msgCustomized = "hello from customized"
+	dataCustomized := []byte(msgCustomized)
+
+	metadata := bindings.Metadata{
+		Name: "testQueue",
+		Properties: map[string]string{
+			"consumerID":        uuid.NewString(),
+			"url":               url,
+			"topic":             topicDefault,
+			"qos":               "1",
+			"retain":            "false",
+			"cleanSession":      "true",
+			"backOffMaxRetries": "0",
+		},
+	}
+
+	logger := logger.NewLogger("test")
+
+	r := NewMQTT(logger)
+	err := r.Init(metadata)
+	assert.Nil(t, err)
+
+	conn, err := r.connect(uuid.NewString())
+	assert.Nil(t, err)
+	defer conn.Disconnect(1)
+
+	msgCh := make(chan interface{})
+	defer close(msgCh)
+
+	token := conn.Subscribe("/app/#", 1, func(client mqtt.Client, mqttMsg mqtt.Message) {
+		msgCh <- mqttMsg
+	})
+	ok := token.WaitTimeout(2 * time.Second)
+	assert.True(t, ok, "subscribe to /app/# timeout")
+	err = token.Error()
+	assert.Nil(t, err, "error subscribe to test topic")
+
+	// Timeout in case message transfer error.
+	go func() {
+		time.Sleep(5 * time.Second)
+		msgCh <- "timeout"
+	}()
+
+	// Test invoke with default topic configured for component.
+	_, err = r.Invoke(context.TODO(), &bindings.InvokeRequest{Data: dataDefault})
+	assert.Nil(t, err)
+
+	m := <-msgCh
+	mqttMessage, ok := m.(mqtt.Message)
+	assert.True(t, ok)
+	assert.Equal(t, dataDefault, mqttMessage.Payload())
+	assert.Equal(t, topicDefault, mqttMessage.Topic())
+
+	// Test invoke with customized topic.
+	_, err = r.Invoke(context.TODO(), &bindings.InvokeRequest{
+		Data: dataCustomized,
+		Metadata: map[string]string{
+			mqttTopic: topicCustomized,
+		},
+	})
+	assert.Nil(t, err)
+
+	m = <-msgCh
+	mqttMessage, ok = m.(mqtt.Message)
+	assert.True(t, ok)
+	assert.Equal(t, dataCustomized, mqttMessage.Payload())
+	assert.Equal(t, topicCustomized, mqttMessage.Topic())
+}

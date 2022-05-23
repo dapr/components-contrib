f3a713d88bae53c2228c7eae7aa267b1f11d1ec1
Shakir Mengrani <shakir.mengrani@gmail.com>
2022-05-23 11:08:42 +0500

    add mssql binding

---
+++ bindings/mssql/mssql.go
@@ -0,0 +1,157 @@
+package mssql
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/dapr/components-contrib/bindings"
+	"github.com/dapr/kit/logger"
+	_ "github.com/denisenkom/go-mssqldb"
+	"github.com/pkg/errors"
+)
+
+const (
+	// list of operations.
+	execOperation  bindings.OperationKind = "exec"
+	queryOperation bindings.OperationKind = "query"
+	closeOperation bindings.OperationKind = "close"
+
+	// configurations to connect to MSSQL, either a data source name represent by URL.
+	connectionURLKey = "url"
+
+	// keys from request's metadata.
+	commandSQLKey = "sql"
+)
+
+type MSSQL struct {
+	db     *sql.DB
+	logger logger.Logger
+}
+
+var _ = bindings.OutputBinding(&MSSQL{})
+
+// NewMSSQL returns a new MSSQL output binding.
+func NewMSSQL(logger logger.Logger) *MSSQL {
+	return &MSSQL{logger: logger}
+}
+
+func (m *MSSQL) Init(metadata bindings.Metadata) error {
+	m.logger.Debug("Initializing MSSQL binding")
+
+	p := metadata.Properties
+	url, ok := p[connectionURLKey]
+	if !ok || url == "" {
+		return fmt.Errorf("missing MSSQL connection string")
+	}
+
+	db, err := sql.Open("sqlserver", url)
+	if err != nil {
+		return fmt.Errorf("%q", err)
+	}
+	m.db = db
+	return nil
+}
+
+func (m *MSSQL) Invoke(ctx context.Context, req *bindings.InvokeRequest) (*bindings.InvokeResponse, error) {
+	if req == nil {
+		return nil, errors.Errorf("invoke request required")
+	}
+
+	if req.Operation == closeOperation {
+		return nil, m.db.Close()
+	}
+
+	if req.Metadata == nil {
+		return nil, errors.Errorf("metadata required")
+	}
+	m.logger.Debugf("operation: %v", req.Operation)
+
+	s, ok := req.Metadata[commandSQLKey]
+	if !ok || s == "" {
+		return nil, errors.Errorf("required metadata not set: %s", commandSQLKey)
+	}
+
+	startTime := time.Now().UTC()
+
+	resp := &bindings.InvokeResponse{
+		Metadata: map[string]string{
+			respOpKey:        string(req.Operation),
+			respSQLKey:       s,
+			respStartTimeKey: startTime.Format(time.RFC3339Nano),
+		},
+	}
+	switch req.Operation {
+	case queryOperation:
+		d, err := m.query(s)
+		if err != nil {
+			return nil, err
+		}
+		resp.Data = d
+	default:
+		return nil, errors.Errorf("invalid operation type: %s. Expected %s, %s, or %s", req.Operation, execOperation, queryOperation, closeOperation)
+	}
+	endTime := time.Now().UTC()
+	resp.Metadata[respEndTimeKey] = endTime.Format(time.RFC3339Nano)
+	resp.Metadata[respDurationKey] = endTime.Sub(startTime).String()
+	return resp, nil
+}
+
+func (m *MSSQL) query(sql string) ([]byte, error) {
+	m.logger.Debugf("query: %s", sql)
+	rows, err := m.db.Query(sql)
+	if err != nil {
+		return nil, errors.Wrapf(err, "error executing %s", sql)
+	}
+	defer func() {
+		_ = rows.Close()
+		_ = rows.Err()
+	}()
+	result, err := m.jsonify(rows)
+	if err != nil {
+		return nil, errors.Wrapf(err, "error marshalling query result for %s", sql)
+	}
+
+	return result, nil
+}
+
+func (m *MSSQL) jsonify(rows *sql.Rows) ([]byte, error) {
+	columnTypes, err := rows.ColumnTypes()
+	if err != nil {
+		return nil, err
+	}
+
+	var ret []interface{}
+	for rows.Next() {
+		values := prepareValues(columnTypes)
+		err := rows.Scan(values...)
+		if err != nil {
+			return nil, err
+		}
+
+		r := m.convert(columnTypes, values)
+		ret = append(ret, r)
+	}
+
+	return json.Marshal(ret)
+}
+
+// Operations returns list of operations supported by MSSQL binding.
+func (m *MSSQL) Operations() []bindings.OperationKind {
+	return []bindings.OperationKind{
+		execOperation,
+		queryOperation,
+		closeOperation,
+	}
+}
+
+// Close will close the DB.
+func (m *MSSQL) Close() error {
+	if m.db != nil {
+		return m.db.Close()
+	}
+
+	return nil
+}
---
+++ bindings/mssql/mssql_integration_test.go
@@ -0,0 +1,191 @@
+/*
+Copyright 2021 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package mssql
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/dapr/components-contrib/bindings"
+	"github.com/dapr/kit/logger"
+)
+
+const (
+	// MSSQL doesn't accept RFC3339 formatted time, rejects trailing 'Z' for UTC indicator.
+	MSSQLDateTimeFormat = "2006-01-02 15:04:05"
+
+	testCreateTable = `CREATE TABLE IF NOT EXISTS foo (
+		id bigint NOT NULL,
+		v1 character varying(50) NOT NULL,
+		b  BOOLEAN,
+		ts TIMESTAMP,
+		data LONGTEXT)`
+	testDropTable         = `DROP TABLE foo`
+	testInsert            = "INSERT INTO foo (id, v1, b, ts, data) VALUES (%d, 'test-%d', %t, '%v', '%s')"
+	testDelete            = "DELETE FROM foo"
+	testUpdate            = "UPDATE foo SET ts = '%v' WHERE id = %d"
+	testSelect            = "SELECT * FROM foo WHERE id < 3"
+	testSelectJSONExtract = "SELECT JSON_EXTRACT(data, '$.key') AS `key` FROM foo WHERE id < 3"
+)
+
+func TestOperations(t *testing.T) {
+	t.Parallel()
+	t.Run("Get operation list", func(t *testing.T) {
+		t.Parallel()
+		b := NewMSSQL(nil)
+		assert.NotNil(t, b)
+		l := b.Operations()
+		assert.Equal(t, 3, len(l))
+		assert.Contains(t, l, execOperation)
+		assert.Contains(t, l, closeOperation)
+		assert.Contains(t, l, queryOperation)
+	})
+}
+
+// SETUP TESTS
+// 1. `CREATE DATABASE daprtest;`
+// 2. `CREATE USER daprtest;`
+// 3. `GRANT ALL PRIVILEGES ON daprtest.* to daprtest;`
+// 4. `export MSSQL_TEST_CONN_URL=daprtest@tcp(localhost:3306)/daprtest`
+// 5. `go test -v -count=1 ./bindings/MSSQL -run ^TestMSSQLIntegrationWithURL`
+
+func TestMSSQLIntegration(t *testing.T) {
+	url := os.Getenv("MSSQL_TEST_CONN_URL")
+	if url == "" {
+		t.SkipNow()
+	}
+
+	b := NewMSSQL(logger.NewLogger("test"))
+	m := bindings.Metadata{Properties: map[string]string{connectionURLKey: url}}
+	if err := b.Init(m); err != nil {
+		t.Fatal(err)
+	}
+
+	defer b.Close()
+
+	req := &bindings.InvokeRequest{Metadata: map[string]string{}}
+
+	t.Run("Invoke create table", func(t *testing.T) {
+		req.Operation = execOperation
+		req.Metadata[commandSQLKey] = testCreateTable
+		res, err := b.Invoke(context.TODO(), req)
+		assertResponse(t, res, err)
+	})
+
+	t.Run("Invoke delete", func(t *testing.T) {
+		req.Operation = execOperation
+		req.Metadata[commandSQLKey] = testDelete
+		res, err := b.Invoke(context.TODO(), req)
+		assertResponse(t, res, err)
+	})
+
+	t.Run("Invoke insert", func(t *testing.T) {
+		req.Operation = execOperation
+		for i := 0; i < 10; i++ {
+			req.Metadata[commandSQLKey] = fmt.Sprintf(testInsert, i, i, true, time.Now().Format(MSSQLDateTimeFormat), "{\"key\":\"val\"}")
+			res, err := b.Invoke(context.TODO(), req)
+			assertResponse(t, res, err)
+		}
+	})
+
+	t.Run("Invoke update", func(t *testing.T) {
+		req.Operation = execOperation
+		for i := 0; i < 10; i++ {
+			req.Metadata[commandSQLKey] = fmt.Sprintf(testUpdate, time.Now().Format(MSSQLDateTimeFormat), i)
+			res, err := b.Invoke(context.TODO(), req)
+			assertResponse(t, res, err)
+		}
+	})
+
+	t.Run("Invoke select", func(t *testing.T) {
+		req.Operation = queryOperation
+		req.Metadata[commandSQLKey] = testSelect
+		res, err := b.Invoke(context.TODO(), req)
+		assertResponse(t, res, err)
+		t.Logf("received result: %s", res.Data)
+
+		// verify number, boolean and string
+		assert.Contains(t, string(res.Data), "\"id\":1")
+		assert.Contains(t, string(res.Data), "\"b\":1")
+		assert.Contains(t, string(res.Data), "\"v1\":\"test-1\"")
+		assert.Contains(t, string(res.Data), "\"data\":\"{\\\"key\\\":\\\"val\\\"}\"")
+
+		result := make([]interface{}, 0)
+		err = json.Unmarshal(res.Data, &result)
+		assert.Nil(t, err)
+		assert.Equal(t, 3, len(result))
+
+		// verify timestamp
+		ts, ok := result[0].(map[string]interface{})["ts"].(string)
+		assert.True(t, ok)
+		// have to use custom layout to parse timestamp, see this: https://github.com/dapr/components-contrib/pull/615
+		var tt time.Time
+		tt, err = time.Parse("2006-01-02T15:04:05Z", ts)
+		assert.Nil(t, err)
+		t.Logf("time stamp is: %v", tt)
+	})
+
+	t.Run("Invoke select JSON_EXTRACT", func(t *testing.T) {
+		req.Operation = queryOperation
+		req.Metadata[commandSQLKey] = testSelectJSONExtract
+		res, err := b.Invoke(context.TODO(), req)
+		assertResponse(t, res, err)
+		t.Logf("received result: %s", res.Data)
+
+		// verify json extract number
+		assert.Contains(t, string(res.Data), "{\"key\":\"\\\"val\\\"\"}")
+
+		result := make([]interface{}, 0)
+		err = json.Unmarshal(res.Data, &result)
+		assert.Nil(t, err)
+		assert.Equal(t, 3, len(result))
+	})
+
+	t.Run("Invoke delete", func(t *testing.T) {
+		req.Operation = execOperation
+		req.Metadata[commandSQLKey] = testDelete
+		req.Data = nil
+		res, err := b.Invoke(context.TODO(), req)
+		assertResponse(t, res, err)
+	})
+
+	t.Run("Invoke drop", func(t *testing.T) {
+		req.Operation = execOperation
+		req.Metadata[commandSQLKey] = testDropTable
+		res, err := b.Invoke(context.TODO(), req)
+		assertResponse(t, res, err)
+	})
+
+	t.Run("Invoke close", func(t *testing.T) {
+		req.Operation = closeOperation
+		req.Metadata = nil
+		req.Data = nil
+		_, err := b.Invoke(context.TODO(), req)
+		assert.NoError(t, err)
+	})
+}
+
+func assertResponse(t *testing.T, res *bindings.InvokeResponse, err error) {
+	assert.NoError(t, err)
+	assert.NotNil(t, res)
+	if res != nil {
+		assert.NotNil(t, res.Metadata)
+	}
+}

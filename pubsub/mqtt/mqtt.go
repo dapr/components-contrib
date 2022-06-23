be2fed6b3aab93b140b830d0bf9fc4b2938cd0c0
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2022-06-23 00:46:53 +0000

    MQTT: re-set "retain" to false as default
    Support for "retain" is currently broken
    
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>

--- pubsub/mqtt/mqtt.go
+++ pubsub/mqtt/mqtt.go
@@ -46,7 +46,7 @@ const (
 
 	// Defaults.
 	defaultQOS          = 1
-	defaultRetain       = true
+	defaultRetain       = false
 	defaultWait         = 30 * time.Second
 	defaultCleanSession = false
 )

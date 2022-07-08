06357e4e0b5f73815a5c205324f5219d149d02c2
RcXu <honeyxhl@163.com>
2022-07-09 00:07:24 +0800

    add new component alicloud sls log
    Signed-off-by: RcXu <honeyxhl@163.com>

---
+++ bindings/alicloud/sls/sls.go
@@ -0,0 +1,123 @@
+package sls
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/signal"
+	"strconv"
+	"time"
+
+	sls "github.com/aliyun/aliyun-log-go-sdk"
+	"github.com/aliyun/aliyun-log-go-sdk/producer"
+	"github.com/dapr/components-contrib/bindings"
+	"github.com/dapr/kit/logger"
+)
+
+type AliCloudSlsLogstorage struct {
+	logger   logger.Logger
+	producer *producer.Producer
+	metadata SlsLogstorageMetadata
+}
+
+type SlsLogstorageMetadata struct {
+	Endpoint        string `json:"endpoint"`
+	AccessKeyID     string `json:"accessKeyID"`
+	AccessKeySecret string `json:"accessKeySecret"`
+}
+
+//parse metadata field
+func (s *AliCloudSlsLogstorage) Init(metadata bindings.Metadata) error {
+	m, err := s.parseMeta(metadata)
+	if err != nil {
+		return err
+	}
+	s.metadata = *m
+	producerConfig := producer.GetDefaultProducerConfig()
+	//the config properties in the component yaml file
+	producerConfig.Endpoint = m.Endpoint
+	producerConfig.AccessKeyID = m.AccessKeyID
+	producerConfig.AccessKeySecret = m.AccessKeySecret
+	s.producer = producer.InitProducer(producerConfig)
+	return nil
+}
+
+func NewSlsLogstorage(logger logger.Logger) *AliCloudSlsLogstorage {
+	logger.Debug("new a Slslog component object")
+	s := &AliCloudSlsLogstorage{
+		logger: logger,
+	}
+	return s
+}
+
+func (s *AliCloudSlsLogstorage) Invoke(ctx context.Context, req *bindings.InvokeRequest) (*bindings.InvokeResponse, error) {
+
+	//verify the metadata property
+	if logProject := req.Metadata["project"]; logProject == "" {
+		return nil, fmt.Errorf("SLS binding error: project property not supplied")
+	}
+	if logstore := req.Metadata["logstore"]; logstore == "" {
+		return nil, fmt.Errorf("SLS binding error: logstore property not supplied")
+	}
+	if topic := req.Metadata["topic"]; topic == "" {
+		return nil, fmt.Errorf("SLS binding error: topic property not supplied")
+	}
+	if source := req.Metadata["source"]; source == "" {
+		return nil, fmt.Errorf("SLS binding error: source property not supplied")
+	}
+
+	ch := make(chan os.Signal)
+	signal.Notify(ch, os.Kill, os.Interrupt)
+
+	// Start producer instancce
+	s.producer.Start()
+	log, err := s.parseLog(req)
+	if err != nil {
+		s.logger.Info(err)
+		return nil, err
+	}
+
+	s.logger.Debug(log)
+	err = s.producer.SendLog(req.Metadata["project"], req.Metadata["logstore"], req.Metadata["topic"], req.Metadata["source"], log)
+	if err != nil {
+		s.logger.Info(err)
+		return nil, err
+	}
+	s.producer.SafeClose()
+	return nil, err
+}
+
+func (s *AliCloudSlsLogstorage) parseLog(req *bindings.InvokeRequest) (*sls.Log, error) {
+
+	var logInfo map[string]string
+
+	err := json.Unmarshal(req.Data, &logInfo)
+	if err != nil {
+		return nil, err
+	}
+	var logTime uint32
+	if logInfo["timestamp"] != "" {
+		t, _ := strconv.Atoi(logInfo["timestamp"])
+		logTime = uint32(t)
+	} else {
+		logTime = uint32(time.Now().Unix())
+	}
+
+	return producer.GenerateLog(logTime, logInfo), nil
+}
+
+func (s *AliCloudSlsLogstorage) parseMeta(metadata bindings.Metadata) (*SlsLogstorageMetadata, error) {
+	b, err := json.Marshal(metadata.Properties)
+	if err != nil {
+		return nil, err
+	}
+
+	var m SlsLogstorageMetadata
+	err = json.Unmarshal(b, &m)
+	if err != nil {
+		return nil, err
+	}
+
+	return &m, nil
+}
---
+++ bindings/alicloud/sls/sls_test.go
@@ -0,0 +1,53 @@
+package sls
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/dapr/components-contrib/bindings"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSlsLogstorageMetadata(t *testing.T) {
+	m := bindings.Metadata{}
+	m.Properties = map[string]string{
+		"Endpoint":        "ENDPOINT",
+		"AccessKeyID":     "ACCESSKEYID",
+		"AccessKeySecret": "ACCESSKEYSECRET",
+	}
+	aliCloudSlsLogstorage := AliCloudSlsLogstorage{}
+
+	meta, err := aliCloudSlsLogstorage.parseMeta(m)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "ENDPOINT", meta.Endpoint)
+	assert.Equal(t, "ACCESSKEYID", meta.AccessKeyID)
+	assert.Equal(t, "ACCESSKEYSECRET", meta.AccessKeySecret)
+}
+
+func TestParseLog(t *testing.T) {
+	aliCloudSlsLogstorage := AliCloudSlsLogstorage{}
+	d, _ := json.Marshal(map[string]string{
+		"log1": "LOG1",
+		"log2": "LOG2",
+	})
+	log := bindings.InvokeRequest{
+		Data: d,
+		Metadata: map[string]string{
+			"project":  "PROJECT",
+			"logstore": "LOGSTORE",
+			"topic":    "TOPIC",
+			"source":   "SOURCE",
+		},
+	}
+	parseLog, _ := aliCloudSlsLogstorage.parseLog(&log)
+	for _, v := range parseLog.Contents {
+		switch *v.Key {
+		case "log1":
+			assert.Equal(t, "LOG1", *v.Value)
+		case "log2":
+			assert.Equal(t, "LOG2", *v.Value)
+		}
+	}
+
+}

10c2c980f161831e550a26ce66dd44500af03be1
Ryan Lettieri <ryanLettieri@microsoft.com>
2022-08-01 11:33:45 -0600

    Initial creation of workflows building block
    Signed-off-by: Ryan Lettieri <ryanLettieri@microsoft.com>

---
+++ tests/certification/workflows/temporal/temporal_test.go
@@ -0,0 +1,31 @@
+/*
+Copyright 2021 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package temporal_test
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+	"time"
+	// "github.com/dapr/components-contrib/workflows"
+)
+
+func TestTemporal(t *testing.T) {
+	log := logger.NewLogger("dapr.components")
+	ports, err := dapr_testing.GetFreePorts(2)
+	assert.NoError(t, err)
+
+	currentGrpcPort := ports[0]
+	currentHTTPPort := ports[1]
+}

87b75b696f509235d95f00e763cf29716d267276
Carlos Mendible <cmendible@gmail.com>
2022-08-06 01:20:11 +0200

    Certification tests for env/file secrets stores #1761 (#1846)
    * Certification tests for env/file secrets store #1761
    
    Signed-off-by: cmendible <cmendible@gmail.com>
    
    * using testing setEnv
    
    Signed-off-by: cmendible <cmendible@gmail.com>
    
    * removing nestedSeparator to test default behaviour
    
    Signed-off-by: cmendible <cmendible@gmail.com>
    
    * make modtidy-all
    
    Signed-off-by: Bernd Verst <4535280+berndverst@users.noreply.github.com>
    
    * Remove unnecessary replace in go.mod s
    
    Signed-off-by: Bernd Verst <4535280+berndverst@users.noreply.github.com>
    
    Co-authored-by: Bernd Verst <4535280+berndverst@users.noreply.github.com>
    Co-authored-by: Dapr Bot <56698301+dapr-bot@users.noreply.github.com>

---
+++ tests/certification/secretstores/local/file/filestore_test.go
@@ -0,0 +1,110 @@
+/*
+Copyright 2021 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package filestore_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	// SecretStores
+	"github.com/dapr/components-contrib/secretstores"
+	secretstore_file "github.com/dapr/components-contrib/secretstores/local/file"
+	secretstores_loader "github.com/dapr/dapr/pkg/components/secretstores"
+	"github.com/dapr/dapr/pkg/runtime"
+	dapr_testing "github.com/dapr/dapr/pkg/testing"
+	"github.com/dapr/kit/logger"
+
+	"github.com/dapr/components-contrib/tests/certification/embedded"
+	"github.com/dapr/components-contrib/tests/certification/flow"
+	"github.com/dapr/components-contrib/tests/certification/flow/sidecar"
+	"github.com/dapr/go-sdk/client"
+)
+
+const (
+	sidecarName = "keyvault-sidecar"
+)
+
+func TestEnv(t *testing.T) {
+	ports, err := dapr_testing.GetFreePorts(2)
+	assert.NoError(t, err)
+
+	currentGrpcPort := ports[0]
+	currentHttpPort := ports[1]
+
+	log := logger.NewLogger("dapr.components")
+
+	testGetKnownSecret := func(ctx flow.Context) error {
+		client, err := client.NewClientWithPort(fmt.Sprint(currentGrpcPort))
+		if err != nil {
+			panic(err)
+		}
+		defer client.Close()
+
+		res, err := client.GetSecret(ctx, "file-secret-store", "certtestsecret", nil)
+		assert.NoError(t, err)
+		assert.Equal(t, "abcd", res["certtestsecret"])
+
+		res, err = client.GetSecret(ctx, "file-secret-store", "nestedsecret:secret", nil)
+		assert.NoError(t, err)
+		assert.Equal(t, "efgh", res["nestedsecret:secret"])
+		return nil
+	}
+
+	testGetKnownSecretWithCustomSeparator := func(ctx flow.Context) error {
+		client, err := client.NewClientWithPort(fmt.Sprint(currentGrpcPort))
+		if err != nil {
+			panic(err)
+		}
+		defer client.Close()
+
+		res, err := client.GetSecret(ctx, "file-secret-store", "certtestsecret", nil)
+		assert.NoError(t, err)
+		assert.Equal(t, "abcd", res["certtestsecret"])
+
+		res, err = client.GetSecret(ctx, "file-secret-store", "nestedsecret::secret", nil)
+		assert.NoError(t, err)
+		assert.Equal(t, "efgh", res["nestedsecret::secret"])
+		return nil
+	}
+
+	flow.New(t, "file secret store reads expected value").
+		Step(sidecar.Run(sidecarName,
+			embedded.WithoutApp(),
+			embedded.WithComponentsPath("./components/defaultnestedseparator/"),
+			embedded.WithDaprGRPCPort(currentGrpcPort),
+			embedded.WithDaprHTTPPort(currentHttpPort),
+			runtime.WithSecretStores(
+				secretstores_loader.New("local.file", func() secretstores.SecretStore {
+					return secretstore_file.NewLocalSecretStore(log)
+				}),
+			))).
+		Step("Getting known secret", testGetKnownSecret).
+		Run()
+
+	flow.New(t, "file secret store reads expected value with custom nested separator").
+		Step(sidecar.Run(sidecarName,
+			embedded.WithoutApp(),
+			embedded.WithComponentsPath("./components/customnestedseparator/"),
+			embedded.WithDaprGRPCPort(currentGrpcPort),
+			embedded.WithDaprHTTPPort(currentHttpPort),
+			runtime.WithSecretStores(
+				secretstores_loader.New("local.file", func() secretstores.SecretStore {
+					return secretstore_file.NewLocalSecretStore(log)
+				}),
+			))).
+		Step("Getting known secret", testGetKnownSecretWithCustomSeparator).
+		Run()
+}

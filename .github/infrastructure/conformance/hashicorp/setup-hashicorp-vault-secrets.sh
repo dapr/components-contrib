a2d831f44e74fbc5f159f05a97f84c7245441816
Tiago Alves Macambira <tmacam@burocrata.org>
2022-08-29 21:04:55 -0700

    Conformance tests for Hashicorp Vault.
    This PR adds conformance tests for Hashicorp Vault secret store component.
    This will help brining this component to stable status.
    
    This addresses (but does not fix) #1926.
    
    Signed-off-by: Tiago Alves Macambira <tmacam@burocrata.org>
    Signed-off-by: Ryan Lettieri <ryanLettieri@microsoft.com>

---
+++ .github/infrastructure/conformance/hashicorp/setup-hashicorp-vault-secrets.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Notice that while hashicorp supports multiple keys in a secret,
+# our confirmance tests needs to go for the common demominator 
+# which is a secret store that only has name/value semantic.
+# Hence we setup secret containing a single key with the their
+# same name.
+
+set -eu
+
+MAX_ATTEMPTS=30
+
+for attempt in `seq $MAX_ATTEMPTS`; do
+    # Test connectivity to vault server and create secrets to match
+    # conformance tests / contents from tests/conformance/secrets.json
+    if vault status && 
+        vault kv put secret/dapr/conftestsecret conftestsecret=abcd &&
+        vault kv put secret/dapr/secondsecret secondsecret=efgh;
+    then
+        echo ✅ secrets set;
+        sleep 1;
+        exit 0;
+    else
+        echo "⏰ vault not available, waiting... - attempt $attempt of $MAX_ATTEMPTS";
+        sleep 1;
+    fi
+done;
+
+echo ❌ Failed to set secrets;
+exit 1

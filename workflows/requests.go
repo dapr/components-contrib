079eebfe67406f138d6b36d57cc44c88e4190a05
Ryan Lettieri <ryanLettieri@microsoft.com>
2022-09-02 16:22:28 -0600

    Updates to workflows and temporal for testing and API Changes
    Signed-off-by: Ryan Lettieri <ryanLettieri@microsoft.com>

--- workflows/requests.go
+++ workflows/requests.go
@@ -6,33 +6,16 @@ import (
 	"go.temporal.io/sdk/workflow"
 )
 
-<<<<<<< HEAD
 type WorkflowReference struct {
 	InstanceID string `json:"instance_id"`
-=======
-type StartRequestOptions struct {
-	ID        string `json:"id"`
-	TaskQueue string `json:"task_queue"`
-}
-
-type WorkflowStruct struct {
-	WorkflowId    string `json:"workflow_id"`
-	WorkflowRunId string `json:"workflow_run_id"`
->>>>>>> Updating requests and responses to be more generic
 }
 
 // StartRequest is the object describing a Start Workflow request.
 type StartRequest struct {
-<<<<<<< HEAD
 	Options           map[string]string `json:"workflow_options"`
 	WorkflowReference WorkflowReference `json:"workflow_reference"`
 	WorkflowName      string            `json:"function_name"`
 	Parameters        interface{}       `json:"parameters"`
-=======
-	Options      StartRequestOptions `json:"workflow_options"`
-	WorkflowName string              `json:"workflow_name"`
-	Parameters   interface{}         `json:"parameters"`
->>>>>>> Updating requests and responses to be more generic
 }
 
 // CreateWorkerRequest is the object describing a Create Worker request.

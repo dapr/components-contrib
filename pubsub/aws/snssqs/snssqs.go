98051324b310359058d919fdff4054308462311e
Deepanshu Agarwal <deepanshu.agarwal1984@gmail.com>
2022-09-07 12:05:57 +0530

    snssqs renam batch to bulk
    Signed-off-by: Deepanshu Agarwal <deepanshu.agarwal1984@gmail.com>

--- pubsub/aws/snssqs/snssqs.go
+++ pubsub/aws/snssqs/snssqs.go
@@ -44,7 +44,7 @@ type topicHandler struct {
 }
 
 type snsSqs struct {
-	pubsub.DefaultBatcher
+	pubsub.DefaultBulker
 	// key is the sanitized topic name
 	topicArns map[string]string
 	// key is the sanitized topic name

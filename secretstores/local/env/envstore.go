8eec2a8c06dd3e7a37e93e690d2b1549392cc0c3
Tiago Alves Macambira <tmacam@burocrata.org>
2022-09-13 15:55:39 -0700

    SecretStores advertise supported Features(). (#2069)
    This PR is aimed at addressing issue #2047.
    
    In the [Secret API Documentation](https://docs.dapr.io/reference/api/secrets_api/#response-body) it is stated:
    
    > If a secret store has support for multiple keys in a secret, a JSON payload is returned with the key names as fields and their respective values.
    >
    > In case of a secret store that only has name/value semantics, a JSON payload is returned with the name of the secret as the field and the value of the secret as the value.
    
    There are two classes of secret stores but there isn't a way to tell them apart at run-time. This limits the ability of conformance tests to verify the behavior of secret stores supporting multiple keys.
    
    We address this by augmenting SecretStores with the ability to advetise `Features`. This is similar
    to what PubSub and StateStores do. Feature `MULTIPLE_KEY_VALUES_PER_SECRET` was added and is
    advertised by Hashicorp Vault (default behaviour) and by Local File SecretStore (depending on its configuration).
    
    Updated tests to account to new method and ensure expected behavior.
    
    Fixes #2047
    
    Signed-off-by: Tiago Alves Macambira <tmacam@burocrata.org>
    
    Signed-off-by: Tiago Alves Macambira <tmacam@burocrata.org>
    Co-authored-by: Bernd Verst <4535280+berndverst@users.noreply.github.com>

--- secretstores/local/env/envstore.go
+++ secretstores/local/env/envstore.go
@@ -59,3 +59,8 @@ func (s *envSecretStore) BulkGetSecret(req secretstores.BulkGetSecretRequest) (s
 		Data: r,
 	}, nil
 }
+
+// Features returns the features available in this secret store.
+func (s *envSecretStore) Features() []secretstores.Feature {
+	return []secretstores.Feature{} // No Feature supported.
+}

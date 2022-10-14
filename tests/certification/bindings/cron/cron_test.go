64d8af4c2760c81fd6660f38eac903ec93230858
Sarthak Sharma <sartsharma@microsoft.com>
2022-10-14 13:43:08 +0530

    update tests
    Signed-off-by: Sarthak Sharma <sartsharma@microsoft.com>

--- tests/certification/bindings/cron/cron_test.go
+++ tests/certification/bindings/cron/cron_test.go
@@ -14,9 +14,7 @@ limitations under the License.
 package cron_test
 
 import (
-
 	"context"
-
 	"fmt"
 	"log"
 	"testing"
@@ -40,38 +38,41 @@ import (
 	"github.com/dapr/components-contrib/tests/certification/flow"
 	"github.com/dapr/components-contrib/tests/certification/flow/app"
 	"github.com/dapr/components-contrib/tests/certification/flow/sidecar"
-
 )
 
 func TestCronBinding(t *testing.T) {
-
 	ports, _ := dapr_testing.GetFreePorts(3)
 	grpcPort := ports[0]
 	httpPort := ports[1]
 	appPort := ports[2]
 
-	// application := func(ctx flow.Context, s common.Service) error {
-	// 	// For cron component with invocation @every 1s, check if the app is invoked 10 times within 10 seconds
-	// 	counter := 10
-	// 	cronContext, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	// Test Frequent Invocations
+	application := func(ctx flow.Context, s common.Service) error {
+		// For cron component with invocation @every 1s, check if the app is invoked 10 times within 10 seconds
+		counter := 10
+		cronContext, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 
-	// 	// Setup the input binding endpoint
-	// 	err := s.AddBindingInvocationHandler("cron", func(_ context.Context, in *common.BindingEvent) ([]byte, error) {
-	// 		ctx.Logf("Cron triggered at %s", time.Now().String())
-	// 		log.Printf("Cron triggered at %s", time.Now().String())
-	// 		counter--
-	// 		if cronContext.Err() != nil && counter > 0 {
-	// 			require.NoError(t, cronContext.Err(), "Cron failed to trigger within deadline.")
-	// 			cancel()
-	// 			return nil, cronContext.Err()
-	// 		}
-	// 		return []byte("{}"), nil
-	// 	})
-	// 	require.NoError(t, err)
-	// 	return nil
-	// }
+		// Setup the input binding endpoint
+		err := s.AddBindingInvocationHandler("cron", func(_ context.Context, in *common.BindingEvent) ([]byte, error) {
+			ctx.Logf("Cron triggered at %s", time.Now().String())
+			log.Printf("Cron triggered at %s", time.Now().String())
+			counter--
+			select {
+			case <-cronContext.Done():
+				if counter > 0 {
+					require.NoError(t, cronContext.Err(), "Cron failed to trigger within deadline.")
+					cancel()
+					return nil, cronContext.Err()
+				}
+			default:
+			}
+			return []byte("{}"), nil
+		})
+		require.NoError(t, err)
+		return nil
+	}
 
-	_ = func(ctx flow.Context) error {
+	testDelete := func(ctx flow.Context) error {
 		client := sidecar.GetClient(ctx, "cron-sidecar")
 
 		req := &daprClient.InvokeBindingRequest{Name: "cron", Operation: "delete"}
@@ -82,37 +83,40 @@ func TestCronBinding(t *testing.T) {
 		return nil
 	}
 
-	// flow.New(t, "test cron trigger schedule @every 1s").
-	// 	Step(app.Run("cronapp", fmt.Sprintf(":%d", appPort), application)).
-	// 	Step(sidecar.Run("cron-sidecar",
-	// 		embedded.WithComponentsPath("./components"),
-	// 		embedded.WithDaprGRPCPort(grpcPort),
-	// 		embedded.WithAppProtocol(runtime.HTTPProtocol, appPort),
-	// 		embedded.WithDaprHTTPPort(httpPort),
-	// 		componentRuntimeOptions(),
-	// 	)).
-	// 	// Step("wait for invoke", test).
-	// 	Step("wait for cron invocations", flow.Sleep(time.Second*10)).
-	// 	// Step("interrupt network", network.InterruptNetwork(time.Second*3, nil, nil, strconv.Itoa(appPort), strconv.Itoa(httpPort), strconv.Itoa(grpcPort))).
-	// 	Step("check schedule cancel", testDelete). //test delete separately? there should be no more invocations
-	// 	Step("stop sidecar", sidecar.Stop("cron-sidecar")).
-	// 	Step("stop app", app.Stop("cronapp")).
-	// 	Run()
+	flow.New(t, "test cron trigger schedule @every 1s").
+		Step(app.Run("cronapp", fmt.Sprintf(":%d", appPort), application)).
+		Step(sidecar.Run("cron-sidecar",
+			embedded.WithComponentsPath("./components"),
+			embedded.WithDaprGRPCPort(grpcPort),
+			embedded.WithAppProtocol(runtime.HTTPProtocol, appPort),
+			embedded.WithDaprHTTPPort(httpPort),
+			componentRuntimeOptions(),
+		)).
+		Step("wait for cron invocations", flow.Sleep(time.Second*10)).
+		Step("check schedule cancel", testDelete).
+		Step("stop sidecar", sidecar.Stop("cron-sidecar")).
+		Step("stop app", app.Stop("cronapp")).
+		Run()
 
+	// Test app restart
+	counter := 3
+	cronContext, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	application3s := func(ctx flow.Context, s common.Service) error {
-		// For cron component with invocation @every 3s, check if the app is invoked 3 times within 9 seconds
-		counter := 3
-		cronContext, cancel := context.WithTimeout(context.Background(), 9*time.Second)
+		// For cron component with invocation @every 3s, check if the app is invoked correctly after app restart
 
 		// Setup the input binding endpoint
 		err := s.AddBindingInvocationHandler("cron3s", func(_ context.Context, in *common.BindingEvent) ([]byte, error) {
 			ctx.Logf("Cron triggered at %s", time.Now().String())
 			log.Printf("Cron triggered at %s", time.Now().String())
 			counter--
-			if cronContext.Err() != nil && counter > 0 {
-				require.NoError(t, cronContext.Err(), "Cron failed to trigger within deadline.")
-				cancel()
-				return nil, cronContext.Err()
+			select {
+			case <-cronContext.Done():
+				if counter > 0 {
+					require.NoError(t, cronContext.Err(), "Cron failed to trigger within deadline.")
+					cancel()
+					return nil, cronContext.Err()
+				}
+			default:
 			}
 			return []byte("{}"), nil
 		})
@@ -129,29 +133,34 @@ func TestCronBinding(t *testing.T) {
 			embedded.WithDaprHTTPPort(httpPort),
 			componentRuntimeOptions(),
 		)).
-		Step("wait for cron invocations", flow.Sleep(time.Second*4)).
-		Step("restart app", app.Stop("cronapp3s")).
-		Step("wait during app restart", flow.Sleep(time.Second*10)).
+		Step("start cron invocation", flow.Sleep(time.Second*4)).
+		Step("stop app", app.Stop("cronapp3s")).
+		Step("wait before app restart", flow.Sleep(time.Second*5)).
 		Step(app.Run("cronapp3s", fmt.Sprintf(":%d", appPort), application3s)).
-		Step("wait for remaining cron invocations", flow.Sleep(time.Second*5)).
+		Step("wait for cron invocation", flow.Sleep(time.Second*6)).
 		Step("stop sidecar", sidecar.Stop("cron-sidecar3s")).
 		Step("stop app", app.Stop("cronapp3s")).
 		Run()
 
 	// Test sidecar restart
+	counter = 3
+	cronContext, cancel = context.WithTimeout(context.Background(), 15*time.Second)
 	application3s = func(ctx flow.Context, s common.Service) error {
-		// For cron component with invocation @every 3s, check if the app is invoked 3 times within 9 seconds
-		counter := 3
-		cronContext, cancel := context.WithTimeout(context.Background(), 9*time.Second)
+		// For cron component with invocation @every 3s, check if the app is invoked correctly after sidecar restart
+
 		// Setup the input binding endpoint
 		err := s.AddBindingInvocationHandler("cron3s", func(_ context.Context, in *common.BindingEvent) ([]byte, error) {
 			ctx.Logf("Cron triggered at %s", time.Now().String())
 			log.Printf("Cron triggered at %s", time.Now().String())
 			counter--
-			if cronContext.Err() != nil && counter > 0 {
-				require.NoError(t, cronContext.Err(), "Cron failed to trigger within deadline.")
-				cancel()
-				return nil, cronContext.Err()
+			select {
+			case <-cronContext.Done():
+				if counter > 0 {
+					require.NoError(t, cronContext.Err(), "Cron failed to trigger within deadline.")
+					cancel()
+					return nil, cronContext.Err()
+				}
+			default:
 			}
 			return []byte("{}"), nil
 		})
@@ -169,8 +178,8 @@ func TestCronBinding(t *testing.T) {
 			componentRuntimeOptions(),
 		)).
 		Step("wait for cron invocations", flow.Sleep(time.Second*4)).
-		Step("restart sidecar", sidecar.Stop("cron-sidecar3s")).
-		Step("wait during sidecar restart", flow.Sleep(time.Second*10)).
+		Step("stop sidecar", sidecar.Stop("cron-sidecar3s")).
+		Step("wait before sidecar restart", flow.Sleep(time.Second*5)).
 		Step(sidecar.Run("cron-sidecar3s",
 			embedded.WithComponentsPath("./components"),
 			embedded.WithDaprGRPCPort(grpcPort),
@@ -178,11 +187,11 @@ func TestCronBinding(t *testing.T) {
 			embedded.WithDaprHTTPPort(httpPort),
 			componentRuntimeOptions(),
 		)).
-		Step("stop sidecar", sidecar.Stop("cron-sidecar3s")).
+		Step("wait for cron invocation", flow.Sleep(time.Second*6)).
+		// Step("stop sidecar", sidecar.Stop("cron-sidecar3s")).
 		Step("stop app", app.Stop("cronapp3s")).
 		Run()
 }
-
 
 func componentRuntimeOptions() []runtime.Option {
 	log := logger.NewLogger("dapr.components")

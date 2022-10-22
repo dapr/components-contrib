d86310b3e9c12d19df04e9e6c315596f5b1625d1
Ryan Lettieri <ryanLettieri@microsoft.com>
2022-10-22 02:16:24 -0600

    Removing worker binary from temporal workflow and updating error messages
    Signed-off-by: Ryan Lettieri <ryanLettieri@microsoft.com>

--- workflows/requests.go
+++ workflows/requests.go
@@ -1,11 +1,5 @@
 package workflows
 
-import (
-	"go.temporal.io/sdk/activity"
-	"go.temporal.io/sdk/worker"
-	"go.temporal.io/sdk/workflow"
-)
-
 type WorkflowReference struct {
 	InstanceID string `json:"instance_id"`
 }
@@ -16,19 +10,4 @@ type StartRequest struct {
 	WorkflowReference WorkflowReference `json:"workflow_reference"`
 	WorkflowName      string            `json:"function_name"`
 	Parameters        interface{}       `json:"parameters"`
-}
-
-// CreateWorkerRequest is the object describing a Create Worker request.
-type CreateWorkerRequest struct {
-	TaskQueue     string         `json:"task_queue"`
-	WorkerOptions worker.Options `json:"worker_options"`
-}
-
-// RegisterWorkflowAndActivitiesRequest is the object describing the wfs and acts to assign to a worker
-type RegisterWorkflowAndActivitiesRequest struct {
-	WorkerName      string                   `json:"worker_name"`
-	WorkflowFunc    interface{}              `json:"workflow_func_name"`
-	ActivityFunc    interface{}              `json:"activity_func_name"`
-	WorkflowOptions workflow.RegisterOptions `json:"workflow_register_options"`
-	ActivityOptions activity.RegisterOptions `json:"activity_register_options"`
 }

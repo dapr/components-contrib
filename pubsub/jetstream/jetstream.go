9c5f0858e438a722d1d953d0e04cb065e66fffb3
Tomasz Pietrek <tomasz@nats.io>
2022-10-31 12:52:38 +0100

    wip on binding by name

--- pubsub/jetstream/jetstream.go
+++ pubsub/jetstream/jetstream.go
@@ -15,6 +15,7 @@ package jetstream
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/nats-io/nats.go"
 	"github.com/nats-io/nkeys"
@@ -95,49 +96,67 @@ func (js *jetstreamPubSub) Publish(req *pubsub.PublishRequest) error {
 }
 
 func (js *jetstreamPubSub) Subscribe(ctx context.Context, req pubsub.SubscribeRequest, handler pubsub.Handler) error {
+
+	js.l.Debug("STARTING SUbSCRIBE")
+
 	var opts []nats.SubOpt
+	var consumerConfig nats.ConsumerConfig
 
 	if v := js.meta.durableName; v != "" {
 		opts = append(opts, nats.Durable(v))
+		consumerConfig.Durable = v
 	}
 
 	if v := js.meta.startTime; !v.IsZero() {
 		opts = append(opts, nats.StartTime(v))
+		consumerConfig.OptStartTime = &v
 	} else if v := js.meta.startSequence; v > 0 {
 		opts = append(opts, nats.StartSequence(v))
+		consumerConfig.OptStartSeq = v
 	} else if js.meta.deliverAll {
 		opts = append(opts, nats.DeliverAll())
+		consumerConfig.DeliverPolicy = nats.DeliverAllPolicy
 	} else {
 		opts = append(opts, nats.DeliverLast())
+		consumerConfig.DeliverPolicy = nats.DeliverLastPolicy
 	}
 
 	if js.meta.flowControl {
 		opts = append(opts, nats.EnableFlowControl())
+		consumerConfig.FlowControl = true
 	}
 
 	if js.meta.ackWait != 0 {
 		opts = append(opts, nats.AckWait(js.meta.ackWait))
+		consumerConfig.AckWait = js.meta.ackWait
 	}
 	if js.meta.maxDeliver != 0 {
 		opts = append(opts, nats.MaxDeliver(js.meta.maxDeliver))
+		consumerConfig.MaxDeliver = js.meta.maxDeliver
 	}
 	if len(js.meta.backOff) != 0 {
 		opts = append(opts, nats.BackOff(js.meta.backOff))
+		consumerConfig.BackOff = js.meta.backOff
 	}
 	if js.meta.maxAckPending != 0 {
 		opts = append(opts, nats.MaxAckPending(js.meta.maxAckPending))
+		consumerConfig.MaxAckPending = js.meta.maxAckPending
 	}
 	if js.meta.replicas != 0 {
 		opts = append(opts, nats.ConsumerReplicas(js.meta.replicas))
+		consumerConfig.Replicas = js.meta.replicas
 	}
 	if js.meta.memoryStorage {
 		opts = append(opts, nats.ConsumerMemoryStorage())
+		consumerConfig.MemoryStorage = true
 	}
 	if js.meta.rateLimit != 0 {
 		opts = append(opts, nats.RateLimit(js.meta.rateLimit))
+		consumerConfig.RateLimit = js.meta.rateLimit
 	}
 	if js.meta.hearbeat != 0 {
 		opts = append(opts, nats.IdleHeartbeat(js.meta.hearbeat))
+		consumerConfig.Heartbeat = js.meta.hearbeat
 	}
 
 	natsHandler := func(m *nats.Msg) {
@@ -177,6 +196,23 @@ func (js *jetstreamPubSub) Subscribe(ctx context.Context, req pubsub.SubscribeRe
 
 	var err error
 	var subscription *nats.Subscription
+	if js.meta.streamName != "" {
+		js.l.Debug("STREAM NAME IS EMPTY")
+		fmt.Println("STREAM NAME IS NOT EMPTY")
+
+		info, err := js.jsc.StreamInfo(js.meta.streamName)
+		if err != nil {
+			return err
+		}
+
+		consumerInfo, err := js.jsc.AddConsumer(info.Config.Name, &consumerConfig)
+		if err != nil {
+			return err
+		}
+		opts = append(opts, nats.Bind(info.Config.Name, consumerInfo.Name))
+	}
+	fmt.Println("OPTS: ", opts)
+
 	if queue := js.meta.queueGroupName; queue != "" {
 		js.l.Debugf("nats: subscribed to subject %s with queue group %s",
 			req.Topic, js.meta.queueGroupName)

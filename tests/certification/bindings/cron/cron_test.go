858b2501f2ac69479bdf2ad3490ebdb3c282f359
Sarthak Sharma <sartsharma@microsoft.com>
2022-11-08 21:02:31 +0530

    parameterise application and assertions
    Signed-off-by: Sarthak Sharma <sartsharma@microsoft.com>

--- tests/certification/bindings/cron/cron_test.go
+++ tests/certification/bindings/cron/cron_test.go
@@ -82,7 +82,7 @@ func TestCronBindingFrequentTrigger(t *testing.T) {
 		Step("allow cron to trigger if delete operation failed", flow.Sleep(waitAfterDelete)).
 		Step("assert cron did not trigger after delete", assertTriggerDelete(t, &triggeredCountAtDelete, &triggeredCount)).
 		Step("stop sidecar", sidecar.Stop(sidecarName)).
-		Step("stop app", app.Stop("cronapp")).
+		Step("stop app", app.Stop(appName)).
 		Run()
 }
 
@@ -93,46 +93,52 @@ func TestCronBindingWithAppRestart(t *testing.T) {
 	httpPort := ports[1]
 	appPort := ports[2]
 
+	cronName := "cron3s"
+	appName := "cronapp3s"
+	sidecarName := "cron-sidecar"
+
+	// Test if cron triggers 5 times within 15 seconds
+	expectedTriggerCount := 5
+	timeoutToObeserveTrigger := time.Second * 15
+
+	// allow cron to trigger for some time before stopping the app
+	waitBeforeAppStop := time.Second * 5
+
+	// wait for some time after the app has stopped, before restarting the app
+	waitBeforeAppRestart := time.Second * 5
+
+	// wait after delete operation to check if cron still triggers
+	waitAfterDelete := time.Second * 10
+
 	triggerWatcher := watcher.NewOrdered()
-	for i := 1; i <= 5; i++ {
+	for i := 1; i <= expectedTriggerCount; i++ {
 		triggerWatcher.ExpectInts(i)
 	}
 
-	// Test if cron triggers 5 times within 15 seconds
-	testAssert := func(ctx flow.Context) error {
-		triggerWatcher.Assert(ctx, time.Second*15)
-		return nil
-	}
-
-	application := func(ctx flow.Context, s common.Service) error {
-		triggeredCount := 0
-		// Setup the input binding endpoint
-		err := s.AddBindingInvocationHandler("cron3s", func(_ context.Context, in *common.BindingEvent) ([]byte, error) {
-			ctx.Logf("Cron triggered at %s", time.Now().String())
-			triggeredCount++
-			triggerWatcher.Observe(triggeredCount)
-			return []byte("{}"), nil
-		})
-		require.NoError(t, err)
-		return nil
-	}
+	// total times cron is triggered
+	triggeredCount := 0
+	// store triggered count at the time of cron delete operation
+	var triggeredCountAtDelete int
 
 	flow.New(t, "test cron trigger schedule @every3s with app restart").
-		Step(app.Run("cronapp", fmt.Sprintf(":%d", appPort), application)).
-		Step(sidecar.Run("cron-sidecar",
+		Step(app.Run(appName, fmt.Sprintf(":%d", appPort), appWithTriggerCounter(t, cronName, triggerWatcher, &triggeredCount))).
+		Step(sidecar.Run(sidecarName,
 			embedded.WithComponentsPath("./components"),
 			embedded.WithDaprGRPCPort(grpcPort),
 			embedded.WithAppProtocol(runtime.HTTPProtocol, appPort),
 			embedded.WithDaprHTTPPort(httpPort),
 			componentRuntimeOptions(),
 		)).
-		Step("allow cron to trigger once", flow.Sleep(time.Second*4)).
-		Step("stop app", app.Stop("cronapp")).
-		Step("wait before app restart", flow.Sleep(time.Second*5)).
-		Step(app.Run("cronapp", fmt.Sprintf(":%d", appPort), application)).
-		Step("assert cron triggered within deadline", testAssert).
-		Step("stop sidecar", sidecar.Stop("cron-sidecar3s")).
-		Step("stop app", app.Stop("cronapp3s")).
+		Step("allow cron to trigger once", flow.Sleep(waitBeforeAppStop)).
+		Step("stop app", app.Stop(appName)).
+		Step("wait before app restart", flow.Sleep(waitBeforeAppRestart)).
+		Step(app.Run(appName, fmt.Sprintf(":%d", appPort), appWithTriggerCounter(t, cronName, triggerWatcher, &triggeredCount))).
+		Step("assert cron triggered within deadline", assertTriggerCount(triggerWatcher, timeoutToObeserveTrigger)).
+		Step("delete cron trigger", deleteCronTrigger(t, cronName, sidecarName, &triggeredCount, &triggeredCountAtDelete)).
+		Step("allow cron to trigger if delete operation failed", flow.Sleep(waitAfterDelete)).
+		Step("assert cron did not trigger after delete", assertTriggerDelete(t, &triggeredCountAtDelete, &triggeredCount)).
+		Step("stop sidecar", sidecar.Stop(sidecarName)).
+		Step("stop app", app.Stop(appName)).
 		Run()
 }
 
@@ -143,52 +149,58 @@ func TestCronBindingWithSidecarRestart(t *testing.T) {
 	httpPort := ports[1]
 	appPort := ports[2]
 
+	cronName := "cron3s"
+	appName := "cronapp3s"
+	sidecarName := "cron-sidecar"
+
+	// Test if cron triggers 5 times within 15 seconds
+	expectedTriggerCount := 5
+	timeoutToObeserveTrigger := time.Second * 15
+
+	// allow cron to trigger for some time before stopping the sidecar
+	waitBeforeSidecarStop := time.Second * 5
+
+	// wait for some time after the app has stopped, before restarting the sidecar
+	waitBeforeSidecarRestart := time.Second * 5
+
+	// wait after delete operation to check if cron still triggers
+	waitAfterDelete := time.Second * 10
+
 	triggerWatcher := watcher.NewOrdered()
-	for i := 1; i <= 5; i++ {
+	for i := 1; i <= expectedTriggerCount; i++ {
 		triggerWatcher.ExpectInts(i)
 	}
 
-	// Test if cron triggers 5 times within 15 seconds
-	testAssert := func(ctx flow.Context) error {
-		triggerWatcher.Assert(ctx, time.Second*15)
-		return nil
-	}
-
-	application := func(ctx flow.Context, s common.Service) error {
-		triggeredCount := 0
-		// Setup the input binding endpoint
-		err := s.AddBindingInvocationHandler("cron3s", func(_ context.Context, in *common.BindingEvent) ([]byte, error) {
-			ctx.Logf("Cron triggered at %s", time.Now().String())
-			triggeredCount++
-			triggerWatcher.Observe(triggeredCount)
-			return []byte("{}"), nil
-		})
-		require.NoError(t, err)
-		return nil
-	}
+	// total times cron is triggered
+	triggeredCount := 0
+	// store triggered count at the time of cron delete operation
+	var triggeredCountAtDelete int
 
 	flow.New(t, "test cron trigger schedule @every 3s with sidecar restart").
-		Step(app.Run("cronapp", fmt.Sprintf(":%d", appPort), application)).
-		Step(sidecar.Run("cron-sidecar",
+		Step(app.Run(appName, fmt.Sprintf(":%d", appPort), appWithTriggerCounter(t, cronName, triggerWatcher, &triggeredCount))).
+		Step(sidecar.Run(sidecarName,
 			embedded.WithComponentsPath("./components"),
 			embedded.WithDaprGRPCPort(grpcPort),
 			embedded.WithAppProtocol(runtime.HTTPProtocol, appPort),
 			embedded.WithDaprHTTPPort(httpPort),
 			componentRuntimeOptions(),
 		)).
-		Step("allow cron to trigger once", flow.Sleep(time.Second*4)).
-		Step("stop sidecar", sidecar.Stop("cron-sidecar")).
-		Step("wait before sidecar restart", flow.Sleep(time.Second*5)).
-		Step(sidecar.Run("cron-sidecar",
+		Step("allow cron to trigger once", flow.Sleep(waitBeforeSidecarStop)).
+		Step("stop sidecar", sidecar.Stop(sidecarName)).
+		Step("wait before sidecar restart", flow.Sleep(waitBeforeSidecarRestart)).
+		Step(sidecar.Run(sidecarName,
 			embedded.WithComponentsPath("./components"),
 			embedded.WithDaprGRPCPort(grpcPort),
 			embedded.WithAppProtocol(runtime.HTTPProtocol, appPort),
 			embedded.WithDaprHTTPPort(httpPort),
 			componentRuntimeOptions(),
 		)).
-		Step("assert cron triggered within deadline", testAssert).
-		Step("stop sidecar", sidecar.Stop("cron-sidecar")).
-		Step("stop app", app.Stop("cronapp")).
+		Step("assert cron triggered within deadline", assertTriggerCount(triggerWatcher, timeoutToObeserveTrigger)).
+		Step("delete cron trigger", deleteCronTrigger(t, cronName, sidecarName, &triggeredCount, &triggeredCountAtDelete)).
+		Step("allow cron to trigger if delete operation failed", flow.Sleep(waitAfterDelete)).
+		Step("assert cron did not trigger after delete", assertTriggerDelete(t, &triggeredCountAtDelete, &triggeredCount)).
+		Step("stop sidecar", sidecar.Stop(sidecarName)).
+		Step("stop app", app.Stop(appName)).
 		Run()
 }
 
@@ -197,7 +209,6 @@ func appWithTriggerCounter(t *testing.T, cronName string, triggerWatcher *watche
 		// Setup the input binding endpoint
 		err := s.AddBindingInvocationHandler(cronName, func(_ context.Context, in *common.BindingEvent) ([]byte, error) {
 			ctx.Logf("Cron triggered at %s", time.Now().String())
-			fmt.Println("Cron triggered at ", time.Now().String())
 			(*triggeredCount)++
 			triggerWatcher.Observe(*triggeredCount)
 			return []byte("{}"), nil

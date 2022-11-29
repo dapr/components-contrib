35bb4e40458462b25a98edd5b8551c0f48e76cb4
shivam <shivamkm07@gmail.com>
2022-11-29 08:23:18 +0530

    Testing even higher value of timeout
    Signed-off-by: shivam <shivamkm07@gmail.com>

--- tests/conformance/pubsub/pubsub.go
+++ tests/conformance/pubsub/pubsub.go
@@ -43,7 +43,7 @@ const (
 	defaultMultiTopic1Name        = "multiTopic1"
 	defaultMultiTopic2Name        = "multiTopic2"
 	defaultMessageCount           = 10
-	defaultMaxReadDuration        = 180 * time.Second
+	defaultMaxReadDuration        = 240 * time.Second
 	defaultWaitDurationToPublish  = 5 * time.Second
 	defaultCheckInOrderProcessing = true
 	defaultMaxBulkCount           = 5

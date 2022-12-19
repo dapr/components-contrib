96d22f461047f0c71e7cafa41c456c6cac6ab4ef
Bernd Verst <4535280+berndverst@users.noreply.github.com>
2022-12-19 15:52:11 -0800

    Update internal/component/kafka/consumer.go
    Co-authored-by: Alessandro (Ale) Segala <43508+ItalyPaleAle@users.noreply.github.com>
    Signed-off-by: Bernd Verst <4535280+berndverst@users.noreply.github.com>

--- internal/component/kafka/consumer.go
+++ internal/component/kafka/consumer.go
@@ -189,7 +189,7 @@ func (consumer *consumer) doCallback(session sarama.ConsumerGroupSession, messag
 		Data:  message.Value,
 	}
 	// This is true only when headers are set (Kafka > 0.11)
-	if message.Headers != nil && len(message.Headers) > 0 {
+	if len(message.Headers) > 0 {
 		event.Metadata = make(map[string]string, len(message.Headers))
 		for _, header := range message.Headers {
 			event.Metadata[string(header.Key)] = string(header.Value)

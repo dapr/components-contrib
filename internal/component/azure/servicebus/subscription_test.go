3b0ff0a49e0455fc1858d184537e452900e25e3b
Joni Collinge <jonathancollinge@live.com>
2023-01-06 17:59:26 +0000

    Add support for Azure Service Bus sessions for topics (#2346)
    * WIP: ASB sessions
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * WIP: Support ASB sessions
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * Add metadata
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * remove dead code
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * lint
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * Add sessionIdleTimeoutInSec md
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * Support blank session IDs
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * If require sessions and no session id, set blank
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * move session config to per subscription metadata
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * add some unit tests
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * lint
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * remove auto blank session id
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * go mod tidy
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * add basic cert test
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * update comment
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * remove dead code
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * remove dead code
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * Update certification test
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * Add comment
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * Add cert test for roundrobin
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * go mod tidy
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * Update readme
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * Use struct for NewSubscription to simplify parameters
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * Address feedback
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * Address feedback
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * Only async receive
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * isTruthy on map without check
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * Use if not select for context check
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * Feedback
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * lint
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    Signed-off-by: Bernd Verst <4535280+berndverst@users.noreply.github.com>
    Co-authored-by: Yaron Schneider <schneider.yaron@live.com>
    Co-authored-by: Bernd Verst <4535280+berndverst@users.noreply.github.com>
    Co-authored-by: Alessandro (Ale) Segala <43508+ItalyPaleAle@users.noreply.github.com>

--- internal/component/azure/servicebus/subscription_test.go
+++ internal/component/azure/servicebus/subscription_test.go
@@ -27,42 +27,57 @@ func TestNewSubscription(t *testing.T) {
 		maxBulkSubCountParam            *int
 		maxBulkSubCountExpected         int
 		activeOperationsChanCapExpected int
+		requireSessionsParam            bool
 	}{
 		{
 			"maxBulkSubCount passed is 0",
 			ptr.Of(0),
 			1,
 			1000,
+			false,
 		},
 		{
 			"maxBulkSubCount passed is negative",
 			ptr.Of(-100),
 			1,
 			1000,
+			false,
 		},
 		{
 			"maxBulkSubCount passed is positive",
 			ptr.Of(100),
 			100,
 			10,
+			false,
 		},
 		{
 			"maxBulkSubCount passed is nil",
 			nil,
 			1,
 			1000,
+			false,
+		},
+		{
+			"requireSessions is true",
+			ptr.Of(0),
+			1,
+			1000,
+			true,
 		},
 	}
 	for _, tc := range testcases {
 		t.Run(tc.name, func(t *testing.T) {
 			sub := NewSubscription(
-				context.Background(),
-				1000,
-				1,
-				tc.maxBulkSubCountParam,
-				10,
-				100,
-				"test",
+				context.Background(), SubsriptionOptions{
+					MaxActiveMessages:     1000,
+					TimeoutInSec:          1,
+					MaxBulkSubCount:       tc.maxBulkSubCountParam,
+					MaxRetriableEPS:       10,
+					MaxConcurrentHandlers: 100,
+					Entity:                "test",
+					LockRenewalInSec:      30,
+					RequireSessions:       tc.requireSessionsParam,
+				},
 				logger.NewLogger("test"),
 			)
 			if sub.maxBulkSubCount != tc.maxBulkSubCountExpected {
@@ -70,6 +85,9 @@ func TestNewSubscription(t *testing.T) {
 			}
 			if cap(sub.activeOperationsChan) != tc.activeOperationsChanCapExpected {
 				t.Errorf("Expected capacity of sub.activeOperationsChan to be %d but got %d", tc.activeOperationsChanCapExpected, cap(sub.activeOperationsChan))
+			}
+			if sub.requireSessions != tc.requireSessionsParam {
+				t.Errorf("Expected requireSessions to be %t but got %t", tc.requireSessionsParam, sub.requireSessions)
 			}
 		})
 	}

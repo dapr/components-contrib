382f84f30c2c7f97de3e32880e54e0e354211271
Joni Collinge <jonathancollinge@live.com>
2023-01-06 17:59:26 +0000

    Add support for Azure Service Bus sessions for topics (#2346)
    * WIP: ASB sessions
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * WIP: Support ASB sessions
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * Add metadata
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * remove dead code
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * lint
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * Add sessionIdleTimeoutInSec md
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * Support blank session IDs
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * If require sessions and no session id, set blank
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * move session config to per subscription metadata
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * add some unit tests
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * lint
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * remove auto blank session id
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * go mod tidy
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * add basic cert test
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * update comment
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * remove dead code
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * remove dead code
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * Update certification test
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * Add comment
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * Add cert test for roundrobin
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * go mod tidy
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * Update readme
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * Use struct for NewSubscription to simplify parameters
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * Address feedback
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * Address feedback
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * Only async receive
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * isTruthy on map without check
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * Use if not select for context check
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * Feedback
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    * lint
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    Signed-off-by: Bernd Verst <4535280+berndverst@users.noreply.github.com>
    Co-authored-by: Yaron Schneider <schneider.yaron@live.com>
    Co-authored-by: Bernd Verst <4535280+berndverst@users.noreply.github.com>
    Co-authored-by: Alessandro (Ale) Segala <43508+ItalyPaleAle@users.noreply.github.com>

---
+++ internal/component/azure/servicebus/receiver.go
@@ -0,0 +1,103 @@
+/*
+Copyright 2021 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package servicebus
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	azservicebus "github.com/Azure/azure-sdk-for-go/sdk/messaging/azservicebus"
+	"go.uber.org/multierr"
+)
+
+type Receiver interface {
+	ReceiveMessages(ctx context.Context, maxMessages int, options *azservicebus.ReceiveMessagesOptions) ([]*azservicebus.ReceivedMessage, error)
+	CompleteMessage(ctx context.Context, m *azservicebus.ReceivedMessage, opts *azservicebus.CompleteMessageOptions) error
+	AbandonMessage(ctx context.Context, m *azservicebus.ReceivedMessage, opts *azservicebus.AbandonMessageOptions) error
+	Close(ctx context.Context) error
+}
+
+var (
+	_ Receiver = (*SessionReceiver)(nil)
+	_ Receiver = (*MessageReceiver)(nil)
+)
+
+func NewSessionReceiver(r *azservicebus.SessionReceiver) *SessionReceiver {
+	return &SessionReceiver{SessionReceiver: r}
+}
+
+type SessionReceiver struct {
+	*azservicebus.SessionReceiver
+}
+
+func (s *SessionReceiver) RenewSessionLocks(ctx context.Context, timeoutInSec int) error {
+	if s == nil {
+		return nil
+	}
+
+	lockCtx, lockCancel := context.WithTimeout(ctx, time.Second*time.Duration(timeoutInSec))
+	defer lockCancel()
+
+	return s.RenewSessionLock(lockCtx, nil)
+}
+
+func NewMessageReceiver(r *azservicebus.Receiver) *MessageReceiver {
+	return &MessageReceiver{Receiver: r}
+}
+
+type MessageReceiver struct {
+	*azservicebus.Receiver
+}
+
+func (m *MessageReceiver) RenewMessageLocks(ctx context.Context, msgs []*azservicebus.ReceivedMessage, timeoutInSec int) error {
+	if m == nil {
+		return nil
+	}
+
+	var wg sync.WaitGroup
+
+	errChan := make(chan error, len(msgs))
+	for _, msg := range msgs {
+		wg.Add(1)
+
+		go func(rmsg *azservicebus.ReceivedMessage) {
+			defer wg.Done()
+
+			lockCtx, lockCancel := context.WithTimeout(ctx, time.Second*time.Duration(timeoutInSec))
+			defer lockCancel()
+
+			// Renew the lock for the message.
+			err := m.RenewMessageLock(lockCtx, rmsg, nil)
+			if err != nil {
+				errChan <- fmt.Errorf("couldn't renew active message lock for message %s, %w", rmsg.MessageID, err)
+			}
+		}(msg)
+	}
+
+	wg.Wait()
+	close(errChan)
+
+	errs := []error{}
+	for err := range errChan {
+		errs = append(errs, err)
+	}
+
+	if len(errs) > 0 {
+		return multierr.Combine(errs...)
+	}
+
+	return nil
+}

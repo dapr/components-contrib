ebc818116fb55c040fd46f3c7cc037b02b325d23
Roberto J Rojas <robertojrojas@gmail.com>
2023-01-11 18:57:54 -0500

    adds conformance tests
    Signed-off-by: Roberto J Rojas <robertojrojas@gmail.com>

---
+++ .github/infrastructure/terraform/conformance/state/aws/dynamodb/dynamodb.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_version = ">=0.13"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+variable "TIMESTAMP" {
+  type        = string
+  description = "Timestamp of the github worklow run."
+}
+
+variable "UNIQUE_ID" {
+  type        = string
+  description = "Unique Id of the github worklow run."
+}
+
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      Purpose   = "AutomatedConformanceTesting"
+      Timestamp = "${var.TIMESTAMP}"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "conformance_test_table" {
+  name           = "conformance-test-terraform-${var.UNIQUE_ID}"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = "10"
+  write_capacity = "10"
+  attribute {
+    name = "key"
+    type = "S"
+  }
+  hash_key = "key"
+}

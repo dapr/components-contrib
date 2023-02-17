c6ed4256f7e1d34f659238869231929ecf256076
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2023-02-17 19:13:06 +0000

    More fixes
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>

--- .github/scripts/components-scripts/certification-state.aws.dynamodb-destroy.sh
+++ .github/scripts/components-scripts/certification-state.aws.dynamodb-destroy.sh
@@ -3,7 +3,7 @@
 set +e
 
 # Navigate to the Terraform directory
-cd ".github/infrastructure/terraform/certification/pubsub/aws/dynamodb"
+cd ".github/infrastructure/terraform/certification/state/aws/dynamodb"
 
 # Run Terraform
 terraform destroy -auto-approve -var="UNIQUE_ID=$UNIQUE_ID" -var="TIMESTAMP=$CURRENT_TIME"

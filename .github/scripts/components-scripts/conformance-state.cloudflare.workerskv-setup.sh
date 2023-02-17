749b5f2f252513b6671133e6617f006774b996d8
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2023-02-17 19:11:03 +0000

    Move code that checks for CF Workers into the setup script
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>

--- .github/scripts/components-scripts/conformance-state.cloudflare.workerskv-setup.sh
+++ .github/scripts/components-scripts/conformance-state.cloudflare.workerskv-setup.sh
@@ -2,8 +2,21 @@
 
 set -e
 
+# Rebuild the Worker
+(
+  cd internal/component/cloudflare/worker-src;
+  npm ci;
+  npm run build;
+)
+
+# Check that the code of the worker is correct
+git diff --exit-code ./internal/component/cloudflare/workers/code \
+  || (echo "The source code of the Cloudflare Worker has changed, but the Worker has not been recompiled. Please re-compile the Worker by running 'npm ci && npm run build' in 'internal/component/cloudflare/worker-src'" && exit 1)
+
 # Remove dashes from UNIQUE_ID
 Suffix=$(echo "$UNIQUE_ID" | sed -E 's/-//g')
+
+# Ensure the Workers KV namespace exists
 CloudflareWorkerName="daprconfkv${Suffix}"
 CloudflareKVNamespaceID=$( curl -s -X POST "https://api.cloudflare.com/client/v4/accounts/${CLOUDFLARE_ACCOUNT_ID}/storage/kv/namespaces" \
   -H "Authorization: Bearer ${CLOUDFLARE_API_TOKEN}" \

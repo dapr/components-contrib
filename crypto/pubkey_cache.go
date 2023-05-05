306e9c488ee7de1bb4add7a98e513318d13ce90f
joshvanl <me@joshvanl.dev>
2023-05-05 12:23:08 +0100

    Update crypto/pubkey_cache to use context pooling when fetching keys.
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- crypto/pubkey_cache.go
+++ crypto/pubkey_cache.go
@@ -19,6 +19,8 @@ import (
 
 	"github.com/chebyrash/promise"
 	"github.com/lestrrat-go/jwx/v2/jwk"
+
+	"github.com/dapr/components-contrib/internal/utils"
 )
 
 // GetKeyFn is the type of the getKeyFn function used by the PubKeyCache.
@@ -28,47 +30,67 @@ type GetKeyFn = func(ctx context.Context, key string) func(resolve func(jwk.Key)
 // entries so that multiple callers getting the same key at the same time
 // (where the key is not in the cache yet), will result in only a single key
 // fetch.
+// Each cache item uses a context pool so that a key fetch call will only be
+// cancelled once all callers have cancelled their context.
 type PubKeyCache struct {
 	getKeyFn GetKeyFn
 
-	pubKeys     map[string]*promise.Promise[jwk.Key]
-	pubKeysLock sync.Mutex
+	pubKeys map[string]pubKeyCacheEntry
+	lock    sync.Mutex
+}
+
+type pubKeyCacheEntry struct {
+	promise *promise.Promise[jwk.Key]
+	ctx     *utils.ContextPool
 }
 
 // NewPubKeyCache returns a new PubKeyCache object
 func NewPubKeyCache(getKeyFn GetKeyFn) *PubKeyCache {
 	return &PubKeyCache{
 		getKeyFn: getKeyFn,
-		pubKeys:  map[string]*promise.Promise[jwk.Key]{},
+		pubKeys:  make(map[string]pubKeyCacheEntry),
 	}
 }
 
 // GetKey returns a public key from the cache, or uses getKeyFn to request it.
 func (kc *PubKeyCache) GetKey(ctx context.Context, key string) (jwk.Key, error) {
-	timeoutPromise := promise.New(func(_ func(jwk.Key), reject func(error)) {
-		<-ctx.Done()
-		reject(ctx.Err())
-	})
-
 	// Check if the key is in the cache already
-	kc.pubKeysLock.Lock()
+	kc.lock.Lock()
 	p, ok := kc.pubKeys[key]
 	if ok {
-		kc.pubKeysLock.Unlock()
-		return promise.Race(p, timeoutPromise).Await()
+		// Add the context to the context pool and return the promise (which may
+		// already be resolved).
+		kc.pubKeys[key].ctx.Add(ctx)
+		kc.lock.Unlock()
+		jwkKey, err := p.promise.Await(ctx)
+		if err != nil {
+			return nil, err
+		}
+		return *jwkKey, nil
 	}
 
 	// Key is not in the cache, create the promise in the cache and return
-	// result.
-	p = promise.New(kc.getKeyFn(ctx, key))
-	p = promise.Catch(p, func(err error) error {
-		kc.pubKeysLock.Lock()
-		delete(kc.pubKeys, key)
-		kc.pubKeysLock.Unlock()
-		return err
-	})
+	// result. Create a new context pool for the promise. Close the pool on
+	// return so that the context pool doesn't expand indefinitely on cache
+	// reads.
+	p.ctx = utils.NewContextPool(ctx)
+	defer p.ctx.Close()
+	p.promise = promise.Catch(
+		promise.New(kc.getKeyFn(p.ctx, key)),
+		p.ctx,
+		func(err error) error {
+			kc.lock.Lock()
+			delete(kc.pubKeys, key)
+			kc.lock.Unlock()
+			return err
+		},
+	)
 	kc.pubKeys[key] = p
-	kc.pubKeysLock.Unlock()
+	kc.lock.Unlock()
 
-	return promise.Race(p, timeoutPromise).Await()
+	jwkKey, err := p.promise.Await(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return *jwkKey, nil
 }

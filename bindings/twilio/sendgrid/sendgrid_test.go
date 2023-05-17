8435a52cbd10c2d891e91ef21c30d160a20d4fd9
Yann Nicolas <yannart@gmail.com>
2023-05-17 17:04:24 +0200

    Add dynamic template support to Twilio Sendgrid binding (#2844)
    Signed-off-by: Yann Nicolas <yannart@gmail.com>
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
    Co-authored-by: Alessandro (Ale) Segala <43508+ItalyPaleAle@users.noreply.github.com>

--- bindings/twilio/sendgrid/sendgrid_test.go
+++ bindings/twilio/sendgrid/sendgrid_test.go
@@ -14,6 +14,7 @@ limitations under the License.
 package sendgrid
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -42,8 +43,24 @@ func TestParseMetadataWithOptionalNames(t *testing.T) {
 	logger := logger.NewLogger("test")
 
 	t.Run("Has correct metadata", func(t *testing.T) {
+		// Sample nested JSON with Dynamic Template Data
+		dynamicTemplateData, _ := json.Marshal(map[string]any{
+			"name": map[string]any{
+				"first": "MyFirst",
+				"last":  "MyLast",
+			},
+		})
+
 		m := bindings.Metadata{}
-		m.Properties = map[string]string{"apiKey": "123", "emailFrom": "test1@example.net", "emailFromName": "test 1", "emailTo": "test2@example.net", "emailToName": "test 2", "subject": "hello"}
+		m.Properties = map[string]string{
+			"apiKey":        "123",
+			"emailFrom":     "test1@example.net",
+			"emailFromName": "test 1",
+			"emailTo":       "test2@example.net",
+			"emailToName":   "test 2", "subject": "hello",
+			"dynamicTemplateData": string(dynamicTemplateData),
+			"dynamicTemplateId":   "456",
+		}
 		r := SendGrid{logger: logger}
 		sgMeta, err := r.parseMetadata(m)
 		assert.Nil(t, err)
@@ -53,5 +70,43 @@ func TestParseMetadataWithOptionalNames(t *testing.T) {
 		assert.Equal(t, "test2@example.net", sgMeta.EmailTo)
 		assert.Equal(t, "test 2", sgMeta.EmailToName)
 		assert.Equal(t, "hello", sgMeta.Subject)
+		assert.Equal(t, `{"name":{"first":"MyFirst","last":"MyLast"}}`, sgMeta.DynamicTemplateData)
+		assert.Equal(t, "456", sgMeta.DynamicTemplateID)
+	})
+
+	t.Run("Has incorrect template data metadata", func(t *testing.T) {
+		m := bindings.Metadata{}
+		m.Properties = map[string]string{
+			"apiKey":              "123",
+			"emailFrom":           "test1@example.net",
+			"emailFromName":       "test 1",
+			"emailTo":             "test2@example.net",
+			"emailToName":         "test 2",
+			"subject":             "hello",
+			"dynamicTemplateData": `{"wrong"}`,
+			"dynamicTemplateId":   "456",
+		}
+		r := SendGrid{logger: logger}
+		_, err := r.parseMetadata(m)
+		assert.Error(t, err)
+	})
+}
+
+// Test UnmarshalDynamicTemplateData function
+func TestUnmarshalDynamicTemplateData(t *testing.T) {
+	t.Run("Test Template Data", func(t *testing.T) {
+		// Sample nested JSON with Dynamic Template Data
+		dynamicTemplateData, _ := json.Marshal(map[string]interface{}{"name": map[string]interface{}{"first": "MyFirst", "last": "MyLast"}})
+
+		var data map[string]interface{}
+
+		// Test valid JSON
+		err := UnmarshalDynamicTemplateData(string(dynamicTemplateData), &data)
+		assert.NoError(t, err)
+		assert.Equal(t, map[string]interface{}{"first": "MyFirst", "last": "MyLast"}, data["name"])
+
+		// Test invalid JSON
+		err = UnmarshalDynamicTemplateData("{\"wrong\"}", &data)
+		assert.Error(t, err)
 	})
 }

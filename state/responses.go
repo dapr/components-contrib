bfa0b01d2494b1c39783f9b658fd29c79d611ada
joshvanl <me@joshvanl.dev>
2023-06-05 14:10:49 +0100

    Update comment to be RFC3339 formatted string
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- state/responses.go
+++ state/responses.go
@@ -15,7 +15,7 @@ package state
 
 const (
 	// GetRespMetaKeyTTLExpireTime is the key for the metadata value of the TTL
-	// expire time. Value is Milliseconds since Unix Epoch.
+	// expire time. Value is a RFC3339 formatted string.
 	GetRespMetaKeyTTLExpireTime string = "ttlExpireTime"
 )
 

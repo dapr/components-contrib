de01000c9bf38ecc44eb07a6f9bcb38d4abfd1a1
Josh van Leeuwen <me@joshvanl.dev>
2023-06-26 17:08:48 +0100

    ETCD State store v2 (#2932)
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- state/utils/utils.go
+++ state/utils/utils.go
@@ -13,6 +13,13 @@ limitations under the License.
 
 package utils
 
+import (
+	"bytes"
+	"encoding/json"
+	"strconv"
+	"strings"
+)
+
 func Marshal(val interface{}, marshaler func(interface{}) ([]byte, error)) ([]byte, error) {
 	var err error = nil
 	bt, ok := val.([]byte)
@@ -21,4 +28,47 @@ func Marshal(val interface{}, marshaler func(interface{}) ([]byte, error)) ([]by
 	}
 
 	return bt, err
+}
+
+func JSONStringify(value any) ([]byte, error) {
+	switch value := value.(type) {
+	case []byte:
+		return value, nil
+	case int:
+		return []byte(strconv.FormatInt(int64(value), 10)), nil
+	case int8:
+		return []byte(strconv.FormatInt(int64(value), 10)), nil
+	case int16:
+		return []byte(strconv.FormatInt(int64(value), 10)), nil
+	case int32:
+		return []byte(strconv.FormatInt(int64(value), 10)), nil
+	case int64:
+		return []byte(strconv.FormatInt(value, 10)), nil
+	case uint:
+		return []byte(strconv.FormatUint(uint64(value), 10)), nil
+	case uint16:
+		return []byte(strconv.FormatUint(uint64(value), 10)), nil
+	case uint32:
+		return []byte(strconv.FormatUint(uint64(value), 10)), nil
+	case uint64:
+		return []byte(strconv.FormatUint(value, 10)), nil
+	case float32:
+		return []byte(strconv.FormatFloat(float64(value), 'f', -1, 64)), nil
+	case float64:
+		return []byte(strconv.FormatFloat(value, 'f', -1, 64)), nil
+	case bool:
+		if value {
+			return []byte("true"), nil
+		}
+		return []byte("false"), nil
+	case string:
+		return []byte(`"` + strings.ReplaceAll(value, `"`, `\"`) + `"`), nil
+	default:
+		var buf bytes.Buffer
+		enc := json.NewEncoder(&buf)
+		enc.SetEscapeHTML(false)
+		err := enc.Encode(value)
+		// Trim newline.
+		return bytes.TrimSuffix(buf.Bytes(), []byte{0xa}), err
+	}
 }

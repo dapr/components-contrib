50e7ee621f33fe2ff566da9e220ecb0fa357ef39
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2023-07-07 16:42:38 +0000

    Add support for Azure AD auth to Postgres config store
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>

--- internal/component/postgresql/metadata.go
+++ internal/component/postgresql/metadata.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 The Dapr Authors
+Copyright 2023 The Dapr Authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -14,15 +14,10 @@ limitations under the License.
 package postgresql
 
 import (
-	"context"
 	"fmt"
 	"time"
 
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
-	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgxpool"
-
-	"github.com/dapr/components-contrib/internal/authentication/azure"
+	pgauth "github.com/dapr/components-contrib/internal/authentication/postgresql"
 	"github.com/dapr/components-contrib/metadata"
 	"github.com/dapr/components-contrib/state"
 	"github.com/dapr/kit/ptr"
@@ -39,24 +34,17 @@ const (
 )
 
 type postgresMetadataStruct struct {
-	ConnectionString      string         `mapstructure:"connectionString"`
-	ConnectionMaxIdleTime time.Duration  `mapstructure:"connectionMaxIdleTime"`
-	TableName             string         `mapstructure:"tableName"`         // Could be in the format "schema.table" or just "table"
-	MetadataTableName     string         `mapstructure:"metadataTableName"` // Could be in the format "schema.table" or just "table"
-	Timeout               time.Duration  `mapstructure:"timeoutInSeconds"`
-	CleanupInterval       *time.Duration `mapstructure:"cleanupIntervalInSeconds"`
-	MaxConns              int            `mapstructure:"maxConns"`
-	UseAzureAD            bool           `mapstructure:"useAzureAD"`
+	pgauth.PostgresAuthMetadata `mapstructure:",squash"`
 
-	// Set to true if the component can support authentication with Azure AD.
-	// This is different from the "useAzureAD" property above, which is provided by the user and instructs the component to authenticate using Azure AD.
-	azureADEnabled bool
-	azureEnv       azure.EnvironmentSettings
+	TableName         string         `mapstructure:"tableName"`         // Could be in the format "schema.table" or just "table"
+	MetadataTableName string         `mapstructure:"metadataTableName"` // Could be in the format "schema.table" or just "table"
+	Timeout           time.Duration  `mapstructure:"timeoutInSeconds"`
+	CleanupInterval   *time.Duration `mapstructure:"cleanupIntervalInSeconds"`
 }
 
-func (m *postgresMetadataStruct) InitWithMetadata(meta state.Metadata) error {
+func (m *postgresMetadataStruct) InitWithMetadata(meta state.Metadata, azureADEnabled bool) error {
 	// Reset the object
-	m.ConnectionString = ""
+	m.PostgresAuthMetadata.Reset()
 	m.TableName = defaultTableName
 	m.MetadataTableName = defaultMetadataTableName
 	m.CleanupInterval = ptr.Of(defaultCleanupInternal * time.Second)
@@ -69,8 +57,9 @@ func (m *postgresMetadataStruct) InitWithMetadata(meta state.Metadata) error {
 	}
 
 	// Validate and sanitize input
-	if m.ConnectionString == "" {
-		return errMissingConnectionString
+	err = m.PostgresAuthMetadata.InitWithMetadata(meta.Properties, azureADEnabled)
+	if err != nil {
+		return err
 	}
 
 	// Timeout
@@ -79,79 +68,15 @@ func (m *postgresMetadataStruct) InitWithMetadata(meta state.Metadata) error {
 	}
 
 	// Cleanup interval
-	if m.CleanupInterval != nil {
-		// Non-positive value from meta means disable auto cleanup.
-		if *m.CleanupInterval <= 0 {
-			if meta.Properties[cleanupIntervalKey] == "" {
-				// unfortunately the mapstructure decoder decodes an empty string to 0, a missing key would be nil however
-				m.CleanupInterval = ptr.Of(defaultCleanupInternal * time.Second)
-			} else {
-				m.CleanupInterval = nil
-			}
-		}
-	}
-
-	// Populate the Azure environment if using Azure AD
-	if m.azureADEnabled && m.UseAzureAD {
-		m.azureEnv, err = azure.NewEnvironmentSettings(meta.Properties)
-		if err != nil {
-			return err
+	// Non-positive value from meta means disable auto cleanup.
+	if m.CleanupInterval != nil && *m.CleanupInterval <= 0 {
+		if meta.Properties[cleanupIntervalKey] == "" {
+			// Unfortunately the mapstructure decoder decodes an empty string to 0, a missing key would be nil however
+			m.CleanupInterval = ptr.Of(defaultCleanupInternal * time.Second)
+		} else {
+			m.CleanupInterval = nil
 		}
 	}
 
 	return nil
-}
-
-// GetPgxPoolConfig returns the pgxpool.Config object that contains the credentials for connecting to Postgres.
-func (m *postgresMetadataStruct) GetPgxPoolConfig() (*pgxpool.Config, error) {
-	// Get the config from the connection string
-	config, err := pgxpool.ParseConfig(m.ConnectionString)
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse connection string: %w", err)
-	}
-	if m.ConnectionMaxIdleTime > 0 {
-		config.MaxConnIdleTime = m.ConnectionMaxIdleTime
-	}
-	if m.MaxConns > 1 {
-		config.MaxConns = int32(m.MaxConns)
-	}
-
-	// Check if we should use Azure AD
-	if m.azureADEnabled && m.UseAzureAD {
-		tokenCred, errToken := m.azureEnv.GetTokenCredential()
-		if errToken != nil {
-			return nil, errToken
-		}
-
-		// Reset the password
-		config.ConnConfig.Password = ""
-
-		/*// For Azure AD, using SSL is required
-		// If not already enabled, configure TLS without certificate validation
-		if config.ConnConfig.TLSConfig == nil {
-			config.ConnConfig.TLSConfig = &tls.Config{
-				//nolint:gosec
-				InsecureSkipVerify: true,
-			}
-		}*/
-
-		// We need to retrieve the token every time we attempt a new connection
-		// This is because tokens expire, and connections can drop and need to be re-established at any time
-		// Fortunately, we can do this with the "BeforeConnect" hook
-		config.BeforeConnect = func(ctx context.Context, cc *pgx.ConnConfig) error {
-			at, err := tokenCred.GetToken(ctx, policy.TokenRequestOptions{
-				Scopes: []string{
-					m.azureEnv.Cloud.Services[azure.ServiceOSSRDBMS].Audience + "/.default",
-				},
-			})
-			if err != nil {
-				return err
-			}
-
-			cc.Password = at.Token
-			return nil
-		}
-	}
-
-	return config, nil
 }

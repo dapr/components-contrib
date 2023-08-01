12989f778047cf95303c630e639b5367f86cc36f
Alessandro (Ale) Segala <43508+ItalyPaleAle@users.noreply.github.com>
2023-08-01 11:54:01 -0700

    Revert "Adds OIDC authentication support for pubsub Apache Pulsar (#3023)"
    This reverts commit af5bacebf4877e0c10ba860737ce8a7ccb0ee02c.

--- pubsub/pulsar/pulsar.go
+++ pubsub/pulsar/pulsar.go
@@ -25,12 +25,12 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/hamba/avro/v2"
+
 	"github.com/apache/pulsar-client-go/pulsar"
 	"github.com/apache/pulsar-client-go/pulsar/crypto"
-	"github.com/hamba/avro/v2"
 	lru "github.com/hashicorp/golang-lru/v2"
 
-	"github.com/dapr/components-contrib/internal/authentication/oidc"
 	"github.com/dapr/components-contrib/metadata"
 	"github.com/dapr/components-contrib/pubsub"
 	"github.com/dapr/kit/logger"
@@ -54,10 +54,6 @@ const (
 	jsonProtocol            = "json"
 	protoProtocol           = "proto"
 	partitionKey            = "partitionKey"
-
-	authTypeNone  = "none"
-	authTypeToken = "token"
-	authTypeOIDC  = "oidc"
 
 	defaultTenant     = "public"
 	defaultNamespace  = "default"
@@ -98,14 +94,13 @@ const (
 type ProcessMode string
 
 type Pulsar struct {
-	logger       logger.Logger
-	client       pulsar.Client
-	metadata     pulsarMetadata
-	oidcProvider *oidc.ClientCredentials
-	cache        *lru.Cache[string, pulsar.Producer]
-	closed       atomic.Bool
-	closeCh      chan struct{}
-	wg           sync.WaitGroup
+	logger   logger.Logger
+	client   pulsar.Client
+	metadata pulsarMetadata
+	cache    *lru.Cache[string, pulsar.Producer]
+	closed   atomic.Bool
+	closeCh  chan struct{}
+	wg       sync.WaitGroup
 }
 
 func NewPulsar(l logger.Logger) pubsub.PubSub {
@@ -162,7 +157,7 @@ func parsePulsarMetadata(meta pubsub.Metadata) (*pulsarMetadata, error) {
 	return &m, nil
 }
 
-func (p *Pulsar) Init(ctx context.Context, metadata pubsub.Metadata) error {
+func (p *Pulsar) Init(_ context.Context, metadata pubsub.Metadata) error {
 	m, err := parsePulsarMetadata(metadata)
 	if err != nil {
 		return err
@@ -178,37 +173,9 @@ func (p *Pulsar) Init(ctx context.Context, metadata pubsub.Metadata) error {
 		ConnectionTimeout:          30 * time.Second,
 		TLSAllowInsecureConnection: !m.EnableTLS,
 	}
-
-	switch m.AuthType {
-	case "":
-		// To ensure backward compatibility, if authType is not set but the token
-		// is we fallthrough to token auth.
-		if m.Token == "" {
-			break
-		}
-		fallthrough
-	case authTypeToken:
+	if m.Token != "" {
 		options.Authentication = pulsar.NewAuthenticationToken(m.Token)
-	case authTypeOIDC:
-		var cc *oidc.ClientCredentials
-		cc, err = oidc.NewClientCredentials(ctx, oidc.ClientCredentialsOptions{
-			Logger:       p.logger,
-			TokenURL:     m.OIDCTokenURL,
-			CAPEM:        []byte(m.OIDCTokenCAPEM),
-			ClientID:     m.OIDCClientID,
-			ClientSecret: m.OIDCClientSecret,
-			Scopes:       m.OIDCScopes,
-			Audiences:    m.OIDCAudiences,
-		})
-		if err != nil {
-			return fmt.Errorf("could not instantiate oidc token provider: %v", err)
-		}
-
-		options.Authentication = pulsar.NewAuthenticationTokenFromSupplier(cc.Token)
-		p.oidcProvider = cc
-		p.oidcProvider.Run(ctx)
 	}
-
 	client, err := pulsar.NewClient(options)
 	if err != nil {
 		return fmt.Errorf("could not instantiate pulsar client: %v", err)
@@ -522,10 +489,6 @@ func (p *Pulsar) Close() error {
 		}
 	}
 	p.client.Close()
-
-	if p.oidcProvider != nil {
-		p.oidcProvider.Close()
-	}
 
 	return nil
 }

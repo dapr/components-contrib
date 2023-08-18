f9f77695de76311624afcc96e4d65fafd9ce6424
yaron2 <schneider.yaron@live.com>
2023-08-17 21:33:44 -0700

    add nil config check
    Signed-off-by: yaron2 <schneider.yaron@live.com>

--- secretstores/hashicorp/vault/vault.go
+++ secretstores/hashicorp/vault/vault.go
@@ -423,7 +423,7 @@ func (v *vaultSecretStore) initVaultToken() error {
 func (v *vaultSecretStore) createHTTPClient(config *tlsConfig) (*http.Client, error) {
 	tlsClientConfig := &tls.Config{MinVersion: tls.VersionTLS12}
 
-	if config.vaultSkipVerify {
+	if config != nil && config.vaultSkipVerify {
 		v.logger.Infof("hashicorp vault: you are using 'skipVerify' to skip server config verify which is unsafe!")
 	}
 

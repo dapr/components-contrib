6adedf59a3dc0866e1cd0659ef796a7e072d3336
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2023-10-31 16:28:33 +0000

    Azure Blob Storage v2 state store
    The v2 of the component does not strip the app's prefix
    
    Fixes #3032
    
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>

--- state/azure/blobstorage/v1/v1_test.go
+++ state/azure/blobstorage/v1/v1_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 The Dapr Authors
+Copyright 2023 The Dapr Authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -14,31 +14,10 @@ limitations under the License.
 package blobstorage
 
 import (
-	"context"
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-
-	"github.com/dapr/components-contrib/state"
-	"github.com/dapr/kit/logger"
 )
-
-func TestInit(t *testing.T) {
-	m := state.Metadata{}
-	s := &StateStore{
-		logger: logger.NewLogger("logger"),
-	}
-
-	t.Run("Init with missing metadata", func(t *testing.T) {
-		m.Properties = map[string]string{
-			"invalidValue": "a",
-		}
-		err := s.Init(context.Background(), m)
-		assert.Error(t, err)
-		assert.Equal(t, err, fmt.Errorf("missing or empty accountName field from metadata"))
-	})
-}
 
 func TestFileName(t *testing.T) {
 	t.Run("Valid composite key", func(t *testing.T) {

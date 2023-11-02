0d488c2a9d724fffcfc5c432cbd814014231b729
Alessandro (Ale) Segala <43508+ItalyPaleAle@users.noreply.github.com>
2023-11-02 16:19:22 -0700

    Azure Blob Storage v2 state store (#3203)
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
    Signed-off-by: Alessandro (Ale) Segala <43508+ItalyPaleAle@users.noreply.github.com>
    Co-authored-by: Bernd Verst <github@bernd.dev>

---
+++ state/azure/blobstorage/internal/blobstorage_test.go
@@ -0,0 +1,41 @@
+/*
+Copyright 2021 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package internal
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/dapr/components-contrib/state"
+	"github.com/dapr/kit/logger"
+)
+
+func TestInit(t *testing.T) {
+	m := state.Metadata{}
+	s := &StateStore{
+		logger: logger.NewLogger("logger"),
+	}
+
+	t.Run("Init with missing metadata", func(t *testing.T) {
+		m.Properties = map[string]string{
+			"invalidValue": "a",
+		}
+		err := s.Init(context.Background(), m)
+		assert.Error(t, err)
+		assert.Equal(t, err, fmt.Errorf("missing or empty accountName field from metadata"))
+	})
+}

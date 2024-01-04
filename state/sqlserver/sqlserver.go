de34ae4fc69aa8138191c76245f1ca951f7476dc
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2024-01-04 01:22:10 +0000

    Create shared auth package for SQL Server
    Similarly to other databases like Postgres and SQLite, moving auth code for MS SQL Server to a shared package.
    
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>

--- state/sqlserver/sqlserver.go
+++ state/sqlserver/sqlserver.go
@@ -151,11 +151,11 @@ END TRY
 BEGIN CATCH
 UPDATE [%[1]s].[%[2]s] SET [Value] = CONVERT(nvarchar(MAX), GETDATE(), 21) WHERE [Key] = 'last-cleanup' AND Datediff_big(MS, [Value], GETUTCDATE()) > @Interval
 END CATCH
-COMMIT TRANSACTION;`, s.metadata.Schema, s.metadata.MetadataTableName), sql.Named("Interval", arg)
+COMMIT TRANSACTION;`, s.metadata.SchemaName, s.metadata.MetadataTableName), sql.Named("Interval", arg)
 		},
 		DeleteExpiredValuesQuery: fmt.Sprintf(
 			`DELETE FROM [%s].[%s] WHERE [ExpireDate] IS NOT NULL AND [ExpireDate] < GETDATE()`,
-			s.metadata.Schema, s.metadata.TableName,
+			s.metadata.SchemaName, s.metadata.TableName,
 		),
 		CleanupInterval: *s.metadata.CleanupInterval,
 		DB:              commonsql.AdaptDatabaseSQLConn(s.db),

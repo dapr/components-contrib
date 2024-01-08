5228ccec7e66fe35c98263cffe3214953f0b9e89
Alessandro (Ale) Segala <43508+ItalyPaleAle@users.noreply.github.com>
2024-01-08 09:13:43 -0800

    [1.12] Nameresolvers: add support for ResolveIDMulti (#3287)
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>

--- nameresolution/nameresolution.go
+++ nameresolution/nameresolution.go
@@ -13,10 +13,37 @@ limitations under the License.
 
 package nameresolution
 
+import (
+	"math/rand"
+)
+
 // Resolver is the interface of name resolver.
 type Resolver interface {
 	// Init initializes name resolver.
 	Init(metadata Metadata) error
 	// ResolveID resolves name to address.
 	ResolveID(req ResolveRequest) (string, error)
+}
+
+// ResolverMulti is an optional interface for name resolvers that can return multiple addresses.
+type ResolverMulti interface {
+	ResolveIDMulti(req ResolveRequest) (AddressList, error)
+}
+
+// AddressList is a list of addresses resolved by the nameresolver
+type AddressList []string
+
+// Pick returns a random address from the list
+func (a AddressList) Pick() string {
+	l := len(a)
+	switch l {
+	case 0:
+		return ""
+	case 1:
+		return a[0]
+	default:
+		// We use math/rand here as we are just picking a random address, so we don't need a CSPRNG
+		//nolint:gosec
+		return a[rand.Intn(l)]
+	}
 }

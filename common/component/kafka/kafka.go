b572ee936a38cc32ca609fe414247b3c18fd167e
Samantha Coyle <sam@diagrid.io>
2024-03-05 16:05:33 -0600

    style: make linter happy
    Signed-off-by: Samantha Coyle <sam@diagrid.io>

--- common/component/kafka/kafka.go
+++ common/component/kafka/kafka.go
@@ -143,8 +143,8 @@ func (k *Kafka) Init(ctx context.Context, metadata map[string]string) error {
 	config.Consumer.Fetch.Default = meta.consumerFetchDefault
 	config.ChannelBufferSize = meta.channelBufferSize
 
-	config.Net.KeepAlive = time.Duration(meta.clientConnectionKeepAliveInterval) * time.Minute
-	config.Metadata.RefreshFrequency = time.Duration(meta.clientConnectionRefreshInterval) * time.Minute
+	config.Net.KeepAlive = meta.clientConnectionKeepAliveInterval
+	config.Metadata.RefreshFrequency = meta.clientConnectionRefreshInterval
 
 	if meta.ClientID != "" {
 		config.ClientID = meta.ClientID

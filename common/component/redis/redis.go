85920e554fb792bcb61ed5e003642606b95905c5
Bernd Verst <github@bernd.dev>
2024-07-01 08:55:00 -0700

    update redundant type declaration for linter
    Signed-off-by: Bernd Verst <github@bernd.dev>

--- common/component/redis/redis.go
+++ common/component/redis/redis.go
@@ -232,8 +232,8 @@ func StartEntraIDTokenRefreshBackgroundRoutine(client RedisClient, username stri
 		backoffConfig.Policy = kitretry.PolicyExponential
 
 		var backoffManager backoff.BackOff
-		var refreshGracePeriod time.Duration = 2 * time.Minute
-		var tokenRefreshDuration time.Duration = time.Until(nextExpiration.Add(-refreshGracePeriod))
+		var refreshGracePeriod = 2 * time.Minute
+		var tokenRefreshDuration = time.Until(nextExpiration.Add(-refreshGracePeriod))
 
 		(*logger).Debugf("redis client: starting entraID token refresh loop")
 

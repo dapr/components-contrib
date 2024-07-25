baf4aee381c9d6e85894a57e90f4ebda5c71f60f
Eileen Yu <48944635+Eileen-Yu@users.noreply.github.com>
2024-07-25 09:40:08 -0700

    fix: drop duplicate awsRegion/region field (#3490)
    Signed-off-by: Eileen Yu <eileenylj@gmail.com>

--- secretstores/aws/parameterstore/parameterstore.go
+++ secretstores/aws/parameterstore/parameterstore.go
@@ -45,11 +45,13 @@ func NewParameterStore(logger logger.Logger) secretstores.SecretStore {
 }
 
 type ParameterStoreMetaData struct {
-	Region       string `json:"region"`
+	// Ignored by metadata parser because included in built-in authentication profile
 	AccessKey    string `json:"accessKey" mapstructure:"accessKey" mdignore:"true"`
 	SecretKey    string `json:"secretKey" mapstructure:"secretKey" mdignore:"true"`
-	SessionToken string `json:"sessionToken"`
-	Prefix       string `json:"prefix"`
+	SessionToken string `json:"sessionToken" mapstructure:"sessionToken" mdignore:"true"`
+
+	Region string `json:"region" mapstructure:"region" mapstructurealiases:"awsRegion" mdignore:"true"`
+	Prefix string `json:"prefix"`
 }
 
 type ssmSecretStore struct {

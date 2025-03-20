768a6259814bd0680f87425d18b859ced1d16d7d
Nemanja Malocic <n.malocic@vegait.rs>
2025-03-20 16:24:11 +0100

    Changed extension ffrom yaml to yml
    Signed-off-by: Nemanja Malocic <n.malocic@vegait.rs>

--- tests/certification/state/ravendb/ravendb_test.go
+++ tests/certification/state/ravendb/ravendb_test.go
@@ -24,7 +24,7 @@ const (
 	sidecarNamePrefix       = "ravendb-sidecar-"
 	stateStoreName          = "statestore"
 	certificationTestPrefix = "stable-certification-"
-	dockerComposeYAML       = "docker-compose.yaml"
+	dockerComposeYAML       = "docker-compose.yml"
 )
 
 func TestRavenDB(t *testing.T) {

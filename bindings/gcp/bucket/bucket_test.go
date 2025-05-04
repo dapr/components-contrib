f933779727a5f58816439170e3f949004eccea9a
nelson.parente <nelson_parente@live.com.pt>
2025-05-04 17:23:40 +0100

    fix: golint
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>

--- bindings/gcp/bucket/bucket_test.go
+++ bindings/gcp/bucket/bucket_test.go
@@ -325,7 +325,6 @@ func TestRenameOption(t *testing.T) {
 		require.Error(t, err)
 		assert.Equal(t, "gcp bucket binding error: required 'newName' missing", err.Error())
 	})
-
 }
 
 func TestMoveOption(t *testing.T) {
@@ -363,5 +362,4 @@ func TestMoveOption(t *testing.T) {
 		require.Error(t, err)
 		assert.Equal(t, "gcp bucket binding error: required 'destinationBucket' missing", err.Error())
 	})
-
 }

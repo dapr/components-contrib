2813db2df288453b716cbc19132675f0dec0901e
Anton Troshin <18515134+antontroshin@users.noreply.github.com>
2025-05-06 10:03:08 -0500

    Fix MQTT3 pubsub certification error (#3805)
    Signed-off-by: Anton Troshin <anton@diagrid.io>

--- tests/certification/pubsub/mqtt3/mqtt_test.go
+++ tests/certification/pubsub/mqtt3/mqtt_test.go
@@ -318,7 +318,7 @@ func TestMQTT(t *testing.T) {
 		// Run the Dapr sidecar with the MQTTPubSub component.
 		Step(sidecar.Run(sidecarName1,
 			append(componentRuntimeOptions(),
-				embedded.WithComponentsPath("./components/consumer1"),
+				embedded.WithResourcesPath("./components/consumer1"),
 				embedded.WithAppProtocol(protocol.HTTPProtocol, strconv.Itoa(appPort)),
 				embedded.WithDaprGRPCPort(strconv.Itoa(runtime.DefaultDaprAPIGRPCPort)),
 				embedded.WithDaprHTTPPort(strconv.Itoa(runtime.DefaultDaprHTTPPort)),
@@ -336,7 +336,7 @@ func TestMQTT(t *testing.T) {
 		// Run the Dapr sidecar with the MQTTPubSub component.
 		Step(sidecar.Run(sidecarName2,
 			append(componentRuntimeOptions(),
-				embedded.WithComponentsPath("./components/consumer2"),
+				embedded.WithResourcesPath("./components/consumer2"),
 				embedded.WithAppProtocol(protocol.HTTPProtocol, strconv.Itoa(appPort+portOffset)),
 				embedded.WithDaprGRPCPort(strconv.Itoa(runtime.DefaultDaprAPIGRPCPort+portOffset)),
 				embedded.WithDaprHTTPPort(strconv.Itoa(runtime.DefaultDaprHTTPPort+portOffset)),

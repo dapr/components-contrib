ab4b1300d0b8b6b868e70e4b986f2123aa8db463
Prashanth Nagaraj <pnagaraj@infoblox.com>
2025-06-20 22:10:55 +0530

    feat: Add support for replicateSubscriptionState in Pulsar pubsub component
    This commit adds support for the replicateSubscriptionState flag in the Pulsar
    pubsub component, which enables replication of subscription state across
    different Pulsar clusters in a geo-replicated setup.
    
    Changes:
    - Added ReplicateSubscriptionState field to pulsarMetadata struct
    - Updated Subscribe method to use ReplicateSubscriptionState in ConsumerOptions
    - Added comprehensive tests for the new functionality
    
    Fixes the missing support for subscription state replication in multi-cluster
    Pulsar deployments.
    
    Signed-off-by: Prashanth Nagaraj <pnagaraj@infoblox.com>

--- pubsub/pulsar/metadata.go
+++ pubsub/pulsar/metadata.go
@@ -39,6 +39,7 @@ type pulsarMetadata struct {
 	ReceiverQueueSize                int                       `mapstructure:"receiverQueueSize"`
 	SubscriptionType                 string                    `mapstructure:"subscribeType"`
 	SubscriptionInitialPosition      string                    `mapstructure:"subscribeInitialPosition"`
+	ReplicateSubscriptionState       bool                      `mapstructure:"replicateSubscriptionState"`
 	Token                            string                    `mapstructure:"token"`
 	oauth2.ClientCredentialsMetadata `mapstructure:",squash"`
 }

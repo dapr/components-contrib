e2e682db76f646d04a87a6fe62b5786cc8bee948
Fabian Martinez <46371672+famarting@users.noreply.github.com>
2025-07-11 15:20:24 +0200

    add conversation components to component metadata schema and bundle (#3882)
    Signed-off-by: Fabian Martinez <46371672+famarting@users.noreply.github.com>

--- metadata/utils.go
+++ metadata/utils.go
@@ -160,7 +160,8 @@ func (t ComponentType) IsValid() bool {
 		SecretStoreType, PubSubType,
 		LockStoreType, ConfigurationStoreType,
 		MiddlewareType, CryptoType,
-		NameResolutionType, WorkflowType:
+		NameResolutionType, WorkflowType,
+		ConversationType:
 		return true
 	default:
 		return false

b0ab0b927998708151ee686d69ac3f1f23c3b1b1
Kobbi Gal <kobbi.g@akeyless.io>
2025-10-02 09:52:37 -0400

    added single dynamic secret value support

--- secretstores/akeyless/utils.go
+++ secretstores/akeyless/utils.go
@@ -159,6 +159,8 @@ func GetSecretType(secretName string, akeylessSecretStore *akeylessSecretStore) 
 	return *describeItemResp.ItemType, nil
 }
 
+// GetSingleSecretValue gets the value of a single secret from Akeyless.
+// It returns the value of the secret or an error if the secret is not found.
 func GetSingleSecretValue(secretName string, secretType string, akeylessSecretStore *akeylessSecretStore) (string, error) {
 
 	var secretValue string
@@ -205,318 +207,45 @@ func GetSingleSecretValue(secretName string, secretType string, akeylessSecretSt
 			err = fmt.Errorf("failed to assert type of secret response to string for secret '%s'", secretName)
 		}
 
-	// TODO implement dynamic secrets
 	case AKEYLESS_SECRET_TYPE_DYNAMIC_SECRET_RESPONSE:
-		return "", errors.New("dynamic secrets are not supported")
+		getDynamicSecretValue := akeyless.NewGetDynamicSecretValue(secretName)
+		getDynamicSecretValue.SetToken(akeylessSecretStore.token)
+		secretRespMap, _, apiErr := akeylessSecretStore.v2.GetDynamicSecretValue(context.Background()).Body(*getDynamicSecretValue).Execute()
+		if apiErr != nil {
+			err = fmt.Errorf("failed to get dynamic secret '%s' value from Akeyless API: %w", secretName, apiErr)
+			break
+		}
+
+		// assert type of secretRespMap to DynamicSecretResponse
+		var dynamicSecretResp DynamicSecretResponse
+		jsonBytes, marshalErr := json.Marshal(secretRespMap)
+		if marshalErr != nil {
+			err = fmt.Errorf("failed to marshal secret response to JSON: %w", marshalErr)
+			break
+		}
+		if unmarshalErr := json.Unmarshal([]byte(jsonBytes), &dynamicSecretResp); unmarshalErr != nil {
+			err = fmt.Errorf("failed to unmarshal secret response to DynamicSecretResponse: %w", unmarshalErr)
+			break
+		}
+
+		// take only relevant fields (DisplayName and SecretText) from response and marshal it to a JSON string
+		dynamicSecretResp.Secret.AppID = ""
+		dynamicSecretResp.Secret.EndDateTime = ""
+		dynamicSecretResp.Secret.KeyID = ""
+		dynamicSecretResp.Secret.TenantID = ""
+		jsonBytes, marshalErr = json.Marshal(dynamicSecretResp.Secret)
+		if marshalErr != nil {
+			err = fmt.Errorf("failed to marshal secret response to JSON: %w", marshalErr)
+			break
+		}
+		secretValue = string(jsonBytes)
+
 	// TODO implement rotated secrets
 	case AKEYLESS_SECRET_TYPE_ROTATED_SECRET_RESPONSE:
 		return "", errors.New("rotated secrets are not supported")
 	}
 
 	return secretValue, err
-}
-
-// GetSecretValueByType gets the secret value by the type of the secret.
-// It returns the secret value or an error if the secret value is not found.
-// If secretName is not a string it means that we're getting numerous static secrets
-// and we can get them all at once
-func GetSecretValueByType(secretName any, secretType string, akeylessSecretStore *akeylessSecretStore) (secretstores.GetSecretResponse, error) {
-	var secretResp secretstores.GetSecretResponse
-	switch secretType {
-	case AKEYLESS_SECRET_TYPE_STATIC_SECRET_RESPONSE:
-		var secrets []string
-		switch secretName := secretName.(type) {
-		case string:
-			secrets = []string{secretName}
-		case []string:
-			secrets = secretName
-		}
-		getSecretValue := akeyless.NewGetSecretValue(secrets)
-		getSecretValue.SetToken(akeylessSecretStore.token)
-		secretRespMap, _, err := akeylessSecretStore.v2.GetSecretValue(context.Background()).Body(*getSecretValue).Execute()
-		if err != nil {
-			return secretstores.GetSecretResponse{}, fmt.Errorf("failed to get secret '%s' value for static secret from Akeyless API: %w", secretName.(string), err)
-		}
-
-		transformedResp, err := transformStaticSecretResponse(secretRespMap)
-		if err != nil {
-			return secretstores.GetSecretResponse{}, fmt.Errorf("failed to transform static secret response for secret '%s': %w", secretName.(string), err)
-		}
-		secretResp = transformedResp
-	// case AKEYLESS_SECRET_TYPE_DYNAMIC_SECRET_RESPONSE:
-	// 	getSecretValue := akeyless.NewGetDynamicSecretValue(secretName.(string))
-	// 	getSecretValue.SetToken(akeylessSecretStore.token)
-	// 	secretRespMap, _, err := akeylessSecretStore.v2.GetDynamicSecretValue(context.Background()).Body(*getSecretValue).Execute()
-
-	// 	if err != nil {
-	// 		return secretstores.GetSecretResponse{}, fmt.Errorf("failed to get dynamic secret '%s' value from Akeyless API: %w", secretName.(string), err)
-	// 	}
-	// 	// Convert secretRespMap (map[string]interface{}) to DynamicSecretResponse using a helper function
-	// 	dynamicSecretResp, err := transformDynamicSecretInterface(secretRespMap)
-	// 	if err != nil {
-	// 		return secretstores.GetSecretResponse{}, fmt.Errorf("failed to transform dynamic secret response for secret '%s': %w", secretName.(string), err)
-	// 	}
-
-	// 	transformedResp := transformDynamicSecretResponse(secretName.(string), dynamicSecretResp)
-	// 	secretResp = transformedResp
-	// case AKEYLESS_SECRET_TYPE_ROTATED_SECRET_RESPONSE:
-	// 	getSecretValue := akeyless.NewGetRotatedSecretValue(secretName.(string))
-	// 	getSecretValue.SetToken(akeylessSecretStore.token)
-	// 	secretRespMap, _, err := akeylessSecretStore.v2.GetRotatedSecretValue(context.Background()).Body(*getSecretValue).Execute()
-	// 	if err != nil {
-	// 		return secretstores.GetSecretResponse{}, fmt.Errorf("failed to get rotated secret '%s' value from Akeyless API: %w", secretName.(string), err)
-	// 	}
-
-	// 	// assert type of secretResp to RotatedSecretResponse
-	// 	rotatedSecretResp, ok := secretRespMap.(RotatedSecretResponse)
-	// 	if !ok {
-	// 		return secretstores.GetSecretResponse{}, fmt.Errorf("failed to assert type of secret response to RotatedSecretResponse: %w", err)
-	// 	}
-
-	// 	// transform the response
-	// 	transformedResp := transformRotatedSecretResponse(secretName.(string), rotatedSecretResp)
-	// 	secretResp = transformedResp
-	default:
-		return secretstores.GetSecretResponse{}, errors.New("unsupported secret type")
-	}
-	return secretResp, nil
-}
-
-// func transformDynamicSecretInterface(secretRespMap map[string]interface{}) (DynamicSecretResponse, error) {
-// 	dynamicSecretResp := DynamicSecretResponse{}
-// 	// Attempt to map fields from secretRespMap to dynamicSecretResp
-// 	// This assumes DynamicSecretResponse is a struct and secretRespMap is a map[string]interface{}
-// 	// Use mapstructure or manual assignment as needed
-// 	err := mapstructure.Decode(secretRespMap, &dynamicSecretResp)
-// 	if err != nil {
-// 		return DynamicSecretResponse{}, fmt.Errorf("failed to decode secret response to DynamicSecretResponse: %w", err)
-// 	}
-// 	return dynamicSecretResp, nil
-// }
-
-// convertSecretToString converts the secret to a string.
-// It returns the secret value or an error if the secret value is not found.
-// func convertSecretResponseToString(secret any) (string, error) {
-
-// 	// If the secret is a string, return it
-// 	if secretStr, ok := secret.(string); ok {
-// 		return secretStr, nil
-// 	}
-
-// 	// If the secret is a map, marshal it to a JSON string
-// 	secretValueBytes, err := json.Marshal(secret)
-// 	if err != nil {
-// 		return "", fmt.Errorf("failed to marshal secret response: %w", err)
-// 	}
-
-// 	// Return the JSON string
-// 	return string(secretValueBytes), nil
-// }
-
-// result is a helper struct to pass results and errors back from goroutines
-// type result struct {
-// 	key   string
-// 	value *secretstores.GetSecretResponse
-// 	error error
-// }
-
-// BulkGetSecretResponse takes in a list of `akeyless.Item`,
-// splits them into static and non-static subsets
-// sends an async request to the appropriate API endpoint for each subset
-// and returns them as the corresponding `secretstores.BulkGetSecretResponse`
-// func BulkGetSecretResponse(ctx context.Context, allSecrets []akeyless.Item, akeylessSecretStore *akeylessSecretStore) (secretstores.BulkGetSecretResponse, error) {
-// 	// Split secrets into static and non-static
-// 	staticSecrets, nonStaticSecrets, err := splitSecretsByType(allSecrets)
-// 	if err != nil {
-// 		return secretstores.BulkGetSecretResponse{}, fmt.Errorf("failed to split secrets by type: %w", err)
-// 	}
-
-// 	akeylessSecretStore.logger.Debug("%d static secrets, %d non-static secrets", len(staticSecrets), len(nonStaticSecrets))
-
-// 	// Process static secrets in batch
-// 	staticResults, err := processStaticSecrets(staticSecrets, akeylessSecretStore)
-// 	if err != nil {
-// 		return secretstores.BulkGetSecretResponse{}, fmt.Errorf("failed to process static secrets: %w", err)
-// 	}
-
-// 	// Process non-static secrets asynchronously
-// 	nonStaticResults, err := processNonStaticSecrets(ctx, nonStaticSecrets, akeylessSecretStore)
-// 	if err != nil {
-// 		return secretstores.BulkGetSecretResponse{}, fmt.Errorf("failed to process non-static secrets: %w", err)
-// 	}
-
-// 	// Combine all results
-// 	allResults := make(map[string]map[string]string)
-// 	maps.Copy(allResults, staticResults)
-// 	maps.Copy(allResults, nonStaticResults)
-
-// 	return secretstores.BulkGetSecretResponse{
-// 		Data: allResults,
-// 	}, nil
-// }
-
-// splitSecretsByType splits secrets into static and non-static (dynamic, rotated) secrets
-// func splitSecretsByType(allSecrets []akeyless.Item) ([]string, []akeyless.Item, error) {
-// 	var staticSecrets []string
-// 	var nonStaticSecrets []akeyless.Item
-
-// 	for _, secret := range allSecrets {
-// 		if secret.ItemType == nil || secret.ItemName == nil {
-// 			continue
-// 		}
-
-// 		if *secret.ItemType == AKEYLESS_SECRET_TYPE_STATIC {
-// 			staticSecrets = append(staticSecrets, *secret.ItemName)
-// 		} else {
-// 			nonStaticSecrets = append(nonStaticSecrets, secret)
-// 		}
-// 	}
-
-// 	return staticSecrets, nonStaticSecrets, nil
-// }
-
-// processStaticSecrets processes all static secrets in a single batch request
-// func processStaticSecrets(staticSecrets []string, akeylessSecretStore *akeylessSecretStore) (map[string]map[string]string, error) {
-// 	if len(staticSecrets) == 0 {
-// 		return make(map[string]map[string]string), nil
-// 	}
-
-// 	// Get all static secrets in one batch request
-// 	secretValue, err := GetSecretValueByType(staticSecrets, AKEYLESS_SECRET_TYPE_STATIC, akeylessSecretStore)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed to get static secrets: %w", err)
-// 	}
-
-// 	// Parse the response - static secrets return a map of secret names to values
-// 	var secretsMap map[string]string
-// 	if err := json.Unmarshal([]byte(secretValue.(string)), &secretsMap); err != nil {
-// 		return nil, fmt.Errorf("failed to parse static secrets response: %w", err)
-// 	}
-
-// 	// Convert to the expected format
-// 	result := make(map[string]map[string]string)
-// 	for secretName, secretValue := range secretsMap {
-// 		result[secretName] = map[string]string{
-// 			secretName: secretValue,
-// 		}
-// 	}
-
-// 	return result, nil
-// }
-
-// processNonStaticSecrets processes dynamic and rotated secrets asynchronously
-// func processNonStaticSecrets(ctx context.Context, nonStaticSecrets []akeyless.Item, akeylessSecretStore *akeylessSecretStore) (map[string]map[string]string, error) {
-// 	if len(nonStaticSecrets) == 0 {
-// 		return make(map[string]map[string]string), nil
-// 	}
-
-// 	// A WaitGroup to wait for all goroutines to finish
-// 	var wg sync.WaitGroup
-
-// 	// A buffered channel to receive results from goroutines
-// 	resultsChannel := make(chan result, len(nonStaticSecrets))
-
-// 	// Launch a goroutine for each non-static secret
-// 	for _, secret := range nonStaticSecrets {
-// 		wg.Add(1)
-
-// 		currentSecret := secret
-
-// 		go func() {
-// 			defer wg.Done()
-
-// 			// Fetch the secret value
-// 			secretValue, err := GetSecretValueByTypeAsync(ctx, *currentSecret.ItemName, *currentSecret.ItemType, akeylessSecretStore)
-
-// 			// Parse the secret value based on its type
-// 			parsedData, parseErr := parseSecretResponse(*currentSecret.ItemName, *currentSecret.ItemType, secretValue, err)
-
-// 			resultsChannel <- result{
-// 				key:   *currentSecret.ItemName,
-// 				value: parsedData,
-// 				error: parseErr,
-// 			}
-// 		}()
-// 	}
-
-// 	// Launch separate goroutine to close channel once all workers are done
-// 	go func() {
-// 		wg.Wait()
-// 		close(resultsChannel)
-// 	}()
-
-// 	// Collect results
-// 	result := make(map[string]map[string]string)
-// 	for res := range resultsChannel {
-// 		if res.error != nil {
-// 			return nil, fmt.Errorf("failed to get secret %s: %w", res.key, res.error)
-// 		}
-// 		result[res.key] = res.value.Data
-// 	}
-
-// 	return result, nil
-// }
-
-// parseSecretResponse parses the secret response based on the secret type
-// func parseSecretResponse(secretName, secretType, secretValue any, err error) (*secretstores.GetSecretResponse, error) {
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	var secretData map[string]string
-
-// 	switch secretType {
-// 	// TODO: Implement
-// 	case AKEYLESS_SECRET_TYPE_DYNAMIC:
-// 		var dynamicResp map[string]interface{}
-// 		if err := json.Unmarshal([]byte(secretValue), &dynamicResp); err != nil {
-// 			return nil, fmt.Errorf("failed to parse dynamic secret response: %w", err)
-// 		}
-
-// 	case AKEYLESS_SECRET_TYPE_ROTATED:
-// 		// TODO: Implement
-// 	default:
-// 		// For any other type, return as a simple key-value pair
-// 		secretData = map[string]string{
-// 			secretName: secretValue,
-// 		}
-// 	}
-
-// 	return &secretstores.GetSecretResponse{
-// 		Data: secretData,
-// 	}, nil
-// }
-
-// GetSecretValueByTypeAsync gets the secret value by the type of the secret asynchronously
-func GetSecretValueByTypeAsync(ctx context.Context, secretName any, secretType string, akeylessSecretStore *akeylessSecretStore) (any, error) {
-	return GetSecretValueByType(secretName, secretType, akeylessSecretStore)
-}
-
-// transformStaticSecretResponse transforms the static secret response we get from Akeyless API
-// into a map ready for Dapr.
-// Static secrets can be of type text, json, email/password, key/value
-// in case they are JSON/email/password, we need to transform the response into a string ready for Dapr.
-func transformStaticSecretResponse(secrets map[string]any) (secretstores.GetSecretResponse, error) {
-
-	secretData := make(map[string]string)
-
-	for path, secret := range secrets {
-		switch secretType := secret.(type) {
-		case string:
-			secretData[path] = secretType
-		case map[string]any:
-			encoded, err := json.Marshal(secretType)
-			if err != nil {
-				return secretstores.GetSecretResponse{}, fmt.Errorf("failed to marshal secret response: %w", err)
-			}
-			secretData[path] = string(encoded)
-		}
-
-	}
-
-	return secretstores.GetSecretResponse{
-		Data: secretData,
-	}, nil
 }
 
 type DynamicSecretResponse struct {
@@ -527,73 +256,13 @@ type DynamicSecretResponse struct {
 }
 
 type DynamicSecretSecret struct {
-	AppID       string `json:"appId"`
+	AppID       string `json:"appId,omitempty"`
 	DisplayName string `json:"displayName"`
-	EndDateTime string `json:"endDateTime"`
-	KeyID       string `json:"keyId"`
-	SecretText  string `json:"secretText"`
-	TenantID    string `json:"tenantId"`
+	EndDateTime string `json:"endDateTime,omitempty"`
+	KeyID       string `json:"keyId,omitempty"`
+	SecretText  string `json:"secretText,omitempty"`
+	TenantID    string `json:"tenantId,omitempty"`
 }
-
-// type DynamicSecretTransformedResponse struct {
-// 	DynamicSecretName DynamicSecretCredentials `json:"secret_name"`
-// }
-
-// type DynamicSecretCredentials struct {
-// 	Username string `json:"username"`
-// 	Password string `json:"password"`
-// }
-
-// transformDynamicSecretResponse transforms the dynamic secret response we get from Akeyless API
-// into a map ready for Dapr.
-// func transformDynamicSecretResponse(dynamicSecretName string, dynamicResp DynamicSecretResponse) secretstores.GetSecretResponse {
-// 	creds := DynamicSecretCredentials{
-// 		Username: dynamicResp.Secret.DisplayName,
-// 		Password: dynamicResp.Secret.SecretText,
-// 	}
-// 	credsJSON, err := json.Marshal(creds)
-// 	if err != nil {
-// 		return secretstores.GetSecretResponse{
-// 			Data: map[string]string{},
-// 		}
-// 	}
-// 	return secretstores.GetSecretResponse{
-// 		Data: map[string]string{
-// 			dynamicSecretName: string(credsJSON),
-// 		},
-// 	}
-// }
-
-// type RotatedSecretResponse struct {
-// 	Value RotatedSecretValue `json:"value"`
-// }
-
-// type RotatedSecretValue struct {
-// 	Username      string `json:"username"`
-// 	Password      string `json:"password"`
-// 	ApplicationID string `json:"application_id"`
-// }
-
-// type RotatedSecretTransformedResponse struct {
-// 	RotatedSecretName RotatedSecretCredentials `json:"secret_name"`
-// }
-
-// type RotatedSecretCredentials struct {
-// 	Username      string `json:"username"`
-// 	Password      string `json:"password"`
-// 	ApplicationID string `json:"application_id"`
-// }
-
-// // transformDynamicSecretResponse transforms the dynamic secret response we get from Akeyless API
-// // into a map ready for Dapr.
-// func transformRotatedSecretResponse(rotatedSecretName string, rotatedResp RotatedSecretResponse) RotatedSecretTransformedResponse {
-// 	return RotatedSecretTransformedResponse{
-// 		RotatedSecretName: RotatedSecretCredentials{
-// 			Username: rotatedResp.Value.Username,
-// 			Password: rotatedResp.Value.Password,
-// 		},
-// 	}
-// }
 
 func GetDaprSingleSecretResponse(secretName string, secretValue string) (secretstores.GetSecretResponse, error) {
 	return secretstores.GetSecretResponse{
@@ -602,9 +271,3 @@ func GetDaprSingleSecretResponse(secretName string, secretValue string) (secrets
 		},
 	}, nil
 }
-
-// func getDaprBulkSecretResponse(secrets map[string]map[string]string) secretstores.BulkGetSecretResponse {
-// 	return secretstores.BulkGetSecretResponse{
-// 		Data: secrets,
-// 	}
-// }

526ee1aa46e6ad54468a443b616f2580d74239ac
joshvanl <me@joshvanl.dev>
2026-01-21 14:16:51 +0000

    Remove in-memory pubsub retires
    Remove the retry logic from the in-memory pubsub implementation as
    retries are and should be handled completely by dapr.
    
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- pubsub/in-memory/in-memory.go
+++ pubsub/in-memory/in-memory.go
@@ -18,7 +18,6 @@ import (
 	"errors"
 	"sync"
 	"sync/atomic"
-	"time"
 
 	"github.com/dapr/components-contrib/common/eventbus"
 	"github.com/dapr/components-contrib/metadata"
@@ -74,20 +73,10 @@ func (a *bus) Subscribe(ctx context.Context, req pubsub.SubscribeRequest, handle
 		return errors.New("component is closed")
 	}
 
-	// For this component we allow built-in retries because it is backed by memory
 	retryHandler := func(data []byte, md map[string]string) {
-		for range 10 {
-			handleErr := handler(ctx, &pubsub.NewMessage{Data: data, Topic: req.Topic, Metadata: md})
-			if handleErr == nil {
-				break
-			}
-			a.log.Error(handleErr)
-			select {
-			case <-time.After(100 * time.Millisecond):
-				// Nop
-			case <-ctx.Done():
-				return
-			}
+		err := handler(ctx, &pubsub.NewMessage{Data: data, Topic: req.Topic, Metadata: md})
+		if err != nil {
+			a.log.Error(err)
 		}
 	}
 	err := a.bus.SubscribeAsync(req.Topic, retryHandler, true)
